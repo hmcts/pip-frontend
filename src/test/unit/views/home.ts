8fe7b94fc9cf8cc70314bba503143614fab179d1
marcogagliardi <marco.gagliardi@justice.gov.uk>
2021-06-30 10:18:42 +0100

    pip - 436
    changed header to nunchucks

---
+++ src/test/unit/views/home.ts
@@ -0,0 +1,6 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HomeView } from '../../../main/views/home.njk';
+
+describe('test', () => {
+  let HomeView: HomeView;
+});
