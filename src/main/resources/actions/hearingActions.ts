c453a8d471b2d9855dbfed689aeea34d16900776
commitBlob <maro.radovic12@gmail.com>
2021-07-27 10:20:30 +0100

    pip-511: refactor into class functions

---
+++ src/main/resources/actions/hearingActions.ts
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import path from 'path';
+
+export class HearingActions {
+  mocksPath = '../mocks/';
+  
+  getCourtHearings(courtId: number): any {
+    const rawData = fs.readFileSync(path.resolve(__dirname, this.mocksPath, 'hearingsList.json'), 'utf-8');
+    const hearingsData = JSON.parse(rawData);
+    const courtHearings = hearingsData?.results.filter((hearing) => hearing.courtId === courtId);
+    console.log(courtHearings);
+    return courtHearings;
+  }
+
+  getHearingDetails(hearingId: number): any {
+    const rawData = fs.readFileSync(path.resolve(__dirname, this.mocksPath, 'hearingsList.json'), 'utf-8');
+    const hearingsData = JSON.parse(rawData);
+    const hearingDetails = hearingsData?.results.find((hearing) => hearing.hearingId === hearingId);
+    console.log(hearingDetails);
+    return hearingDetails ? hearingDetails : null;
+  }
+}
