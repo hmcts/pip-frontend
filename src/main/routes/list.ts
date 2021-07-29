62cb4a87860db24884c2b2864b716a328c781ce7
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2021-07-29 17:31:28 +0100

    Started to parse initial list

--- src/main/routes/list.ts
+++
@@ -1,9 +0,0 @@
-import { Application } from 'express';
-
-export default function(app: Application): void {
-
-  app.get('/list', (req, res) => {
-    res.render('list');
-  });
-
-}
---
+++ src/main/routes/pages/list.ts
@@ -0,0 +1,12 @@
+import { Application } from 'express';
+import { HearingActions } from '../../resources/actions/hearingActions';
+
+export default function(app: Application): void {
+
+  var courtList = new HearingActions().getCourtHearings(2);
+  console.log(courtList);
+  app.get('/list', (req, res) => {
+    res.render('list', {hearings: courtList});
+  });
+
+}
