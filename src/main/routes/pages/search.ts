2075eac5f06e841b765b62e4164c0d4564c7dc5f
commitBlob <maro.radovic12@gmail.com>
2021-07-30 13:12:47 +0100

    pip-506: retrieve search input on continue

--- src/main/routes/pages/search.ts
+++ src/main/routes/pages/search.ts
@@ -1,9 +1,15 @@
 import { Application } from 'express';
 
-export default function(app: Application): void {
+export default function (app: Application): void {
 
   app.get('/search', (req, res) => {
     res.render('search');
+  });
+
+  app.post('/search', (req, res) => {
+    const searchInput = req.body['search-input'];
+    // TODO: PUB-508
+    console.log(searchInput);
   });
 
 }
