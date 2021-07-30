90421404a947cb721b3782fdef0c5d9d0b0213f8
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2021-07-30 16:16:13 +0100

    Updated Display List

--- src/main/routes/pages/list.ts
+++ src/main/routes/pages/list.ts
@@ -1,12 +1,31 @@
 import { Application } from 'express';
 import { HearingActions } from '../../resources/actions/hearingActions';
+import moment from 'moment';
 
 export default function(app: Application): void {
 
-  var courtList = new HearingActions().getCourtHearings(2);
-  console.log(courtList);
   app.get('/list', (req, res) => {
-    res.render('list', {courtName: 'Test Court', hearings: courtList});
+
+    var courtId = req.query.courtId as string;
+
+    //If no court ID has been supplied, then return the error page
+    if (courtId != undefined) {
+      var courtList = new HearingActions().getCourtHearings(parseInt(courtId as string));
+
+      //Returns the error page if the court list is empty
+      console.log(courtList)
+      if (courtList.size == 0) {
+        res.render('error')
+      }
+
+      res.render('list', {
+        courtName: 'Test Court',
+        hearings: courtList,
+        date: moment().format('MMMM DD YYYY')
+      });
+    } else {
+      res.render('error');
+    }
   });
 
 }
