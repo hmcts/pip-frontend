1ad3c9eef3c9c61ec547fb4930d730f57fe48cef
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2021-08-02 16:57:08 +0100

    PUB-571 - Fixed Linting

--- src/main/controllers/ListController.ts
+++ src/main/controllers/ListController.ts
@@ -6,26 +6,25 @@ export default class ListController {
 
   public get(req: Request, res: Response): void {
 
-    var courtId = req.query.courtId as string;
+    const courtId = req.query.courtId as string;
 
     //If no court ID has been supplied, then return the error page
     if (courtId != undefined) {
-      var courtList = new HearingActions().getCourtHearings(parseInt(courtId as string));
+      const courtList = new HearingActions().getCourtHearings(parseInt(courtId as string));
 
       //Returns the error page if the court list is empty
-      console.log(courtList)
       if (courtList.length == 0) {
-        res.render('error')
+        res.render('error');
       } else {
         res.render('list', {
           courtName: 'Test Court',
           hearings: courtList,
-          date: moment().format('MMMM DD YYYY')
+          date: moment().format('MMMM DD YYYY'),
         });
       }
     } else {
       res.render('error');
     }
-  };
+  }
 
 }
--- src/test/unit/views/list.ts
+++ src/test/unit/views/list.ts
@@ -1,10 +1,10 @@
 import { expect } from 'chai';
-import request from 'supertest'
+import request from 'supertest';
 import moment from 'moment';
 
 import { app } from '../../../main/app';
 
-const PAGE_URL = '/list?courtId=2'
+const PAGE_URL = '/list?courtId=2';
 
 let htmlRes: Document;
 
@@ -16,21 +16,21 @@ describe('List page', () => {
   });
 
   it('Check that back button exists', () => {
-   const header = htmlRes.getElementsByClassName('govuk-back-link');
-   expect(header[0].innerHTML).contains('Back', 'Back button does not contain correct text');
-   expect(header[0].getAttribute('href')).equal('/', 'Back value does not contain correct link');
-  })
+    const header = htmlRes.getElementsByClassName('govuk-back-link');
+    expect(header[0].innerHTML).contains('Back', 'Back button does not contain correct text');
+    expect(header[0].getAttribute('href')).equal('/', 'Back value does not contain correct link');
+  });
 
   it('Should display court name header', () => {
     const header = htmlRes.getElementsByClassName('govuk-heading-l');
     expect(header[0].innerHTML).contains('Test Court hearing list', 'Could not find the header');
-  })
+  });
 
   it('Should display table caption', () => {
     const header = htmlRes.getElementsByClassName('govuk-table__caption--m');
-    let formattedDate = moment().format('MMMM DD YYYY');
+    const formattedDate = moment().format('MMMM DD YYYY');
     expect(header[0].innerHTML).contains('List for ' + formattedDate, 'Could not find the table header');
-  })
+  });
 
   it('Should contain expected column headings', () => {
     const header = htmlRes.getElementsByClassName('govuk-table__header');
@@ -40,12 +40,12 @@ describe('List page', () => {
     expect(header[3].innerHTML).contains('Judges', 'Could not find judges header');
     expect(header[4].innerHTML).contains('Time', 'Could not find time header');
     expect(header[5].innerHTML).contains('Hearing platform', 'Could not find hearing platform header');
-  })
+  });
 
   it('Should contain 4 rows, including the header', () => {
     const rows = htmlRes.getElementsByClassName('govuk-table__row');
     expect(rows.length).equal(4, 'Number of rows is not equal to expected amount');
-  })
+  });
 
   it('Row should contain the correct values', () => {
     const rows = htmlRes.getElementsByClassName('govuk-table__row');
@@ -57,19 +57,19 @@ describe('List page', () => {
     expect(items.item(3).innerHTML).contains('Judge Margarita Ivanonko', 'Judges not found / correct');
     expect(items.item(4).innerHTML).contains('4:46 PM', 'Time not found / correct');
     expect(items.item(5).innerHTML).contains('Microsoft Teams', 'Hearing Platform not found / correct');
-  })
+  });
 
   it('Related content section found', () => {
     const relatedContent = htmlRes.getElementsByClassName('govuk-heading-s');
 
     expect(relatedContent.item(0).innerHTML).contains('Related Content', 'Related Content header not present');
-  })
+  });
 
   it('Link found', () => {
     const relatedContent = htmlRes.getElementsByClassName('govuk-link');
 
     expect(relatedContent.item(0).innerHTML).contains('Find a court or tribunal list', 'Link text is not present');
     expect(relatedContent.item(0).getAttribute("href")).equal('/search-option', 'Link value is not correct');
-  })
+  });
 
-})
+});
