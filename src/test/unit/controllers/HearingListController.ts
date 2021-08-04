ff350c8eab2453a7c97dc77a07dcf3636c21540b
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2021-08-04 10:09:00 +0100

    PUB-571 - Fixed linting errors

--- src/test/unit/controllers/HearingListController.ts
+++ src/test/unit/controllers/HearingListController.ts
@@ -1,50 +1,49 @@
-import HearingListController from "../../../main/controllers/HearingListController";
+import HearingListController from '../../../main/controllers/HearingListController';
 import sinon from 'sinon';
 import { Request, Response } from 'express';
 
 describe('Hearing list Controller', () => {
-  test("Check that court ID that exists renders the list page", () =>  {
-    let hearingListController = new HearingListController();
+  test('Check that court ID that exists renders the list page', () =>  {
+    const hearingListController = new HearingListController();
 
-    let response = { render: function(page, options) {}} as unknown as Response;
-    let request = {query: {courtId: 2}} as unknown as Request;
+    const response = { render: function() {return '';}} as unknown as Response;
+    const request = {query: {courtId: 2}} as unknown as Request;
 
-    let responseMock = sinon.mock(response);
+    const responseMock = sinon.mock(response);
 
-    responseMock.expects('render').once().withArgs('hearing-list')
+    responseMock.expects('render').once().withArgs('hearing-list');
 
-    hearingListController.get(request, response)
-
-    responseMock.verify();
-  });
-
-  test("Check that a court ID that does not return any results renders an error page", () =>  {
-    let hearingListController = new HearingListController();
-
-    let response = { render: function(page, options) {}} as unknown as Response;
-    let request = { query: { courtId: 400}} as unknown as Request;
-
-    let responseMock = sinon.mock(response);
-
-    responseMock.expects('render').once().withArgs('error')
-
-    hearingListController.get(request, response)
+    hearingListController.get(request, response);
 
     responseMock.verify();
   });
 
-  test("Check that a court ID is not defined renders an error", () =>  {
-    let hearingListController = new HearingListController();
+  test('Check that a court ID that does not return any results renders an error page', () =>  {
+    const hearingListController = new HearingListController();
 
-    let response = { render: function(page, options) {}} as unknown as Response;
-    let request = { query: {}} as unknown as Request;
+    const response = { render: function() {return '';}} as unknown as Response;
+    const request = { query: { courtId: 400}} as unknown as Request;
 
-    let responseMock = sinon.mock(response);
+    const responseMock = sinon.mock(response);
 
-    responseMock.expects('render').once().withArgs('error')
+    responseMock.expects('render').once().withArgs('error');
 
-    // @ts-ignore
-    hearingListController.get(request, response)
+    hearingListController.get(request, response);
+
+    responseMock.verify();
+  });
+
+  test('Check that a court ID is not defined renders an error', () =>  {
+    const hearingListController = new HearingListController();
+
+    const response = { render: function() {return '';}} as unknown as Response;
+    const request = { query: {}} as unknown as Request;
+
+    const responseMock = sinon.mock(response);
+
+    responseMock.expects('render').once().withArgs('error');
+
+    hearingListController.get(request, response);
 
     responseMock.verify();
   });
