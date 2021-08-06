36fa67cf869a2bd766ad02bd232a7f3959093cc1
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2021-08-06 14:48:05 +0100

    PUB-507 - Fixed back to top link, updated styling

--- src/main/controllers/CourtListController.ts
+++ src/main/controllers/CourtListController.ts
@@ -1,38 +1,85 @@
 import {Application} from "express";
 import {CourtActions} from '../resources/actions/CourtActions';
+import {InputFilterService} from "../service/inputFilterService";
 import {randomBytes} from 'crypto'
 
 export default class CourtListController {
 
-  public get(req: Request, res: Application) {
+  /**
+   * Generates the Nonce header for the inline javascript, and returns the updated headers
+   * @param currentCspHeader - The current headers
+   * @param bytes The byte array representing the nonce
+   * @private
+   */
+  private generateNonce(currentCspHeader, bytes) {
 
-    let courtsList = new CourtActions().getCourtsList();
-    /*Should produce something like:
-    {
-      "A": {
-        "Court A": "1",
-        "Court B": "2",
-      },
-      "B": {}
-    }
-    */
+    let cspHeaders = currentCspHeader.split(';');
+    let updatedHeaders = '';
+    cspHeaders.forEach(item => {
+      let itemToAdd = '';
+      if (item.startsWith("script-src")) {
+        itemToAdd = item + ' \'nonce-' + bytes + '\'';
+      } else {
+        itemToAdd = item;
+      }
+
+      if (updatedHeaders === '') {
+        updatedHeaders = itemToAdd;
+      } else {
+        updatedHeaders = updatedHeaders + ';' + itemToAdd;
+      }
+    });
+
+    return updatedHeaders;
+  }
+
+  /**
+   * Generates the court array, that can then be processed by the template
+   * The court array is of the format
+   * {
+   *   "A": {
+   *     "CourtName": {
+   *       "id": 1,
+   *       "hearings": 4
+   *     }
+   *   }
+   * }
+   * The produced list is ordered in alphabetical order
+   * @param courtsList The original court array
+   * @private
+   */
+  private generateCourtArray(courtsList) {
     let alphabetArray = {};
-
     //Firstly creates the array for the possible alphabet options
     for (let i = 0; i < 26; i++) {
       let letter = String.fromCharCode(65 + i)
       alphabetArray[letter] = {};
     }
 
+    courtsList = new InputFilterService().alphabetiseResults(courtsList, 'name')
+
     //Then loop through each court, and add it to the list
     courtsList.forEach(item => {
-      let courtName = item.name as string;
-      alphabetArray[courtName.charAt(0).toUpperCase()][courtName] = item.hearings;
+      if (item.hearings != 0) {
+        let courtName = item.name as string;
+        alphabetArray[courtName.charAt(0).toUpperCase()][courtName] = {
+          id: item.courtId,
+          hearings: item.hearings
+        };
+      }
     })
+    return alphabetArray;
+  }
+
+  public get(req: Request, res: Application) {
+
+    let courtsList = new CourtActions().getCourtsList();
+    let alphabetArray = this.generateCourtArray(courtsList);
 
     const bytes = randomBytes(16).toString('base64')
+    let updatedHeaders = this.generateNonce(res.get("Content-Security-Policy"), bytes)
 
-    res.set("Content-Security-Policy", 'script-src \'nonce-' + bytes + '\'');
+    res.set("Content-Security-Policy", updatedHeaders);
     res.render("court-list", {
       courtList: alphabetArray,
       scriptNonce: bytes
