bd48346fcbec39357f34d56ca68d3db3e2752b9e
commitBlob <maro.radovic12@gmail.com>
2021-08-10 14:10:56 +0100

    pip-569 WIP

--- src/main/controllers/SearchController.ts
+++ src/main/controllers/SearchController.ts
@@ -1,15 +1,25 @@
 import { Request, Response } from 'express';
-import {CourtActions} from '../resources/actions/courtActions';
+import { CourtActions } from '../resources/actions/courtActions';
 
 const courtList = new CourtActions();
+const autocompleteList = courtList.getCourtsList();
+
 export default class SearchController {
   public get(req: Request, res: Response): void {
-    const autocompleteList = courtList.getCourtsList();
-    res.render('search', {autocompleteList: autocompleteList});
+
+    res.render('search', {autocompleteList: autocompleteList, invalidInputError: false, noResultsError: false});
   }
 
   public post(req: Request, res: Response): void {
     const searchInput = req.body['input-autocomplete'];
-    res.redirect(`search-results?search-input=${searchInput}`);
+    if (searchInput) {
+      if (searchInput.length < 3) {
+        res.render('search', {autocompleteList: autocompleteList, invalidInputError: true, noResultsError: false});
+      } else {
+        res.redirect(`search-results?search-input=${searchInput}`);
+      }
+    } else {
+      res.render('search', {autocompleteList: autocompleteList, invalidInputError: false, noResultsError: true});
+    }
   }
 }
