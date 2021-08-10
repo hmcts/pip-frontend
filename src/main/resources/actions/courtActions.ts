514d2e03c05f3d9b3467f9de9c8051f11368e952
Kurt <kurtplaschke@hotmail.co.uk>
2021-08-10 13:58:14 +0100

    fixed linting issues and removed excessive logs (#39)

--- src/main/resources/actions/courtActions.ts
+++ src/main/resources/actions/courtActions.ts
@@ -9,7 +9,6 @@ export class CourtActions {
     const courtsData = JSON.parse(rawData);
     const court = courtsData?.results.filter((court) => court.courtId === courtId);
     if (court.length) {
-      console.log(court[0]);
       return court[0];
     } else {
       console.log(`Court with id ${courtId} does not exist`);
@@ -21,7 +20,6 @@ export class CourtActions {
     const rawData = fs.readFileSync(path.resolve(__dirname, this.mocksPath, 'courtsAndHearingsCount.json'), 'utf-8');
     const courtsData = JSON.parse(rawData);
     if (courtsData?.results) {
-      console.log(courtsData.results);
       return courtsData.results;
     } else {
       console.error('unable to get courts list');
