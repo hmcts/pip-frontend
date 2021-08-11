82c338c3d77a8a120a05ad9ed90516ef20fbfc93
commitBlob <maro.radovic12@gmail.com>
2021-08-11 11:15:35 +0100

    pip-569 styling no results error message

--- src/main/controllers/SearchController.ts
+++ src/main/controllers/SearchController.ts
@@ -6,20 +6,19 @@ const autocompleteList = courtList.getCourtsList();
 
 export default class SearchController {
   public get(req: Request, res: Response): void {
-
-    res.render('search', {autocompleteList: autocompleteList, invalidInputError: false, noResultsError: false});
+    res.render('search', { autocompleteList: autocompleteList, invalidInputError: false, noResultsError: false });
   }
 
   public post(req: Request, res: Response): void {
     const searchInput = req.body['input-autocomplete'];
     if (searchInput) {
       if (searchInput.length < 3) {
-        res.render('search', {autocompleteList: autocompleteList, invalidInputError: true, noResultsError: false});
+        res.render('search', { autocompleteList: autocompleteList, invalidInputError: true, noResultsError: false });
       } else {
         res.redirect(`search-results?search-input=${searchInput}`);
       }
     } else {
-      res.render('search', {autocompleteList: autocompleteList, invalidInputError: false, noResultsError: true});
+      res.render('search', { autocompleteList: autocompleteList, invalidInputError: false, noResultsError: true});
     }
   }
 }
