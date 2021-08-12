889d82cd2ff1bf7e02e145d23e9ac72c46916fca
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2021-08-12 11:26:14 +0100

    PUB-507 - Fixed linting on test

--- src/test/unit/assets/back-to-top.js.ts
+++ src/test/unit/assets/back-to-top.js.ts
@@ -5,71 +5,75 @@ describe('Testing back to top button', () => {
   afterEach(() => {
     jest.resetModules();
     sinon.restore();
-  })
+  });
 
   let interactionObserver;
 
   beforeAll(() => {
     interactionObserver = class IntersectionObserver {
-      constructor() {}
+      constructor() {
+        // do nothing
+      }
 
-      observe() {
+      observe(): any {
         return null;
       }
     };
-  })
+  });
 
   it('should remove the app-back-to-top--hidden property, when the InteractionObserver is not enabled', async () => {
 
-    let mockElement = document.createElement('div');
-    mockElement.classList.add('app-back-to-top--hidden')
+    const mockElement = document.createElement('div');
+    mockElement.classList.add('app-back-to-top--hidden');
 
     sinon.stub(document, 'querySelector').withArgs('#back-to-top-button').returns(mockElement);
 
     await import('../../../main/assets/js/back-to-top');
 
     expect(mockElement.classList.length).toEqual(0);
-  })
+  });
 
   it('should keep the app-back-to-top--hidden property, when the InteractionObserver is enabled', async () => {
 
-    let mockElement = document.createElement('div');
+    const mockElement = document.createElement('div');
     mockElement.classList.add('app-back-to-top--hidden');
 
-    (<any> window).IntersectionObserver = () => {};
+    (window as any).IntersectionObserver = (): void => {
+      // do nothing
+    };
 
     const stub = sinon.stub(document, 'querySelector');
     stub.withArgs('#back-to-top-button').returns(mockElement);
     stub.withArgs('.govuk-footer').returns(null);
 
-    let interactionStub = sinon.spy(interactionObserver.prototype, 'observe');
+    const interactionStub = sinon.spy(interactionObserver.prototype, 'observe');
 
     await import('../../../main/assets/js/back-to-top');
 
     expect(mockElement.classList.length).toEqual(1);
     expect(interactionStub.callCount).toEqual(0);
-  })
+  });
 
   it('should call the observe function, when the footer exists and the InteractionObserver is enabled', async () => {
 
-    let mockElement = document.createElement('div');
+    const mockElement = document.createElement('div');
     mockElement.classList.add('app-back-to-top--hidden');
 
-    let mockFooter = document.createElement('footer');
+    const mockFooter = document.createElement('footer');
     mockFooter.classList.add('.govuk-footer');
 
-    (<any> window).IntersectionObserver = interactionObserver;
+    (window as any).IntersectionObserver = interactionObserver;
 
     const stub = sinon.stub(document, 'querySelector');
     stub.withArgs('#back-to-top-button').returns(mockElement);
     stub.withArgs('.govuk-footer').returns(mockFooter);
 
-    let interactionStub = sinon.spy(interactionObserver.prototype, 'observe');
+    const interactionStub = sinon.spy(interactionObserver.prototype, 'observe');
 
     await import('../../../main/assets/js/back-to-top');
 
     expect(mockElement.classList.length).toEqual(1);
     expect(interactionStub.callCount).toEqual(1);
-  })
+  });
 
 });
