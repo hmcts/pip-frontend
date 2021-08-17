a18ffa4b519d2f7fcd6225adc5cde51b9b9b8563
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2021-08-17 16:10:13 +0100

    Updated so that Back to top works in IE11, and switched functionality to typescript

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const sourcePath = path.resolve(__dirname, 'src/main/');
 const govukFrontend = require(path.resolve(__dirname, 'webpack/govukFrontend'));
 const scss = require(path.resolve(__dirname,'webpack/scss'));
 const HtmlWebpack = require(path.resolve(__dirname,'webpack/htmlWebpack'));
-const customScripts = require(path.resolve(__dirname,'webpack/customScripts'));
 const autocomplete = require(path.resolve(__dirname,'webpack/accessible-autocomplete'));
 
 const devMode = process.env.NODE_ENV !== 'production';
@@ -12,11 +11,24 @@ const fileNameSuffix = devMode ? '-dev' : '.[contenthash]';
 const filename = `[name]${fileNameSuffix}.js`;
 
 module.exports = {
-  plugins: [...govukFrontend.plugins, ...scss.plugins, ...HtmlWebpack.plugins, ...customScripts.plugins, ...autocomplete.plugins ],
-  entry: path.resolve(sourcePath, 'index.js') ,
+  plugins: [...govukFrontend.plugins, ...scss.plugins, ...HtmlWebpack.plugins, ...autocomplete.plugins ],
+  entry: {
+    main: path.resolve(sourcePath, 'index.js'),
+    alphabetical: path.resolve(sourcePath, 'alphabetical.ts' ),
+  } ,
   mode: devMode ? 'development': 'production',
+
   module: {
-    rules: [...scss.rules],
+    rules: [
+      ...scss.rules,
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      }],
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
   },
   output: {
     path: path.resolve(__dirname, 'src/main/public/'),
