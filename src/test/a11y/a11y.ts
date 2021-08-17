057c5b07de217d663e1bbd74bc2bba58baa70a1e
commitBlob <maro.radovic12@gmail.com>
2021-08-17 10:37:51 +0100

    pip-569 - add accessibility tests for search error views

--- src/test/a11y/a11y.ts
+++ src/test/a11y/a11y.ts
@@ -15,13 +15,13 @@ const routesNotTested = [
   'info.ts',
 ];
 
-class Pa11yResult {
+export class Pa11yResult {
   documentTitle: string;
   pageUrl: string;
   issues: PallyIssue[];
 }
 
-class PallyIssue {
+export class PallyIssue {
   code: string;
   context: string;
   message: string;
@@ -34,7 +34,7 @@ beforeAll((done /* call it or remove it*/) => {
   done(); // calling it
 });
 
-function ensurePageCallWillSucceed(url: string): Promise<void> {
+export function ensurePageCallWillSucceed(url: string): Promise<void> {
   return agent.get(url).then((res: supertest.Response) => {
     if (res.redirect) {
       throw new Error(
@@ -47,13 +47,13 @@ function ensurePageCallWillSucceed(url: string): Promise<void> {
   });
 }
 
-function runPally(url: string): Pa11yResult {
+export function runPally(url: string): Pa11yResult {
   return pa11y(url, {
     hideElements: '.govuk-footer__licence-logo, .govuk-header__logotype-crown',
   });
 }
 
-function expectNoErrors(messages: PallyIssue[]): void {
+export function expectNoErrors(messages: PallyIssue[]): void {
   const errors = messages.filter(m => m.type === 'error');
 
   if (errors.length > 0) {
