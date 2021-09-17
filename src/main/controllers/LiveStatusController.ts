4760c7da6aa7d015761650f86c36cdc7412581fe
commitBlob <maro.radovic12@gmail.com>
2021-09-17 13:49:49 +0100

    pub-720: add 15 seconds refresh timer

--- src/main/controllers/LiveStatusController.ts
+++ src/main/controllers/LiveStatusController.ts
@@ -5,6 +5,7 @@ import moment from 'moment';
 export default class LiveStatusController {
   public get(req: Request, res: Response): void {
     const courtId = req.query.courtId as string;
+    const timerMilliseconds = process.env.REFRESH_TIME_MILLISECONDS ? process.env.REFRESH_TIME_MILLISECONDS : 15000;
 
     if (courtId !== undefined) {
       const liveCases = new LiveHearingsActions().getLiveCases(parseInt(courtId));
@@ -13,6 +14,7 @@ export default class LiveStatusController {
           courtName: liveCases.courtName,
           updateDateTime: moment.unix(liveCases.lastUpdated).format('MMMM Do YYYY, h:mm a'),
           liveHearings: liveCases.courtUpdates,
+          refreshTimer: timerMilliseconds,
         });
       } else {
         res.redirect('not-found');
