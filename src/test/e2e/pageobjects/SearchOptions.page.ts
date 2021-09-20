ff71048ad6fe7cb423fe21ceb716ba65ad2502c9
Matthew Summerbell <79512635+Matthew-Summerbell@users.noreply.github.com>
2021-09-20 10:52:51 +0100

    Pub 695 (#62)
    * initial setup and tests for webdriver
    
    * further config changes and added test
    
    * updated configuration, added more tests, refactored naming for improved readability
    
    * added tests, cleaned up code
    
    * fixed import quotes
    
    * refactored pageTitle method to getPageTitle method, updated to hit https, added await to enter text
    
    * removed jest puppeteer config
    
    * removed hardcoded url

---
+++ src/test/e2e/pageobjects/SearchOptions.page.ts
@@ -0,0 +1,56 @@
+import { AlphabeticalSearchPage } from './AlphabeticalSearch.page';
+import { SearchPage } from './Search.page';
+
+const helpers = require('../Helpers/Selectors');
+
+export class SearchOptionsPage {
+
+  async getPageTitle(): Promise<string> {
+    $(helpers.SearchOptionsTitle).catch(() => {
+      console.log(`${helpers.SearchOptionsTitle} not found`);
+    });
+
+    return $(helpers.SearchOptionsTitle).getText();
+  }
+
+  get radioButtons(): Promise<number> {
+    const radioButtons = $$(helpers.RadioButton);
+    return radioButtons.length;
+  }
+
+  async selectSearchRadio(): Promise<void> {
+    $(helpers.SearchRadioButton).catch(() => {
+      console.log(`${helpers.SearchRadioButton} not found`);
+    });
+    const radioButton = await $(helpers.SearchRadioButton);
+    radioButton.click();
+  }
+
+  async selectFindRadio(): Promise<void> {
+    $(helpers.FindRadioButton).catch(() => {
+      console.log(`${helpers.FindRadioButton} not found`);
+    });
+    const radioButton = await $(helpers.FindRadioButton);
+    radioButton.click();
+  }
+
+  async clickContinueForSearch(): Promise<SearchPage> {
+    $(helpers.ContinueButton).catch(() => {
+      console.log(`${helpers.ContinueButton} not found`);
+    });
+    const continueButton = await $(helpers.ContinueButton);
+    continueButton.click();
+
+    return new SearchPage();
+  }
+
+  async clickContinueForAlphabetical(): Promise<AlphabeticalSearchPage> {
+    $(helpers.ContinueButton).catch(() => {
+      console.log(`${helpers.ContinueButton} not found`);
+    });
+    const continueButton = await $(helpers.ContinueButton);
+    continueButton.click();
+
+    return new AlphabeticalSearchPage();
+  }
+}
---
+++ src/test/e2e/tests/specs.test.ts
@@ -0,0 +1,112 @@
+import { HomePage } from '../pageobjects/Home.page';
+import { SearchOptionsPage } from '../pageobjects/SearchOptions.page';
+import { AlphabeticalSearchPage } from '../pageobjects/AlphabeticalSearch.page';
+import { HearingListPage } from '../pageobjects/HearingList.page';
+import { SearchPage } from '../pageobjects/Search.page';
+import { SearchResultsPage } from '../pageobjects/SearchResults.page';
+import { OtpLoginPage } from '../pageobjects/OtpLogin.page';
+import { SubscriptionManagementPage } from '../pageobjects/SubscriptionManagement.page';
+
+const homePage = new HomePage;
+const otpLoginPage = new OtpLoginPage();
+let searchOptionsPage: SearchOptionsPage;
+let alphabeticalSearchPage: AlphabeticalSearchPage;
+let hearingListPage: HearingListPage;
+let searchPage: SearchPage;
+let searchResultsPage: SearchResultsPage;
+let subscriptionManagementPage: SubscriptionManagementPage;
+
+describe('Finding a court or tribunal listing', () => {
+  it('should open main page with "Find a court or tribunal listing title', async () => {
+    await homePage.open('');
+    expect(await homePage.getPageTitle()).toEqual('Find a court or tribunal listing');
+  });
+
+  it('should click on the "Start now button and navigate to Search Options page', async () => {
+    searchOptionsPage = await homePage.clickStartNowButton();
+    expect(await searchOptionsPage.getPageTitle()).toEqual('Find a court or tribunal list');
+  });
+
+  it('should see both radio buttons', async () => {
+    expect(await searchOptionsPage.radioButtons).toBe(2);
+  });
+
+  describe('Following the \'find\' path', () => {
+    after(async () => {
+      await homePage.open('');
+      searchOptionsPage = await homePage.clickStartNowButton();
+    });
+
+    it('should select \'find\' option and navigate to alphabetical search page', async() => {
+      await searchOptionsPage.selectFindRadio();
+      alphabeticalSearchPage = await searchOptionsPage.clickContinueForAlphabetical();
+      expect(await alphabeticalSearchPage.getPageTitle()).toEqual('Find a court or tribunal listing');
+    });
+
+    it('should select \'Z\' option, and navigate to the end of the page', async() => {
+      const endLetter = 'Z';
+      await alphabeticalSearchPage.selectLetter(endLetter);
+      expect(await alphabeticalSearchPage.checkIfLetterIsVisible('Z')).toBeTruthy();
+    });
+
+    it('selecting back to top should navigate to the top of the page', async() => {
+      const startLetter = 'A';
+      await alphabeticalSearchPage.selectBackToTop();
+      expect(await alphabeticalSearchPage.checkIfLetterIsVisible(startLetter)).toBeTruthy();
+    });
+
+    it('selecting first result should take you to to the hearings list page', async() => {
+      hearingListPage = await alphabeticalSearchPage.selectFirstListResult();
+      expect(await hearingListPage.getPageTitle()).toEqual('Albertville Court hearing list');
+    });
+
+    it('should display 1 result', async() => {
+      expect(await hearingListPage.getResults()).toBe(1);
+    });
+  });
+
+  describe('Following the \'search\' path', () => {
+    const searchTerm = 'aylesbury';
+    const expectedNumOfResults = 2;
+    const expectedNumOfHearings = 3;
+
+    it('should select \'search\' option and navigate to search page', async() => {
+      await searchOptionsPage.selectSearchRadio();
+      searchPage = await searchOptionsPage.clickContinueForSearch();
+      expect(await searchPage.getPageTitle()).toEqual('What court or tribunal are you interested in?');
+    });
+
+    it('should enter text and click continue', async() => {
+      await searchPage.enterText(searchTerm);
+      searchResultsPage = await searchPage.clickContinue();
+      expect(await searchResultsPage.getPageTitle()).toEqual(`Courts or tribunals in ${searchTerm}`);
+    });
+
+    it(`should display ${expectedNumOfResults} results`, async() => {
+      expect(await searchResultsPage.getResults()).toBe(2);
+    });
+
+    it('should navigate to hearing list page', async() => {
+      hearingListPage = await searchResultsPage.selectCourt();
+      expect(await hearingListPage.getPageTitle()).toEqual('Aylesbury Crown Court hearing list');
+    });
+
+    it(`should display ${expectedNumOfHearings} results`, async() => {
+      expect(await hearingListPage.getResults()).toBe(3);
+    });
+  });
+
+  describe('Media User Login', () => {
+    it('should open the OTP login page', async () => {
+      await otpLoginPage.open('otp-login');
+      expect(await otpLoginPage.getPageTitle()).toBe('Verify your email address');
+    });
+
+    it('should navigate to subscription page when correct passcode is entered', async () => {
+      await otpLoginPage.enterText('222222');
+      subscriptionManagementPage = await otpLoginPage.clickContinue();
+      expect(await subscriptionManagementPage.getPageTitle()).toEqual('Subscription Management');
+    });
+  });
+});
+
