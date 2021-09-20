cad8edc439588bc38be1d3f087b679e911acb67c
commitBlob <maro.radovic12@gmail.com>
2021-09-20 14:44:25 +0100

    pub-720: refactor e2e

---
+++ src/test/e2e/PageObjects/LiveCaseStatus.page.ts
@@ -0,0 +1,19 @@
+const helpers = require('../Helpers/Selectors');
+
+export class LiveCaseStatusPage {
+  async getPageTitle(): Promise<string> {
+    $(helpers.CommonPageTitle).catch(() => {
+      console.log(`${helpers.CommonPageTitle} not found`);
+    });
+
+    return $(helpers.CommonPageTitle).getText();
+  }
+
+  async getResults(): Promise<number> {
+    $(helpers.Results).catch(() => {
+      console.log(`${helpers.Results} not found`);
+    });
+    const results = $$(helpers.Results);
+    return results.length;
+  }
+}
