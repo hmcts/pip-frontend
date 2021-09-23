5b1bcd1764942d0b919197a24be9e39f1ce3242f
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2021-09-23 13:52:07 +0100

    removed unused script data,

--- src/test/unit/controllers/StatusDescriptionController.test.ts
+++ src/test/unit/controllers/StatusDescriptionController.test.ts
@@ -3,12 +3,12 @@ import { Request, Response } from 'express';
 import StatusDescriptionController from '../../../main/controllers/StatusDescriptionController';
 
 describe('Status Description Controller', () => {
-  it('should render the alphabetical search page', () =>  {
+  it('should render the status descroiption page', () =>  {
     const statusDescriptionController = new StatusDescriptionController();
 
     const response = {
       render: function() {return '';},
-      get: function() {return 'test test;script-src test';},
+      get: function() {return '';},
       set: function() {return '';},
     } as unknown as Response;
     const request = {query: {}} as unknown as Request;
