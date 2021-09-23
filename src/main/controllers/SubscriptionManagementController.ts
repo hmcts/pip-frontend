76ea28cbc8068b7f3fdf93ceca86fda16be114ba
commitBlob <maro.radovic12@gmail.com>
2021-09-23 15:28:41 +0100

    pub-744: get subscriptions and display them on the page

--- src/main/controllers/SubscriptionManagementController.ts
+++ src/main/controllers/SubscriptionManagementController.ts
@@ -1,7 +1,12 @@
 import { Request, Response } from 'express';
+import { SubscriptionActions } from '../resources/actions/subscriptionActions';
+
 
 export default class SubscriptionManagementController {
   public get(req: Request, res: Response): void {
-    res.render('subscription-management');
+    const subscriptionActions = new SubscriptionActions();
+    const caseTableData = subscriptionActions.generateCaseTableRows();
+    const courtTableData = subscriptionActions.generateCourtTableRows();
+    res.render('subscription-management', {caseTableData, courtTableData});
   }
 }
---
+++ src/main/resources/actions/subscriptionActions.ts
@@ -0,0 +1,73 @@
+import fs from 'fs';
+import path from 'path';
+import moment from 'moment';
+
+export class SubscriptionActions {
+  mocksPath = '../mocks/';
+  rawData = fs.readFileSync(path.resolve(__dirname, this.mocksPath, 'userSubscriptions.json'), 'utf-8');
+
+  getUserSubscriptions(userId: number): any {
+    const subscriptionsData = JSON.parse(this.rawData);
+    const userSubscription = subscriptionsData?.results.filter((user) => user.userId === userId);
+    if (userSubscription.length) {
+      return userSubscription[0];
+    } else {
+      console.log(`User with id ${userId} does not exist`);
+      return null;
+    }
+  }
+
+  generateCaseTableRows(): any[] {
+    const userSubscriptions = this.getUserSubscriptions(1);
+    const caseRows = [];
+    const caseSubscriptions = userSubscriptions.caseSubscriptions;
+    if (caseSubscriptions.length) {
+      caseSubscriptions.forEach((subscription) => {
+        caseRows.push(
+          [
+            {
+              text: subscription.name,
+              classes: 'govuk-!-width-one-third',
+            },
+            {
+              text: subscription.reference,
+              classes: 'govuk-!-width-one-third',
+            },
+            {
+              text: moment.unix(subscription.dateAdded).format('D MMM YYYY'),
+            },
+            {
+              html: '<a href=\'#\'>Unsubscribe</a>',
+              format: 'numeric',
+            },
+          ],
+        );
+      });
+    }
+    return caseRows;
+  }
+
+  generateCourtTableRows(): any[] {
+    const userSubscriptions = this.getUserSubscriptions(1);
+    const courtRows = [];
+    const courtSubscriptions = userSubscriptions.courtSubscriptions;
+    if (courtSubscriptions.length) {
+      courtSubscriptions.forEach((subscription) => {
+        courtRows.push([
+          {
+            text: subscription.name,
+            classes: 'govuk-!-width-two-thirds',
+          },
+          {
+            text: moment.unix(subscription.dateAdded).format('D MMM YYYY'),
+          },
+          {
+            html: '<a href=\'#\'>Unsubscribe</a>',
+            format: 'numeric',
+          },
+        ]);
+      });
+    }
+    return courtRows;
+  }
+}
