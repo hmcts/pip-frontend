5436e0dd949b2c509dec564fa4fb0aad761a8d91
commitBlob <maro.radovic12@gmail.com>
2021-09-24 11:59:44 +0100

    pub-744: "tabs" data switching

--- src/main/controllers/SubscriptionManagementController.ts
+++ src/main/controllers/SubscriptionManagementController.ts
@@ -1,12 +1,34 @@
 import { Request, Response } from 'express';
 import { SubscriptionActions } from '../resources/actions/subscriptionActions';
 
-
 export default class SubscriptionManagementController {
   public get(req: Request, res: Response): void {
     const subscriptionActions = new SubscriptionActions();
     const caseTableData = subscriptionActions.generateCaseTableRows();
     const courtTableData = subscriptionActions.generateCourtTableRows();
-    res.render('subscription-management', {caseTableData, courtTableData});
+    let activeAllTab, activeCaseTab, activeCourtTab;
+    switch (Object.keys(req.query)[0]) {
+      case 'all':
+        activeAllTab = true;
+        activeCaseTab = false;
+        activeCourtTab = false;
+        break;
+      case 'case':
+        activeAllTab = false;
+        activeCaseTab = true;
+        activeCourtTab = false;
+        break;
+      case 'court':
+        activeAllTab = false;
+        activeCaseTab = false;
+        activeCourtTab = true;
+        break;
+      default:
+        activeAllTab = true;
+        activeCaseTab = false;
+        activeCourtTab = false;
+        break;
+    }
+    res.render('subscription-management', {caseTableData, courtTableData, activeAllTab, activeCaseTab, activeCourtTab});
   }
 }
