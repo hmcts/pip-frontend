b89e589f9453cbeaabbaacedee1015d4c8a22836
commitBlob <maro.radovic12@gmail.com>
2021-09-27 15:17:43 +0100

    pub-744: remove false sets

--- src/main/controllers/SubscriptionManagementController.ts
+++ src/main/controllers/SubscriptionManagementController.ts
@@ -6,27 +6,19 @@ export default class SubscriptionManagementController {
     const subscriptionActions = new SubscriptionActions();
     const caseTableData = subscriptionActions.generateCaseTableRows();
     const courtTableData = subscriptionActions.generateCourtTableRows();
-    let activeAllTab, activeCaseTab, activeCourtTab;
+    let activeAllTab, activeCaseTab, activeCourtTab = false;
     switch (Object.keys(req.query)[0]) {
       case 'all':
         activeAllTab = true;
-        activeCaseTab = false;
-        activeCourtTab = false;
         break;
       case 'case':
-        activeAllTab = false;
         activeCaseTab = true;
-        activeCourtTab = false;
         break;
       case 'court':
-        activeAllTab = false;
-        activeCaseTab = false;
         activeCourtTab = true;
         break;
       default:
         activeAllTab = true;
-        activeCaseTab = false;
-        activeCourtTab = false;
         break;
     }
     res.render('subscription-management', {caseTableData, courtTableData, activeAllTab, activeCaseTab, activeCourtTab});
