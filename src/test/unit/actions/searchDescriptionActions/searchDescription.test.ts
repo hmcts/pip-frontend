e70adb48bc66164dc6ff326246ef1520d2dd6d21
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2021-09-28 12:16:01 +0100

    Add more test as part of code review

--- src/test/unit/actions/searchDescriptionActions/searchDescription.test.ts
+++ src/test/unit/actions/searchDescriptionActions/searchDescription.test.ts
@@ -9,3 +9,30 @@ describe('getStatusDescriptionList()', () => {
     expect(results.length).toBe(49);
   });
 });
+
+describe('First glossary should be Adjourned', () => {
+  const results = searchDescriptionActions.getStatusDescriptionList();
+
+  it('First glossary should be Adjourned', () => {
+    expect(results[0].name).toEqual('Adjourned');
+  });
+});
+
+describe('Description fof First glossary must not be empty', () => {
+  const results = searchDescriptionActions.getStatusDescriptionList();
+
+  it('Description fof First glossary must not be empty', () => {
+    expect(results[0].description).not.toBeNull();
+  });
+});
+
+describe('All Glossary items must have name and description', () => {
+  const results = searchDescriptionActions.getStatusDescriptionList();
+
+  it('All Glossary items must have name and description', () => {
+    for (let i = 0; i < 49; i++) {
+      expect(results[i].name).not.toBeNull();
+      expect(results[i].description).not.toBeNull();
+    }
+  });
+});
