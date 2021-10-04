adb16b6e72c1820526f2a3c6f70df494c6865aa1
marcogagliardi <marco.gagliardi@justice.gov.uk>
2021-10-04 15:35:05 +0100

    pip-700-676 - deleted 735 merge and recreated branch

---
+++ src/test/unit/views/single-justice-procedure-search.test.ts
@@ -0,0 +1,21 @@
+import {expect} from 'chai';
+import request from 'supertest';
+
+import {app} from '../../../main/app';
+
+const PAGE_URL = '/single-justice-procedure-search';
+
+let htmlRes: Document;
+
+describe('Single Justice Procedure Search Page', () => {
+  beforeAll(async () => {
+    await request(app).get(PAGE_URL).then(res => {
+      htmlRes = new DOMParser().parseFromString(res.text, 'text/html');
+    });
+  });
+
+  it('should display header', () => {
+    const header = htmlRes.getElementsByClassName('govuk-heading-l');
+    expect(header[0].innerHTML).contains('Single Justice Procedure list', 'Could not find correct value in header');
+  });
+});
