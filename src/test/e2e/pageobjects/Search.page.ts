dfabaf8232b96f9f74a75a733c05949a8260086c
marcogagliardi <marco.gagliardi@justice.gov.uk>
2021-10-18 10:29:23 +0100

    pub-764

--- src/test/e2e/pageobjects/Search.page.ts
+++ src/test/e2e/pageobjects/Search.page.ts
@@ -5,6 +5,14 @@ const helpers = require('../Helpers/Selectors');
 
 export class SearchPage extends PageBase {
 
+  async getPageTitle(): Promise<string> {
+    $(helpers.SearchTitle).catch(() => {
+      console.log(`${helpers.SearchTitle} not found`);
+    });
+
+    return $(helpers.SearchTitle).getText();
+  }
+
   async enterText(text: string): Promise<void> {
     $(helpers.SearchInput).catch(() => {
       console.log(`${helpers.SearchInput} not found`);
