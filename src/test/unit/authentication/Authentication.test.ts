2b0a88e4e031e746f8c0c67226d5c41410385339
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2021-10-18 17:25:29 +0100

    PUB-748 - Added further testing of authentication ts

--- src/test/unit/authentication/Authentication.test.ts
+++ src/test/unit/authentication/Authentication.test.ts
@@ -36,4 +36,42 @@ describe('Authentication', () => {
     expect(passport._strategies).to.have.property('azuread-openidconnect');
   });
 
+  it('Test that a new user is added for azure authentication', () => {
+    const passport = require('passport');
+    authentication('true');
+
+    const strategy = passport._strategies['azuread-openidconnect'];
+    const verifyFunction = strategy._verify;
+
+    const profile = {oid: '1234', profile: 'test-profile'};
+    const mockCallback = jest.fn();
+
+    verifyFunction(null, null, profile, null, null, mockCallback);
+
+    expect(mockCallback.mock.calls.length).to.eql(1);
+    expect(mockCallback.mock.calls[0][0]).to.eql(null);
+    expect(mockCallback.mock.calls[0][1]).to.eql(profile);
+  });
+
+  it('Test that if an existing user is found, then that user is returned', () => {
+    const passport = require('passport');
+    authentication('true');
+
+    const strategy = passport._strategies['azuread-openidconnect'];
+    const verifyFunction = strategy._verify;
+
+    const firstProfile = {oid: '1234', profile: 'test-profile'};
+    const mockCallback = jest.fn();
+
+    verifyFunction(null, null, firstProfile, null, null, mockCallback);
+
+    const secondProfile = {oid: '1234', profile: 'test-profile2'};
+
+    verifyFunction(null, null, secondProfile, null, null, mockCallback);
+
+    expect(mockCallback.mock.calls.length).to.eql(2);
+    expect(mockCallback.mock.calls[1][0]).to.eql(null);
+    expect(mockCallback.mock.calls[1][1]).to.eql(firstProfile);
+  });
+
 });
