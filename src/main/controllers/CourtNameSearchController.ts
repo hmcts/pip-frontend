4ab35dce0f6be5c7db1d4fd00c3824c23f45633f
commitBlob <maro.radovic12@gmail.com>
2021-10-20 11:34:59 +0100

    pub-736: add filter checkboxes logic

--- src/main/controllers/CourtNameSearchController.ts
+++ src/main/controllers/CourtNameSearchController.ts
@@ -7,19 +7,27 @@ import { CourtService } from '../service/courtService';
 let _api: PipApi;
 let courtActions;
 const filterService = new FilterService();
+let checkedRegionsFilter;
+let checkedJurisdictionsFilter;
 
 export default class CourtNameSearchController {
   constructor(private readonly api: PipApi) {
     _api = this.api;
     courtActions = new CourtActions(_api);
+    checkedJurisdictionsFilter = [];
+    checkedRegionsFilter = [];
   }
   
   public async get(req: Request, res: Response): Promise<void> {
+    if (req.query['clear'] === 'all') {
+      checkedJurisdictionsFilter = [];
+      checkedRegionsFilter = [];
+    }
     const alphabeticalCourts = await new CourtService(_api).generateCourtsAlphabetObject();
     const jurisdictionsList = await courtActions.getJurisdictionList();
     const regionsList = await courtActions.getRegionsList();
-    const regionCheckboxes = filterService.generateCheckboxObjects(regionsList, [1]);
-    const jurisdictionCheckboxes = filterService.generateCheckboxObjects(jurisdictionsList, []);
+    const regionCheckboxes = filterService.generateCheckboxObjects(regionsList, checkedRegionsFilter);
+    const jurisdictionCheckboxes = filterService.generateCheckboxObjects(jurisdictionsList, checkedJurisdictionsFilter);
     const checkBoxesComponents = [
       filterService.generateCheckboxGroup(jurisdictionCheckboxes, 'Jurisdiction'),
       filterService.generateCheckboxGroup(regionCheckboxes, 'Region'),
@@ -28,8 +36,22 @@ export default class CourtNameSearchController {
   }
 
   public async post(req: Request, res: Response): Promise<void> {
-    console.log(req.body);
-    const courtsList = await courtActions.getCourtsList();
-    res.render('court-name-search', {courtsList});
+    if (req.body.jurisdiction) {
+      // Convert strings array to numbers '1' => 1
+      checkedJurisdictionsFilter = req.body.jurisdiction.map(Number);
+    }
+    if (req.body.region) {
+      checkedRegionsFilter = req.body.region.map(Number);
+    }
+    const alphabeticalCourts = await new CourtService(_api).generateCourtsAlphabetObject();
+    const jurisdictionsList = await courtActions.getJurisdictionList();
+    const regionsList = await courtActions.getRegionsList();
+    const regionCheckboxes = filterService.generateCheckboxObjects(regionsList, checkedRegionsFilter);
+    const jurisdictionCheckboxes = filterService.generateCheckboxObjects(jurisdictionsList, checkedJurisdictionsFilter);
+    const checkBoxesComponents = [
+      filterService.generateCheckboxGroup(jurisdictionCheckboxes, 'Jurisdiction'),
+      filterService.generateCheckboxGroup(regionCheckboxes, 'Region'),
+    ];
+    res.render('court-name-search', {alphabeticalCourts, checkBoxesComponents});
   }
 }
