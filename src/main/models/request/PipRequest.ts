4e78a702c3f8c3cc9ff7a9ecf1253633eb4bd336
commitBlob <maro.radovic12@gmail.com>
2021-10-26 16:53:16 +0100

    PUB-733: add translations

--- src/main/models/request/PipRequest.ts
+++ src/main/models/request/PipRequest.ts
@@ -20,6 +20,7 @@ export interface PipRequest extends Request {
       'view-option': {};
       'single-justice-procedure-search': {};
       'subscription-add': {};
+      'court-name-search': {};
     };
   };
   lng?: string;
