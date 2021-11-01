217480035b8911431ecfbb306000ce3bed3c9abe
Maro <maro.radovic12@gmail.com>
2021-11-01 16:12:33 +0000

    PUB-736: Build Subscriptions Case Name Search (#114)
    * pub-736: add case name search view
    
    * pub-744: search results WIP
    
    * pub-736: roll-back local command
    
    * pub-736: update back link
    
    * pub-736: search and results pages
    
    * pub-736: revert local command
    
    * pub-736: do not check case number field against search query
    
    * pub-736: remove unnecessary script imports
    
    * pub-736: render error page instead of redirect
    
    * pub-736: add labels for accessibility tests
    
    * pub-736: import moj dependencies
    
    * pub-736: use label instead of hint
    
    * pub-736: convert search query to lowercase
    
    * pub-736: wrap table into form and pass data into other page
    
    * pub-736: e2e WIP
    
    * pub-736: update e2e tests
    
    * pub-736: case-name-search routes test
    
    * update add new subscription button
    
    * redirect case name search selection to proper page
    
    * refactor test cases to match proper e2e path
    
    * pub-736: remove redundant declaration
    
    * pub-736: add route tests
    
    * pub-736: WIP
    
    * pub-736: add actions unit tests
    
    * pub-736: add controller unit tests
    
    * pub-736: add utils unit tests
    
    * pub-736: add view unit tests
    
    * pub-736: additional unit tests to improve coverage
    
    * pub-736: add LocalApiController test for apiFindHearings
    
    * pub-736: refactor two inputs into one and render depending on the noResultsError boolean
    
    * pub-736: import javascript for menu toggle
    
    * pub-736: amend breakpoints
    
    * pub-736: add a11y error states tests
    
    * pub-736: add translations
    
    * pub-736: apply translations
    
    * pub-736: remove whitespaces
    
    * PUB-736: refactor CaseNameSearchController
    
    * PUB-736: refactor controller tests
    
    * PUB-736: add service and request tests
    
    * PUB-736: fix - display additional error messages
    
    * PUB-736: refactor view unit test
    
    * PUB-736: mock data in unit test
    
    * PUB-736: update tests
    
    * PUB-736: update e2e tests
    
    * PUB-736: use macro back button
    
    * PUB-736: a11y mock requests, remove commented out code
    
    * PUB-736: remove actions

---
+++ src/test/a11y/case-name-search.test.ts
@@ -0,0 +1,32 @@
+import * as supertest from 'supertest';
+import sinon from 'sinon';
+import { app } from '../../main/app';
+import { ensurePageCallWillSucceed, expectNoErrors, Pa11yResult, runPally } from './a11y';
+import { HearingService } from '../../main/service/hearingService';
+const agent = supertest.agent(app);
+
+const URL = '/case-name-search';
+
+const stub = sinon.stub(HearingService.prototype, 'getHearingsByCaseName');
+stub.withArgs('').returns([]);
+stub.withArgs('foo').returns([]);
+
+describe('Accessibility Case Name Search Page Error States',  () => {
+  test('should have no accessibility errors for no input data', done => {
+    ensurePageCallWillSucceed(URL)
+      .then(() => runPally(agent.post(URL).send({'case-name': ''}).url))
+      .then((result: Pa11yResult) => {
+        expectNoErrors(result.issues);
+        done();
+      }).catch((err: Error) => done(err));
+  });
+
+  test('should have no accessibility errors for invalid input data', done => {
+    ensurePageCallWillSucceed(URL)
+      .then(() => runPally(agent.post(URL).send({'case-name': 'foo'}).url))
+      .then((result: Pa11yResult) => {
+        expectNoErrors(result.issues);
+        done();
+      }).catch((err: Error) => done(err));
+  });
+});
