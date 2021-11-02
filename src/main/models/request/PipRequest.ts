8071dbdff3d6d8be04e0b93d553e7b529d6259da
junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>
2021-11-02 10:06:54 +0000

    Pub 721 latest master and use mock data (#133) - WILL BE REVERTED
    * Pub-720:  Live case status updates online - screen 5 UI
    
    * fix linting errors
    
    * fix the build and code review items.
    
    * add the unit test for code coverage
    
    * Update src/main/views/status-description.njk
    
    Co-authored-by: Kurt <kurtplaschke@hotmail.co.uk>
    
    * Add tests for View
    
    * change back link as per code review
    
    * Fixed the test
    
    * Refactor code to move code from controller to service
    
    * add status description service tests
    
    * removed unused script data,
    
    * Changed the test on base of code review feedback
    
    * remove custom styling and add govuk styling
    
    * remove unused css class.
    
    * refactor the code to reuse the function already available
    
    * Add more test as part of code review
    
    * fix type
    
    * Change the code to use local api
    
    * Fix all tests to use local api
    
    * fix bug
    
    * fix accessibility bug
    
    * Fix the deployment
    
    * Fix the deployment
    
    * Make status Id consistent
    
    * Fix the styling issue
    
    * fix Accessibility test
    
    * remove unnecessary set
    
    * check deployment
    
    * add code review comment
    
    * Stretching the content across the page
    
    * Fix the bug and add more test
    
    Fix the bug raised by QA team and also add test to check the link ID for the event status
    
    * Fix bug raised by QA team
    
    * call backend api from frontend
    
    * Fix all the unit tests
    
    * fix build
    
    * fix sona code coverage
    
    * fix sonarqube issue
    
    * update the endpoint
    
    * Fix the tests
    
    * Get data from mock and add localisation
    
    * fix build
    
    * fix code coverage
    
    Co-authored-by: Kurt <kurtplaschke@hotmail.co.uk>
    Co-authored-by: John O'Neill <johnoneill546@gmail.com>

--- src/main/models/request/PipRequest.ts
+++ src/main/models/request/PipRequest.ts
@@ -19,6 +19,7 @@ export interface PipRequest extends Request {
       'subscription-management': {};
       'view-option': {};
       'single-justice-procedure-search': {};
+      'status-description': {};
       'subscription-add': {};
       'case-name-search': {};
       'case-name-search-results': {};
