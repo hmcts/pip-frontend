da08d875c795442095ff14497cc8614ab1fecf2e
commitBlob <maro.radovic12@gmail.com>
2021-11-04 10:54:40 +0000

    fix: refactor names

--- src/test/unit/requests/statusDescriptionRequests.test.ts
+++ src/test/unit/requests/statusDescriptionRequests.test.ts
@@ -4,7 +4,7 @@ import {StatusDescriptionRequests} from '../../../main/resources/requests/status
 import fs from 'fs';
 import path from 'path';
 
-const searchDescriptionRequests = new StatusDescriptionRequests();
+const statusDescriptionRequests = new StatusDescriptionRequests();
 
 const rawData = fs.readFileSync(path.resolve(__dirname, '../mocks/StatusDescription.json'), 'utf-8');
 const statusDescriptionData = JSON.parse(rawData);
@@ -15,37 +15,37 @@ describe('getStatusDescriptionList()', () => {
 
   it('should return list of 49 courts events status', () => {
     stubGetStatusDescriptionList.withArgs('/glossary').resolves({data: statusDescriptionData});
-    return searchDescriptionRequests.getStatusDescriptionList().then(data => {
+    return statusDescriptionRequests.getStatusDescriptionList().then(data => {
       expect(data.length).toBe(49);
     });
   });
 
   it('should return null if request fails', async () => {
     stubGetStatusDescriptionList.withArgs('/glossary').resolves(Promise.reject({response:{data: 'test error'}}));
-    expect(await searchDescriptionRequests.getStatusDescriptionList()).toHaveLength(0);
+    expect(await statusDescriptionRequests.getStatusDescriptionList()).toHaveLength(0);
   });
 
   it('should return null if request fails', async () => {
     stubGetStatusDescriptionList.withArgs('/glossary').resolves(Promise.reject({request:'test error'}));
-    expect(await searchDescriptionRequests.getStatusDescriptionList()).toHaveLength(0);
+    expect(await statusDescriptionRequests.getStatusDescriptionList()).toHaveLength(0);
   });
 
   it('should return null if request fails', async () => {
     stubGetStatusDescriptionList.withArgs('/glossary').resolves(Promise.reject({message:'test error'}));
-    expect(await searchDescriptionRequests.getStatusDescriptionList()).toHaveLength(0);
+    expect(await statusDescriptionRequests.getStatusDescriptionList()).toHaveLength(0);
   });
 
 
   it('First glossary should be Adjourned', () => {
     stubGetStatusDescriptionList.withArgs('/glossary').resolves({data: statusDescriptionData});
-    return searchDescriptionRequests.getStatusDescriptionList().then(data => {
+    return statusDescriptionRequests.getStatusDescriptionList().then(data => {
       expect(data[0].eventName).toEqual('Adjourned');
     });
   });
 
   it('Description fof First glossary must not be empty', () => {
     stubGetStatusDescriptionList.withArgs('/glossary').resolves({data: statusDescriptionData});
-    return searchDescriptionRequests.getStatusDescriptionList().then(data => {
+    return statusDescriptionRequests.getStatusDescriptionList().then(data => {
       expect(data[0].eventStatus).not.toBeNull();
     });
   });
@@ -53,7 +53,7 @@ describe('getStatusDescriptionList()', () => {
   let i = 0;
   it('All Glossary items must have name and description', () => {
     stubGetStatusDescriptionList.withArgs('/courteventglossary').resolves({data: statusDescriptionData});
-    return searchDescriptionRequests.getStatusDescriptionList().then(data => {
+    return statusDescriptionRequests.getStatusDescriptionList().then(data => {
       expect(data[i].eventName).not.toBeNull();
       expect(data[i].eventStatus).not.toBeNull();
       i++;
