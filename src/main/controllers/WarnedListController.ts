0c5ce79b5961f0a9e19ac129b7790dc3577fc04e
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2021-11-09 09:42:45 +0000

    Added localisation

--- src/main/controllers/WarnedListController.ts
+++ src/main/controllers/WarnedListController.ts
@@ -1,9 +1,23 @@
 import {PipRequest} from '../models/request/PipRequest';
+import { CourtService } from '../service/courtService';
+import {cloneDeep} from 'lodash';
 import { Response } from 'express';
+
+const courtService = new CourtService();
 
 export default class WarnedListController {
 
-  public get(req: PipRequest, res: Response): void {
-    res.render('warned-list');
+  public async get(req: PipRequest, res: Response): Promise<void> {
+    const courtId = req.query['courtId'];
+
+    if (courtId && req.user) {
+      const court = await courtService.getCourtById(parseInt(courtId.toString()));
+      res.render('warned-list', {
+        ...cloneDeep(req.i18n.getDataByLanguage(req.lng)['warned-list']),
+        court: court,
+      });
+    } else {
+      res.render('error', req.i18n.getDataByLanguage(req.lng).error);
+    }
   }
 }
