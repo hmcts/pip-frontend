0e051219d28dcc1c28686448fc8dd59d6afb8fc1
commitBlob <maro.radovic12@gmail.com>
2021-11-10 14:30:32 +0000

    pub-876: remove commented out code

--- src/test/unit/views/session-test.ts
+++ src/test/unit/views/session-test.ts
@@ -28,24 +28,9 @@ describe('Subscription add Page initial load', () => {
       htmlRes = new DOMParser().parseFromString(res.text, 'text/html');
     });
   });
+
   it('should display sign out button in header for a logged in user', () => {
     const header = htmlRes.getElementsByClassName('govuk-header__navigation govuk-header__navigation--end');
     expect(header[0].innerHTML).contains('Sign out', 'Could not find correct value in header');
   });
 });
-
-
-// describe('Log out from page', () => {
-//   beforeAll(async () => {
-//
-//     await request(app).get(PAGE_URL).then(res => {
-//       htmlRes = new DOMParser().parseFromString(res.text, 'text/html');
-//     });
-//   });
-//   it('should display sign in button in header for a logged out user', () => {
-//     const header = htmlRes.getElementsByClassName('govuk-header__navigation govuk-header__navigation--end');
-//     // request(app).get('/logout');
-//     expect(header[0].innerHTML).contains('Sign in', 'Could not find correct value in header');
-//   });
-// });
-
