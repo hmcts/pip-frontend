46345fbbcb629756f54898752aa7a4ebd41c931b
commitBlob <maro.radovic12@gmail.com>
2021-11-19 09:27:24 +0000

    pub-742: selections routing

--- src/main/controllers/UnsubscribeConfirmationController.ts
+++ src/main/controllers/UnsubscribeConfirmationController.ts
@@ -5,8 +5,18 @@ import { cloneDeep } from 'lodash';
 export default class UnsubscribeConfirmationController {
   public post(req: PipRequest, res: Response): void {
     console.log('req', req.body);
-    res.render('unsubscribe-confirmation', {
-      ...cloneDeep(req.i18n.getDataByLanguage(req.lng)['unsubscribe-confirmation']),
-    });
+    if (req.body['unsubscribe-confirm']) {
+      const unsubscribeBody = req.body['unsubscribe-confirm'];
+      if (unsubscribeBody === 'yes') {
+        res.render('unsubscribe-confirmation', {
+          ...cloneDeep(req.i18n.getDataByLanguage(req.lng)['unsubscribe-confirmation']),
+        });
+      }
+      else {
+        res.redirect('/subscription-management');
+      }
+    } else {
+      res.render('error', req.i18n.getDataByLanguage(req.lng).error);
+    }
   }
 }
