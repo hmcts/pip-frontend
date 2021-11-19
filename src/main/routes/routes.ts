81958ebaa9fc7e308e3ccbabc7ecebad39c89dc0
commitBlob <maro.radovic12@gmail.com>
2021-11-19 14:37:48 +0000

    pub-933: Add isAuthenticated context to the POST pages

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -46,6 +46,7 @@ export default function(app: Application): void {
     });
   }
   app.get('/*', globalAuthGiver);
+  app.post('/*', globalAuthGiver);
   app.get('/', app.locals.container.cradle.homeController.get);
   app.get('/search-option', app.locals.container.cradle.searchOptionController.get);
   app.get('/alphabetical-search', app.locals.container.cradle.alphabeticalSearchController.get);
