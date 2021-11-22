6155b12b93d3ef74865847edebe4a64e799e48c9
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2021-11-22 09:39:23 +0000

    Revert "Merge remote-tracking branch 'origin/pre-release' into PUB-734-Case-Search"
    This reverts commit 0380c9402b0b4d7cb90a3a18b34a95134f7af827, reversing
    changes made to 20c55c83da56d4a55c85c758f28b0f04d58fa9fd.

--- jest.a11y.config.js
+++ jest.a11y.config.js
@@ -1,12 +1,14 @@
 module.exports = {
   roots: ['<rootDir>/src/test/a11y'],
-  testRegex: "(/src/test/.*|\\.(test|spec))\\.(ts|js)$",
-  testEnvironment: "node",
+  "testRegex": "(/src/test/.*|\\.(test|spec))\\.(ts|js)$",
+   "moduleFileExtensions": [
+    "ts",
+    "js"
+  ],
+  "testEnvironment": "node",
   transform: {
     '^.+\\.ts?$': 'ts-jest',
   },
-  moduleFileExtensions: ['ts', 'js'],
-  setupFiles: ["<rootDir>/jestEnvVars.js"],
-  // pa11y recommended default
-  testTimeout: 30000,
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFiles: ["<rootDir>/jestEnvVars.js"]
 }
