4f05207999d2caa65d6585ad5675c399bd24ec8f
commitBlob <maro.radovic12@gmail.com>
2021-12-09 14:30:05 +0000

    PUB-878: fix indents

--- src/main/cacheManager.ts
+++ src/main/cacheManager.ts
@@ -2,9 +2,9 @@ import { Logger } from '@hmcts/nodejs-logging';
 import config from 'config';
 
 const redisPortalCredential = {
-	host: config.get('secrets.pip-ss-kv.REDIS_HOST'),
-	port: config.get('secrets.pip-ss-kv.REDIS_PORT'),
-	password: config.get('secrets.pip-ss-kv.REDIS_PASSWORD'),
+  host: config.get('secrets.pip-ss-kv.REDIS_HOST'),
+  port: config.get('secrets.pip-ss-kv.REDIS_PORT'),
+  password: config.get('secrets.pip-ss-kv.REDIS_PASSWORD'),
 };
 
 const logger = Logger.getLogger('app');
@@ -18,14 +18,14 @@ const ioRedis = require('ioredis');
 const redisClient = ioRedis.createClient(connectionTest);
 
 redisClient.on('connect', () => {
-	logger.info('Connected to Redis');
+  logger.info('Connected to Redis');
 });
 
 redisClient.on('error', error => {
-	redisClient.quit();
-	logger.error('Failed to connect to Redis', error.message);
+  redisClient.quit();
+  logger.error('Failed to connect to Redis', error.message);
 });
 
 module.exports = {
-	redisClient,
+  redisClient,
 };
