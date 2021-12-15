d707ab86098b3f93ca1bb083ac9313250366cec9
commitBlob <maro.radovic12@gmail.com>
2021-12-15 15:00:51 +0000

    PUB-737: add pending subscription controller tests

--- src/test/unit/controllers/PendingSubscriptionsController.test.ts
+++ src/test/unit/controllers/PendingSubscriptionsController.test.ts
@@ -1,62 +1,125 @@
 import sinon from 'sinon';
 import { Response } from 'express';
-import fs from 'fs';
-import path from 'path';
-import {SubscriptionService} from '../../../main/service/subscriptionService';
-import {mockRequest} from '../mocks/mockRequest';
+import { SubscriptionService } from '../../../main/service/subscriptionService';
+import { mockRequest } from '../mocks/mockRequest';
 import PendingSubscriptionsController from '../../../main/controllers/PendingSubscriptionsController';
 
-const pendingSubscriptionController = new PendingSubscriptionsController();
-const rawData = fs.readFileSync(path.resolve(__dirname, '../mocks/subscriptionListResult.json'), 'utf-8');
-const subscriptionsData = JSON.parse(rawData);
-sinon.stub(SubscriptionService.prototype, 'getPendingSubscriptions').returns(subscriptionsData);
-
-const i18n = {
-  'pending-subscriptions': {},
+const mockCase = {
+  hearingId: 1,
+  courtId: 50,
+  courtNumber: 1,
+  date: '15/11/2021 10:00:00',
+  judge: 'His Honour Judge A Morley QC',
+  platform: 'In person',
+  caseNumber: 'T485913',
+  caseName: 'Tom Clancy',
+  urn: 'N363N6R4OG',
 };
-const response = {
-  render: function () {
-    return '';
-  },
-} as unknown as Response;
-const request = mockRequest(i18n);
-const responseMock = sinon.mock(response);
+const mockCourt = {
+  courtId: 643,
+  name: 'Aberdeen Tribunal Hearing Centre',
+  jurisdiction: 'Tribunal',
+  location: 'Scotland',
+  hearingList: [],
+  hearings: 0,
+};
+const postData = {'hearing-selections[]': 'T485913'};
+const queryParams = {'court': '643'};
+const userWithSubscriptions = '1';
+const userWithoutSubscriptions = '2';
+const pendingSubscriptionController = new PendingSubscriptionsController();
+const handleSubStub = sinon.stub(SubscriptionService.prototype, 'handleNewSubscription');
+const subscriptionStub = sinon.stub(SubscriptionService.prototype, 'getPendingSubscriptions');
+sinon.stub(SubscriptionService.prototype, 'removeFromCache').withArgs(queryParams, '3').resolves(true);
+subscriptionStub.withArgs(userWithoutSubscriptions, 'courts').resolves([]);
+subscriptionStub.withArgs(userWithoutSubscriptions, 'cases').resolves([]);
+subscriptionStub.withArgs(userWithSubscriptions, 'cases').resolves([mockCase]);
+subscriptionStub.withArgs(userWithSubscriptions, 'courts').resolves([mockCourt]);
+subscriptionStub.withArgs('3', 'cases').resolves([mockCase]);
+subscriptionStub.withArgs('3', 'courts').resolves([]);
+handleSubStub.withArgs(postData, userWithSubscriptions).resolves(true);
+
+const i18n = {'pending-subscriptions': {}};
+const response = {render: () => {return '';}} as unknown as Response;
 
 describe('Pending Subscriptions Controller', () => {
-  it('should render the pending subscription page', () => {
-    const expectedData = {
-      ...i18n['pending-subscriptions'],
-      searchInput : null,
-      searchResults: subscriptionsData,
-    };
+  describe('GET view', () => {
+    it('should render the pending subscription page without subscriptions', () => {
+      const request = mockRequest(i18n);
+      request.user = {id: userWithoutSubscriptions};
+      const expectedData = {
+        ...i18n['pending-subscriptions'],
+        pendingSubscriptions: {
+          cases: [],
+          courts: [],
+        },
+      };
+      const responseMock = sinon.mock(response);
+      responseMock.expects('render').once().withArgs('pending-subscriptions', expectedData);
 
-    responseMock.expects('render').once().withArgs('pending-subscriptions', expectedData);
+      return pendingSubscriptionController.get(request, response).then(() => {
+        responseMock.verify();
+      });
+    });
 
-    return pendingSubscriptionController.get(request, response).then(() => {
-      responseMock.verify();
+    it('should render pending subscriptions page with set subscriptions', () => {
+      const request = mockRequest(i18n);
+      request.user = {id: userWithSubscriptions};
+      const expectedData = {
+        ...i18n['pending-subscriptions'],
+        pendingSubscriptions: {
+          cases: [mockCase],
+          courts: [mockCourt],
+        },
+      };
+      const responseMock = sinon.mock(response);
+      responseMock.expects('render').once().withArgs('pending-subscriptions', expectedData);
+
+      return pendingSubscriptionController.get(request, response).then(() => {
+        responseMock.verify();
+      });
     });
   });
 
+  describe('POST view', () => {
+    it('should render pending subscriptions page if post data is provided', () => {
+      const request = mockRequest(i18n);
+      request.user = {id: '3'};
+      request.body = postData;
+      const expectedData = {
+        ...i18n['pending-subscriptions'],
+        pendingSubscriptions: {
+          cases: [mockCase],
+          courts: [],
+        },
+      };
+      const responseMock = sinon.mock(response);
+      responseMock.expects('render').once().withArgs('pending-subscriptions', expectedData);
 
-  it('should redirect to confirmation page if cases selected is multiple', async () => {
-    const request = mockRequest(i18n);
-    request.body = {'hearing-selections[]':['1', '10']};
-
-    const responseMock = sinon.mock(response);
-
-    responseMock.expects('render').once().withArgs('pending-subscriptions');
-    await pendingSubscriptionController.post(request, response);
-    return responseMock.verify();
+      return pendingSubscriptionController.post(request, response).then(() => {
+        responseMock.verify();
+      });
+    });
   });
 
-  it('should redirect to confirmation page if cases selected is one', async () => {
-    const request = mockRequest(i18n);
-    request.body = {'hearing-selections[]':'1'};
+  describe('removeSubscription view', () => {
+    it('should render pending subscriptions page on removeSubscription call', () => {
+      const request = mockRequest(i18n);
+      request.user = {id: '3'};
+      request.query = queryParams;
+      const expectedData = {
+        ...i18n['pending-subscriptions'],
+        pendingSubscriptions: {
+          cases: [mockCase],
+          courts: [],
+        },
+      };
+      const responseMock = sinon.mock(response);
+      responseMock.expects('render').once().withArgs('pending-subscriptions', expectedData);
 
-    const responseMock = sinon.mock(response);
-
-    responseMock.expects('render').once().withArgs('pending-subscriptions');
-    await pendingSubscriptionController.post(request, response);
-    return responseMock.verify();
+      return pendingSubscriptionController.removeSubscription(request, response).then(() => {
+        responseMock.verify();
+      });
+    });
   });
 });
