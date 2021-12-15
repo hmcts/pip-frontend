3f9a53afa9beeafe31554ee94a36d2a2a5961563
commitBlob <maro.radovic12@gmail.com>
2021-12-15 12:12:16 +0000

    PUB-737: refactor pending subscriptions cache requests

--- src/main/resources/requests/utils/pendingSubscriptionsFromCache.ts
+++ src/main/resources/requests/utils/pendingSubscriptionsFromCache.ts
@@ -1,62 +1,48 @@
-import {CaseSubscription} from '../../../models/caseSubscription';
 const { redisClient } = require('../../../cacheManager');
 
 export class PendingSubscriptionsFromCache {
+  public async setPendingSubscriptions(subscriptions, subscriptionType, userId): Promise<void> {
+    const filter = subscriptionType ===  'courts' ? 'courtId' : 'caseNumber';
 
-  public async setPendingSubscriptions(searchResult: CaseSubscription[], user): Promise<void> {
     if (redisClient.status === 'ready') {
-      const rawData = await redisClient.get(`pending-subscriptions${user.id}`);
-      const cacheResult = JSON.parse(rawData);
-      if (cacheResult) {
-        searchResult.forEach(hearing => {
-          if (cacheResult.filter(x => x.hearingId === hearing.hearingId).length === 0) {
-            cacheResult.push(hearing);
-          }
-        });
+      let subscriptionsSet = [];
+      const rawData = await redisClient.get(`pending-${subscriptionType}-subscriptions-${userId}`);
+      const cachedResults = JSON.parse(rawData);
+      if (cachedResults && cachedResults.length) {
+        subscriptionsSet = cachedResults;
       }
-      await redisClient.set(`pending-subscriptions${user.id}`, JSON.stringify(searchResult));
+      subscriptions.forEach((subscription) => {
+        // check if already in the cache
+        if (!subscriptionsSet.some(cached => cached[filter] === subscription[filter])) {
+          subscriptionsSet.push(subscription);
+        }
+      });
+      await redisClient.set(`pending-${subscriptionType}-subscriptions-${userId}`, JSON.stringify(subscriptionsSet));
     }
   }
 
-  public async getPendingSubscriptions(user): Promise<CaseSubscription[]> {
-    let cacheResult = null;
-    if (redisClient.status === 'ready' && user) {
-      cacheResult = JSON.parse(await redisClient.get(`pending-subscriptions${user.id}`));
+  public async getPendingSubscriptions(userId, type): Promise<any[]> {
+    let cacheResult = [];
+    if (redisClient.status === 'ready' && userId) {
+      cacheResult = JSON.parse(await redisClient.get(`pending-${type}-subscriptions-${userId}`));
     }
     return cacheResult;
   }
 
-  public async clearPendingSubscription(user): Promise<boolean> {
-    let subscribed = false;
-    if (redisClient.status === 'ready' && user) {
-      await redisClient.del(`pending-subscriptions${user.id}`);
-      subscribed = true;
-    }
-    return subscribed;
-  }
-
-  public async removeFromCache(id, user): Promise<boolean> {
-    let removed = false;
-    if (redisClient.status === 'ready' && user) {
-      let result = null;
-      const cacheResult = JSON.parse(await redisClient.get(`pending-subscriptions${user.id}`));
-      if (cacheResult) {
-        cacheResult.forEach(x=>{
-          if (x.hearingId === id)
-            result = x;
-        });
-
-        if (result) {
-          const index = cacheResult.indexOf(result, 0);
-          if (index > -1) {
-            cacheResult.splice(index, 1);
-            removed = true;
-          }
-        }
-        redisClient.set(`pending-subscriptions${user.id}`, JSON.stringify(cacheResult));
+  // @param removeObject - post data object {case: 'id'} || {court: 'id'}
+  public async removeFromCache(removeObject, userId): Promise<void> {
+    if (redisClient.status === 'ready' && userId) {
+      if (removeObject.case) {
+        const cachedCases = await this.getPendingSubscriptions(userId, 'cases');
+        const filteredCases = cachedCases.filter((c) => c.caseNumber !== removeObject.case);
+        redisClient.set(`pending-cases-subscriptions-${userId}`, JSON.stringify(filteredCases));
+      }
+      if (removeObject.court) {
+        const cachedCourts = await this.getPendingSubscriptions(userId, 'courts');
+        const filteredCourts = cachedCourts.filter((court) => court.courtId !== parseInt(removeObject.court));
+        redisClient.set(`pending-courts-subscriptions-${userId}`, JSON.stringify(filteredCourts));
       }
     }
-    return removed;
   }
 
 }
