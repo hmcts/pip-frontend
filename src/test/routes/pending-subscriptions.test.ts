571ee215350532a810ad3aca2b47098ba2f77e61
commitBlob <maro.radovic12@gmail.com>
2021-12-15 15:38:15 +0000

    PUB-737: refactor pending-subscriptions route tests

--- src/test/routes/pending-subscriptions.test.ts
+++ src/test/routes/pending-subscriptions.test.ts
@@ -1,16 +1,25 @@
 import { expect } from 'chai';
+import { app } from '../../main/app';
+import { request as expressRequest } from 'express';
 import request from 'supertest';
 import sinon from 'sinon';
-import { app } from '../../main/app';
-import {request as expressRequest} from 'express';
 
 sinon.stub(expressRequest, 'isAuthenticated').returns(true);
+app.request['user'] = {id: '1'};
 
 describe('subscription Confirmation', () => {
   describe('on GET', () => {
     test('should return subscription confirmation page', async () => {
       await request(app)
         .get('/pending-subscriptions')
+        .expect((res) => expect(res.status).to.equal(200));
+    });
+  });
+
+  describe('on POST', () => {
+    test('should return subscription confirmation page', async () => {
+      await request(app)
+        .post('/pending-subscriptions')
         .expect((res) => expect(res.status).to.equal(200));
     });
   });
