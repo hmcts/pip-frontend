be4e799ccf7ba101e4c0c3aaae36fbbd48cc7760
commitBlob <maro.radovic12@gmail.com>
2021-12-15 12:07:05 +0000

    PUB-737: refactor pending subscriptions controller

--- src/main/controllers/PendingSubscriptionsController.ts
+++ src/main/controllers/PendingSubscriptionsController.ts
@@ -1,46 +1,45 @@
 import { Response } from 'express';
 import { PipRequest } from '../models/request/PipRequest';
 import { SubscriptionService } from '../service/subscriptionService';
-import { HearingService } from '../service/hearingService';
-import validateRendering from '../common/utils';
+import { cloneDeep } from 'lodash';
 
 const subscriptionService = new SubscriptionService();
-const hearingService = new HearingService();
 
 export default class PendingSubscriptionsController {
-
   public async get(req: PipRequest, res: Response): Promise<void> {
-    const searchResults = await subscriptionService.getPendingSubscriptions(req.user);
-    validateRendering(searchResults,'pending-subscriptions',req, res);
+    const pendingSubscriptions = {
+      cases: await subscriptionService.getPendingSubscriptions(req.user['id'], 'cases'),
+      courts: await subscriptionService.getPendingSubscriptions(req.user['id'], 'courts'),
+    };
+    res.render('pending-subscriptions', {
+      ...cloneDeep(req.i18n.getDataByLanguage(req.lng)['pending-subscriptions']),
+      pendingSubscriptions,
+    });
   }
 
   public async post(req: PipRequest, res: Response): Promise<void> {
-    const searchInput = req.body['hearing-selections[]'];
-    let searchResults = [];
-    if (Array.isArray(searchInput)) {
-      // get and collect all the hearings id in searchInput array
-      for (const id of searchInput) {
-        const hearing = await hearingService.getHearingsById(parseInt(id));
-        if (hearing) {
-          searchResults.push(hearing);
-        }
-      }
-    }
-    else {
-      const hearing = await hearingService.getHearingsById(parseInt(searchInput));
-      if (hearing) {
-        searchResults.push(hearing);
-      }
-    }
+    await subscriptionService.handleNewSubscription(req.body, req.user);
+    const pendingSubscriptions = {
+      cases: await subscriptionService.getPendingSubscriptions(req.user['id'], 'cases'),
+      courts: await subscriptionService.getPendingSubscriptions(req.user['id'], 'courts'),
+    };
 
-    await subscriptionService.setPendingSubscriptions(searchResults, req.user);
-    searchResults = await subscriptionService.getPendingSubscriptions(req.user);
-    validateRendering(searchResults,'pending-subscriptions', req, res);
+    res.render('pending-subscriptions', {
+      ...cloneDeep(req.i18n.getDataByLanguage(req.lng)['pending-subscriptions']),
+      pendingSubscriptions,
+    });
   }
 
-  public async removeCase(req: PipRequest, res: Response): Promise<void> {
-    await subscriptionService.removeFromCache(parseInt(req.query['id'] as string), req.user);
-    const searchResults = await subscriptionService.getPendingSubscriptions(req.user);
-    validateRendering(searchResults,'pending-subscriptions', req, res);
+  public async removeSubscription(req: PipRequest, res: Response): Promise<void> {
+    await subscriptionService.removeFromCache(req.query, req.user['id']);
+    const pendingSubscriptions = {
+      cases: await subscriptionService.getPendingSubscriptions(req.user['id'], 'cases'),
+      courts: await subscriptionService.getPendingSubscriptions(req.user['id'], 'courts'),
+    };
+
+    res.render('pending-subscriptions', {
+      ...cloneDeep(req.i18n.getDataByLanguage(req.lng)['pending-subscriptions']),
+      pendingSubscriptions,
+    });
   }
 }
