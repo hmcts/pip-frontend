769fc8cb128bff6a4791565c635078127740e8a4
commitBlob <maro.radovic12@gmail.com>
2021-12-15 15:29:30 +0000

    PUB-737: add subscription-confirmed route tests

---
+++ src/test/routes/subscription-confirmed.test.ts
@@ -0,0 +1,22 @@
+import { expect } from 'chai';
+import request from 'supertest';
+import sinon from 'sinon';
+import { app } from '../../main/app';
+import { request as expressRequest } from 'express';
+import { SubscriptionService } from '../../main/service/subscriptionService';
+
+describe('Subscription Confirmed', () => {
+  beforeEach(() => {
+    sinon.stub(SubscriptionService.prototype, 'subscribe').withArgs('1').resolves(true);
+    sinon.stub(expressRequest, 'isAuthenticated').returns(true);
+    app.request['user'] = {id: '1'};
+  });
+
+  describe('on POST', () => {
+    test('should return subscription confirmation page', async () => {
+      await request(app)
+        .post('/subscription-confirmed')
+        .expect((res) => expect(res.status).to.equal(200));
+    });
+  });
+});
