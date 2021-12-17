724f52ea7fd18a8582855d71e917a5de48426711
commitBlob <maro.radovic12@gmail.com>
2021-12-17 09:28:57 +0000

    Merge branch 'pre-release' into pub-742-delete-subscription

--- src/test/e2e/pageobjects/SubscriptionManagement.page.ts
+++ src/test/e2e/pageobjects/SubscriptionManagement.page.ts
@@ -1,21 +1,10 @@
 import { SubscriptionAddPage } from './SubscriptionAdd.page';
 import { DeleteSubscriptionPage } from './DeleteSubscription.page';
+import { CommonPage } from './Common.page';
 
 const helpers = require('../Helpers/Selectors');
 
-export class SubscriptionManagementPage {
-  open (path): Promise<string> {
-    return browser.url(path);
-  }
-
-  async getPageTitle(): Promise<string> {
-    $(helpers.CommonPageTitle).catch(() => {
-      console.log(`${helpers.CommonPageTitle} not found`);
-    });
-
-    return $(helpers.CommonPageTitle).getText();
-  }
-
+export class SubscriptionManagementPage extends CommonPage {
   async clickAddNewSubscriptionButton(): Promise<SubscriptionAddPage> {
     $(helpers.ContinueButton).catch(() => {
       console.log(`${helpers.ContinueButton} not found`);
