7c8ce8fb71d6a7829c5ff4933598e07e422f4f9b
commitBlob <maro.radovic12@gmail.com>
2021-12-17 15:00:07 +0000

    rollback

--- src/main/cacheManager.ts
+++ src/main/cacheManager.ts
@@ -1,17 +1,16 @@
 import { Logger } from '@hmcts/nodejs-logging';
-// import config from 'config';
+import config from 'config';
 
-// const redisCredentials = {
-//   host: config.get('secrets.pip-ss-kv.REDIS_HOST'),
-//   port: config.get('secrets.pip-ss-kv.REDIS_PORT'),
-//   password: config.get('secrets.pip-ss-kv.REDIS_PASSWORD'),
-// };
+const redisCredentials = {
+  host: config.get('secrets.pip-ss-kv.REDIS_HOST'),
+  port: config.get('secrets.pip-ss-kv.REDIS_PORT'),
+  password: config.get('secrets.pip-ss-kv.REDIS_PASSWORD'),
+};
 
 const logger = Logger.getLogger('app');
 const ioRedis = require('ioredis');
 // double s is required when using TLS connection
-// const connectionString = `rediss://:${redisCredentials.password}@${redisCredentials.host}:${redisCredentials.port}`;
-const connectionString = `rediss://:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`;
+const connectionString = `rediss://:${redisCredentials.password}@${redisCredentials.host}:${redisCredentials.port}`;
 const redisClient = new ioRedis(connectionString);
 
 redisClient.on('connect', () => {
