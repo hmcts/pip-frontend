477a1ab1cf9cd3f61776aceedfbdfc0bee4e5aef
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2021-12-20 12:28:08 +0000

    Added space

--- src/test/e2e/pageobjects/LiveCaseCourtSearchController.page.ts
+++ src/test/e2e/pageobjects/LiveCaseCourtSearchController.page.ts
@@ -3,7 +3,7 @@ import {CommonPage} from './Common.page';
 
 const helpers = require('../Helpers/Selectors');
 
-export class LiveCaseCourtSearchControllerPage extends CommonPage{
+export class LiveCaseCourtSearchControllerPage extends CommonPage {
   async getPageTitle(): Promise<string> {
     $(helpers.CommonPageTitle).catch(() => {
       console.log(`${helpers.CommonPageTitle} not found`);
