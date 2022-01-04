057b1c507130f307a99c2381c72bbd68e070b11c
commitBlob <maro.radovic12@gmail.com>
2022-01-04 15:21:35 +0000

    PUB-737: refactor e2e page objects

--- src/test/e2e/PageObjects/SubscriptionConfirmation.page.ts
+++ src/test/e2e/PageObjects/SubscriptionConfirmation.page.ts
@@ -1,17 +1,9 @@
-import {SubscriptionConfirmedPage} from './SubscriptionConfirmed.page';
+import { SubscriptionConfirmedPage } from './SubscriptionConfirmed.page';
+import { CommonPage } from './Common.page';
 
 const helpers = require('../Helpers/Selectors');
 
-export class SubscriptionConfirmationPage {
-
-  async getPageTitle(): Promise<string> {
-    $(helpers.SearchResultTitle).catch(() => {
-      console.log(`${helpers.SearchResultTitle} not found`);
-    });
-
-    return $(helpers.SearchResultTitle).getText();
-  }
-
+export class SubscriptionConfirmationPage extends CommonPage{
   async getResults(): Promise<number> {
     $(helpers.Results).catch(() => {
       console.log(`${helpers.Results} not found`);
--- src/test/e2e/PageObjects/SubscriptionConfirmed.page.ts
+++ src/test/e2e/PageObjects/SubscriptionConfirmed.page.ts
@@ -1,15 +1,4 @@
+import { CommonPage } from './Common.page';
 
-const helpers = require('../Helpers/Selectors');
-
-export class SubscriptionConfirmedPage {
-
-  async getPageTitle(): Promise<string> {
-    $(helpers.SearchResultTitle).catch(() => {
-      console.log(`${helpers.SearchResultTitle} not found`);
-    });
-
-    return $(helpers.SearchResultTitle).getText();
-  }
-
-
+export class SubscriptionConfirmedPage extends CommonPage {
 }
