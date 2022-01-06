01fc226e6e0330aeea1160591d7265d184c66529
commitBlob <maro.radovic12@gmail.com>
2022-01-06 17:14:42 +0000

    PUB-737: remove console log

--- src/main/resources/requests/utils/pendingSubscriptionsFromCache.ts
+++ src/main/resources/requests/utils/pendingSubscriptionsFromCache.ts
@@ -26,7 +26,6 @@ export class PendingSubscriptionsFromCache {
     if (redisClient.status === 'ready' && userId) {
       cacheResult = JSON.parse(await redisClient.get(`pending-${type}-subscriptions-${userId}`));
     }
-    console.log('getPendingSubscriptions', cacheResult);
     return cacheResult;
   }
 
