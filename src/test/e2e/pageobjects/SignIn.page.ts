8e7cf365f512a9b07487dab52763825a1d1492cc
marcogagliardi <marco.gagliardi@justice.gov.uk>
2022-01-12 16:58:19 +0000

    pub-939 - added file

---
+++ src/test/e2e/pageobjects/SignIn.page.ts
@@ -0,0 +1,45 @@
+import { CommonPage } from './Common.page';
+
+const helpers = require('../Helpers/Selectors');
+
+export class SignInPage extends CommonPage {
+  async getPageTitle(): Promise<string> {
+    $(helpers.SearchOptionsTitle).catch(() => {
+      console.log(`${helpers.SearchOptionsTitle} not found`);
+    });
+
+    return $(helpers.SearchOptionsTitle).getText();
+  }
+
+  async clickContinueForRadio1(): Promise<string> {
+    $(helpers.ContinueButton).catch(() => {
+      console.log(`${helpers.ContinueButton} not found`);
+    });
+    const continueButton = await $(helpers.ContinueButton);
+    continueButton.click();
+
+    return 'https://google.com';
+  }
+
+  async clickContinueForRadio2(): Promise<string> {
+    $(helpers.ContinueButton).catch(() => {
+      console.log(`${helpers.ContinueButton} not found`);
+    });
+    const continueButton = await $(helpers.ContinueButton);
+    continueButton.click();
+
+    return 'https://google.com';
+  }
+
+
+  async clickContinueForRadio3(): Promise<string> {
+    $(helpers.ContinueButton).catch(() => {
+      console.log(`${helpers.ContinueButton} not found`);
+    });
+    const continueButton = await $(helpers.ContinueButton);
+    continueButton.click();
+
+    return 'https://google.com';
+  }
+
+}
