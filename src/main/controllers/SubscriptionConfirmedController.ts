12ac5554de0c3cc2a022e1a44f9a2afb14672d9d
commitBlob <maro.radovic12@gmail.com>
2022-01-13 09:44:47 +0000

    PUB-1017: display error message if there are no pending subscriptions

--- src/main/controllers/SubscriptionConfirmedController.ts
+++ src/main/controllers/SubscriptionConfirmedController.ts
@@ -1,14 +1,25 @@
 import { Response } from 'express';
 import { PipRequest } from '../models/request/PipRequest';
 import { SubscriptionService } from '../service/subscriptionService';
+import { PendingSubscriptionsFromCache } from '../resources/requests/utils/pendingSubscriptionsFromCache';
 
 const subscriptionService = new SubscriptionService();
+const cacheService = new PendingSubscriptionsFromCache();
 
 export default class SubscriptionConfirmedController {
   public async post(req: PipRequest, res: Response): Promise<void> {
-    const subscribed = await subscriptionService.subscribe(req.user['id']);
-    subscribed ?
-      res.render('subscription-confirmed', req.i18n.getDataByLanguage(req.lng)['subscription-confirmed']) :
-      res.render('error', req.i18n.getDataByLanguage(req.lng).error);
+    const userId = req.user['id'];
+    const cachedCourts = await cacheService.getPendingSubscriptions(userId, 'courts');
+    const cachedCases = await cacheService.getPendingSubscriptions(userId, 'cases');
+
+    if (cachedCases?.length || cachedCourts?.length) {
+      const subscribed = await subscriptionService.subscribe(userId);
+      subscribed ?
+        res.render('subscription-confirmed', req.i18n.getDataByLanguage(req.lng)['subscription-confirmed']) :
+        res.render('error', req.i18n.getDataByLanguage(req.lng).error);
+    } else {
+      res.redirect('pending-subscriptions?no-subscriptions=true');
+    }
+
   }
 }
