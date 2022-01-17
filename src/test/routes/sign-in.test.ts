932d58adc235d28cdadd2aaf6abb6975ea23e4ff
marcogagliardi <marco.gagliardi@justice.gov.uk>
2022-01-17 11:17:18 +0000

    pub-939 - tests

--- src/test/routes/sign-in.test.ts
+++ src/test/routes/sign-in.test.ts
@@ -2,6 +2,9 @@ import { expect } from 'chai';
 import request from 'supertest';
 import { app } from '../../main/app';
 
+const options = ['hmcts', 'common', 'pi'];
+const externalUrls = ['https://www.google.com','https://www.google.com','https://www.google.com'];
+
 describe('Sign In option', () => {
   describe('on GET', () => {
     test('should return sign-in routing page', async () => {
@@ -10,4 +13,18 @@ describe('Sign In option', () => {
         .expect((res) => expect(res.status).to.equal(200));
     });
   });
+
+  for (let i = 0; i < options.length; i += 1) {
+    describe('on POST', () => {
+      test('should redirect to external url when '+ options[i] +' is chosen', async () => {
+        await request(app)
+          .post('/sign-in')
+          .send({'sign-in': options[i]})
+          .expect((res) => {
+            expect(res.status).to.equal(302);
+            expect(res.header['location']).to.equal(externalUrls[i]);
+          });
+      });
+    });
+  }
 });
