c639c4cb77cb8ff1766a449efd7ad42eed1c0fc3
marcogagliardi <marco.gagliardi@justice.gov.uk>
2022-01-20 13:51:23 +0000

    pub-960 - added heading and test

--- src/test/unit/views/login-return.test.ts
+++ src/test/unit/views/login-return.test.ts
@@ -5,7 +5,9 @@ import {expect} from 'chai';
 const PAGE_URL = '/login/return';
 
 const navigationClass = 'moj-sub-navigation__link';
-const expectedHeader = 'Home';
+const headerClass = 'govuk-heading-l';
+const expectedHeaderMenu = 'Home';
+const expectedHeader = 'You are logged in';
 
 let htmlRes: Document;
 
@@ -16,8 +18,13 @@ describe('Login return Page', () => {
     });
   });
 
-  it('should display header', () => {
+  it('should display header menu', () => {
     const navigation = htmlRes.getElementsByClassName(navigationClass);
+    expect(navigation[0].innerHTML).contains(expectedHeaderMenu, 'Could not find the header');
+  });
+
+  it('should display header', () => {
+    const navigation = htmlRes.getElementsByClassName(headerClass);
     expect(navigation[0].innerHTML).contains(expectedHeader, 'Could not find the header');
   });
 });
