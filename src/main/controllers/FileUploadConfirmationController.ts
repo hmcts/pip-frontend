c797d1319207bd1d9d4d5faa6184a57d00c352cb
commitBlob <maro.radovic12@gmail.com>
2022-01-27 10:24:38 +0000

    PUB-544: upload confirmation controller WIP

---
+++ src/main/controllers/FileUploadConfirmationController.ts
@@ -0,0 +1,8 @@
+import { PipRequest } from '../models/request/PipRequest';
+import { Response } from 'express';
+
+export default class FileUploadConfirmationController {
+  public async post(req: PipRequest, res: Response): Promise<void> {
+    
+  }
+}
