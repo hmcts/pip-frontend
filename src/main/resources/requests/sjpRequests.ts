9ad0c11e495a5d0a2ffb581294160a61368c1b5d
Daniel Furnivall <danielfurnivall@gmail.com>
2022-02-01 00:00:33 +0000

    working axios connection to local data-management. SJP page has been hit with a bomb, may well need to be redone. Also, redis needs changed back.

--- src/main/resources/requests/sjpRequests.ts
+++ src/main/resources/requests/sjpRequests.ts
@@ -1,12 +1,25 @@
 import fs from 'fs';
 import path from 'path';
 import { SingleJusticeProcedureCase } from '../../models/singleJusticeProcedureCase';
+import {dataManagementApi} from './utils/axiosConfig';
 
 export class SjpRequests {
   mocksPath = '../mocks';
   rawData = fs.readFileSync(path.resolve(__dirname, this.mocksPath, 'SingleJusticeProcedureCases.json'), 'utf-8');
 
-  getSJPCases(): SingleJusticeProcedureCase[] {
-    return JSON.parse(this.rawData).results;
+  public async getSJPCases(): Promise<SingleJusticeProcedureCase[]> {
+    try {
+      const response = await dataManagementApi.get('/publication/search/10', {headers: {'verification':'true'}});
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.data);
+      } else if (error.request) {
+        console.log(`Request failed. ${error.request}`);
+      } else {
+        console.log(`ERROR: ${error.message}`);
+      }
+    }
   }
 }
