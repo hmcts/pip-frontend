42c242ffac9b19c82081fa852cc55d9b66737ba2
alberto-hortiguela <alberto.hortiguela@version1.com>
2022-02-03 12:41:46 +0000

    Fix PageObject corrupt file

---
+++ src/test/e2e/PageObjects/CaseReferenceNumberSearchResults.page.ts
@@ -0,0 +1,14 @@
+import { CommonPage } from './Common.page';
+
+const helpers = require('../Helpers/Selectors');
+
+export class CaseReferenceNumberSearchResultsPage extends CommonPage {
+  async getResults(): Promise<number> {
+    $(helpers.Results).catch(() => {
+      console.log(`${helpers.Results} not found`);
+    });
+
+    const results = $$(helpers.Results);
+    return results.length;
+  }
+}
