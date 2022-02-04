6b431f02b9ed78223c872e12fe37e4076966ef9a
commitBlob <maro.radovic12@gmail.com>
2022-02-04 08:36:51 +0000

    add logging for staging and additional e2e tests

--- src/main/cacheManager.ts
+++ src/main/cacheManager.ts
@@ -2,9 +2,9 @@ import { Logger } from '@hmcts/nodejs-logging';
 import config from 'config';
 
 const redisCredentials = {
-  host: config.get('secrets.pip-ss-kv.REDIS_HOST'),
-  port: config.get('secrets.pip-ss-kv.REDIS_PORT'),
-  password: config.get('secrets.pip-ss-kv.REDIS_PASSWORD'),
+  host: (process.env.REDIS_HOST) ? process.env.REDIS_HOST : config.get('secrets.pip-ss-kv.REDIS_HOST'),
+  port: (process.env.REDIS_PORT) ? process.env.REDIS_PORT :config.get('secrets.pip-ss-kv.REDIS_PORT'),
+  password: (process.env.REDIS_PASSWORD) ? process.env.REDIS_PASSWORD :config.get('secrets.pip-ss-kv.REDIS_PASSWORD'),
 };
 
 const logger = Logger.getLogger('app');
@@ -12,6 +12,9 @@ const ioRedis = require('ioredis');
 // double s is required when using TLS connection
 const connectionString = `rediss://:${redisCredentials.password}@${redisCredentials.host}:${redisCredentials.port}`;
 const redisClient = new ioRedis(connectionString);
+
+logger.info('redis env var', redisCredentials.host);
+logger.info('redis env port', redisCredentials.port);
 
 redisClient.on('connect', () => {
   logger.info('Connected to Redis');
