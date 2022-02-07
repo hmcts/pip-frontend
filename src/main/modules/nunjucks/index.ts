e5113a2817b01820a2d8421e7317842f536087c6
Daniel Furnivall <danielfurnivall@gmail.com>
2022-02-07 12:05:52 +0000

    added json config file for list type lookup

--- src/main/modules/nunjucks/index.ts
+++ src/main/modules/nunjucks/index.ts
@@ -39,18 +39,10 @@ export class Nunjucks {
 
     const dateFilter = require('nunjucks-date-filter');
     env.addFilter('date', dateFilter);
-    const listTypeLookup = new Map([
-      ['SJP_PUBLIC_LIST', 'Single Justice Procedure Public List'],
-      ['SJP_PRESS_LIST', 'Single Justice Procedure Press List'],
-      ['CROWN_DAILY_LIST', 'Crown Daily List'],
-      ['CROWN_WARNED_LIST', 'Crown Warned List'],
-      ['CROWN_FIRM_LIST', 'Crown Firm List'],
-      ['MAGS_PUBLIC_LIST', 'Magistrate Public List'],
-      ['MAGS_STANDARD_LIST', 'Magistrate Standard List'],
-      ['CIVIL_DAILY_CAUSE_LIST', 'Civil Daily Cause List'],
-      ['FAMILY_DAILY_CAUSE_LIST', 'Family Daily Cause List'],
-    ]);
-    env.addFilter('listType', function(x){return listTypeLookup.get(x);});
+    // const listTypeLookup = require('listTypeLookup');
+    const fs = require ('fs');
+    const listTypeLookup = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'listTypeLookup.json')));
+    env.addFilter('listType', function(x){return listTypeLookup[x];});
 
     app.use((req, res, next) => {
       res.locals.pagePath = req.path;
