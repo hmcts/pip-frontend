bfcc2edaf7809e9f76eec1021f0acbace6d3a80b
commitBlob <maro.radovic12@gmail.com>
2022-02-10 10:51:32 +0000

    PUB-517: more efficient language switch

--- src/main/controllers/InterstitialController.ts
+++ src/main/controllers/InterstitialController.ts
@@ -4,7 +4,7 @@ import { cloneDeep } from 'lodash';
 
 export default class InterstitialController {
   public get(req: PipRequest, res: Response): void {
-    const currentLanguage = (req.cookies['i18next']) ? req.cookies['i18next'] : 'en';
+    const currentLanguage = req?.lng ? req.lng : 'en';
     res.render('interstitial', {
       ...cloneDeep(req.i18n.getDataByLanguage(req.lng).interstitial),
       currentLanguage,
