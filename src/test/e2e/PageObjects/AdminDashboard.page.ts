06764fceab55b66c2deb502de7482a61f36b9674
commitBlob <maro.radovic12@gmail.com>
2022-02-16 13:00:37 +0000

    PUB-543: correct helpers variable

--- src/test/e2e/PageObjects/AdminDashboard.page.ts
+++ src/test/e2e/PageObjects/AdminDashboard.page.ts
@@ -6,10 +6,10 @@ const helpers = require('../Helpers/Selectors');
 export class AdminDashboardPage extends CommonPage {
   async clickUploadFileCard(): Promise<ManualUploadPage> {
     await $(helpers.UploadFile).catch(() => {
-      console.log(`${helpers.EmailSubscriptionLink} not found`);
+      console.log(`${helpers.UploadFile} not found`);
     });
 
-    await $(helpers.EmailSubscriptionLink).click();
+    await $(helpers.UploadFile).click();
     return new ManualUploadPage();
   }
 }
