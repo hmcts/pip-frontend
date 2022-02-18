1cc7e893482b77a37cc3dc5d18590088cd8a586e
commitBlob <maro.radovic12@gmail.com>
2022-02-18 09:54:51 +0000

    Demo - Fix manual upload errors

--- src/main/controllers/ManualUploadController.ts
+++ src/main/controllers/ManualUploadController.ts
@@ -22,36 +22,39 @@ export default class ManualUploadController {
   }
 
   public async post(req: PipRequest, res: Response): Promise<void> {
-    const errors = {
-      fileErrors: manualUploadService.validateFileUpload(req.file),
-      formErrors: await manualUploadService.validateFormFields(req.body),
-    };
-    const listItems = await manualUploadService.buildFormData();
-    const formValues = {
-      ...cloneDeep(req.i18n.getDataByLanguage(req.lng)['manual-upload']),
-      listItems,
-      errors,
-      formData: req.body,
-    };
+    if (req.query?.showerror === 'true') {
+      res.render('error', req.i18n.getDataByLanguage(req.lng).error);
+    } else  {
+      const errors = {
+        fileErrors: manualUploadService.validateFileUpload(req.file),
+        formErrors: await manualUploadService.validateFormFields(req.body),
+      };
+      const listItems = await manualUploadService.buildFormData();
+      const formValues = {
+        ...cloneDeep(req.i18n.getDataByLanguage(req.lng)['manual-upload']),
+        listItems,
+        errors,
+        formData: req.body,
+      };
 
-    if (errors.fileErrors || errors.formErrors) {
-      res.render('manual-upload', formValues);
-    } else {
-      req.body['court'] = await manualUploadService.appendCourtId(req.body['input-autocomplete']);
-      req.body['artefactType'] = 'LIST'; //Agreed on defaulting to only option available until more types become ready
-      req.body['fileName'] = req.file['originalname'];
-      req.body['display-from'] = manualUploadService.buildDate(req.body, 'display-date-from');
-      req.body['display-to'] = manualUploadService.buildDate(req.body, 'display-date-to');
-      req.body['content-date-from'] = manualUploadService.buildDate(req.body, 'content-date-from');
-      if (req.body?.language) {
-        req.body['languageName'] = formValues['form'].language.find(item => item.value === req.body.language).text;
+      if (errors.fileErrors || errors.formErrors) {
+        res.render('manual-upload', formValues);
+      } else {
+        req.body['court'] = await manualUploadService.appendCourtId(req.body['input-autocomplete']);
+        req.body['artefactType'] = 'LIST'; //Agreed on defaulting to only option available until more types become ready
+        req.body['fileName'] = req.file['originalname'];
+        req.body['display-from'] = manualUploadService.buildDate(req.body, 'display-date-from');
+        req.body['display-to'] = manualUploadService.buildDate(req.body, 'display-date-to');
+        req.body['content-date-from'] = manualUploadService.buildDate(req.body, 'content-date-from');
+        if (req.body?.language) {
+          req.body['languageName'] = formValues['form'].language.find(item => item.value === req.body.language).text;
+        }
+        if (req.body?.classification) {
+          req.body['classificationName'] = formValues['form'].classification.find(item => item.value === req.body.classification).text;
+        }
+        res.cookie('formCookie', JSON.stringify(req.body));
+        res.redirect('/manual-upload-summary?check=true');
       }
-      if (req.body?.classification) {
-        req.body['classificationName'] = formValues['form'].classification.find(item => item.value === req.body.classification).text;
-      }
-      res.cookie('formCookie', JSON.stringify(req.body));
-      res.redirect('/manual-upload-summary?check=true');
     }
-
   }
 }
