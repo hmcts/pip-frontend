1123379e60661bfe2679b29865fa4c579622f4f6
Daniel Furnivall <danielfurnivall@gmail.com>
2022-02-21 16:16:44 +0000

    test commit

--- src/main/service/summaryOfPublicationsService.ts
+++ src/main/service/summaryOfPublicationsService.ts
@@ -1,10 +1,9 @@
 import {PublicationRequests} from '../resources/requests/publicationRequests';
-import {Artefact} from '../models/artefact';
+import {Artefact} from '../models/Artefact';
 
 const PublicationReqs = new PublicationRequests();
 
 export class SummaryOfPublicationsService {
-
   public async getPublications(courtId, verification: boolean): Promise<Artefact[]> {
     return PublicationReqs.getPublicationsByCourt(courtId, verification);
   }
