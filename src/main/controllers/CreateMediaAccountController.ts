f366b88f5183fa84d7b65c365af2dcacc6bd7f28
marcogagliardi <marco.gagliardi@justice.gov.uk>
2022-02-21 15:17:53 +0000

    pub-1095 - merge master

--- src/main/controllers/CreateMediaAccountController.ts
+++ src/main/controllers/CreateMediaAccountController.ts
@@ -13,11 +13,25 @@ export default class CreateMediaAccountController {
   public post(req: PipRequest, res: Response): void {
     const formValidation = createAccountService.validateFormFields(req.body);
     const isValidForm = Object.values(formValidation).every(o => o.message === null);
-    isValidForm ?
-      res.redirect('account-request-submitted') :
+    if (isValidForm) {
+      let inputEl: HTMLInputElement = this.inputEl.nativeElement;
+      let fileCount: number = inputEl.files.length;
+      let formData = new FormData();
+      if (fileCount > 0) { // a file was selected
+        for (let i = 0; i < fileCount; i++) {
+          formData.append('file[]', inputEl.files.item(i));
+        }
+        this.http
+          .post('http://your.upload.url', formData)
+        // do whatever you do...
+        // subscribe to observable to listen for response
+      }
+      res.redirect('account-request-submitted');
+    } else {
       res.render('create-media-account', {
         ...cloneDeep(req.i18n.getDataByLanguage(req.lng)['create-media-account']),
         formErrors: formValidation,
       });
+    }
   }
 }
