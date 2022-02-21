5319c6dfb721ee4f3d27dc13a3bb9dfe3a40f620
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2022-02-21 11:34:36 +0000

    add missing code

--- src/main/resources/requests/publicationRequests.ts
+++ src/main/resources/requests/publicationRequests.ts
@@ -1,4 +1,5 @@
 import { dataManagementApi } from './utils/axiosConfig';
+import {Publication} from '../../models/publication';
 
 export class PublicationRequests {
 
@@ -21,6 +22,22 @@ export class PublicationRequests {
   public async getIndividualPubJson(artefactId, verification): Promise<string> {
     try {
       const response = await dataManagementApi.get('/publication/' + artefactId + '/payload', {headers: {'verification': `${verification}`}});
+      return response.data;
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.data);
+      } else if (error.request) {
+        console.log(`Request failed. ${error.request}`);
+      } else {
+        console.log(`ERROR: ${error.message}`);
+      }
+    }
+    return null;
+  }
+
+  public async getListOfPubs(courtId, verification): Promise<Publication[]> {
+    try {
+      const response = await dataManagementApi.get(`/publication/search/${courtId}`, {headers: {'verification':`${verification}`}});
       return response.data;
     } catch (error) {
       if (error.response) {
