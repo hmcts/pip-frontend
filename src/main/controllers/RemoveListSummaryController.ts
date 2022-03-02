aa65a2ca758f636c7df913bfd40fae17f9cbc422
commitBlob <maro.radovic12@gmail.com>
2022-03-02 13:12:21 +0000

    PUB-1037: initialise list summary page

---
+++ src/main/controllers/RemoveListSummaryController.ts
@@ -0,0 +1,23 @@
+import { PipRequest } from '../models/request/PipRequest';
+import { Response } from 'express';
+import { cloneDeep } from 'lodash';
+import { CourtService } from '../service/courtService';
+import { SummaryOfPublicationsService } from '../service/summaryOfPublicationsService';
+import { ManualUploadService } from '../service/manualUploadService';
+
+const courtService = new CourtService();
+const publicationService = new SummaryOfPublicationsService();
+const manualUploadService = new ManualUploadService();
+
+export default class RemoveListSummaryController {
+  public async get(req: PipRequest, res: Response): Promise<void> {
+    const courtId = parseInt(req.query?.courtId as string);
+    courtId ?
+      res.render('remove-list-summary', {
+        ...cloneDeep(req.i18n.getDataByLanguage(req.lng)['remove-list-summary']),
+        court: await courtService.getCourtById(courtId),
+        removalList: manualUploadService.formatListRemovalValues(await publicationService.getPublications(courtId, !!req.user)),
+      }) :
+      res.render('error', req.i18n.getDataByLanguage(req.lng).error);
+  }
+}
