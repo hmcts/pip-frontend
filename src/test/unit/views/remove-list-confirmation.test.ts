3bcd32a094e0c52c5e1a46354f6a3427fda96d26
commitBlob <maro.radovic12@gmail.com>
2022-03-03 15:22:27 +0000

    PUB-1037: add view unit tests

---
+++ src/test/unit/views/remove-list-confirmation.test.ts
@@ -0,0 +1,59 @@
+import { request as expressRequest } from 'express';
+import sinon from 'sinon';
+import request from 'supertest';
+import { app } from '../../../main/app';
+import { PublicationService } from '../../../main/service/publicationService';
+import { expect } from 'chai';
+
+const PAGE_URL = '/remove-list-confirmation?artefact=18dec6ee-3a30-47bb-9fb3-6a343d6b9efb&court=10';
+sinon.stub(expressRequest, 'isAuthenticated').returns(true);
+sinon.stub(PublicationService.prototype, 'removePublication').withArgs('foo').resolves(false);
+
+let htmlRes: Document;
+
+describe('Remove List Confirmation Page', () => {
+  describe('without error', () => {
+    beforeAll(async () => {
+      await request(app).get(PAGE_URL).then(res => {
+        htmlRes = new DOMParser().parseFromString(res.text, 'text/html');
+      });
+    });
+
+    it('should display header', () => {
+      const header = htmlRes.getElementsByClassName('govuk-heading-l');
+      expect(header[0].innerHTML).contains('Are you sure you want to remove this content?', 'Could not find correct value in header');
+    });
+
+    it('should display 2 radio options with valid values', () => {
+      const radios = htmlRes.getElementsByClassName('govuk-radios__input');
+      expect(radios.length).equals(2, 'Could not find all radio buttons');
+      expect(radios[0].getAttribute('value')).equals('yes', 'Could not find valid radio value');
+      expect(radios[1].getAttribute('value')).equals('no', 'Could not find valid radio value');
+    });
+
+    it('should display continue button',  () => {
+      const buttons = htmlRes.getElementsByClassName('govuk-button');
+      expect(buttons[0].innerHTML).contains('Continue', 'Could not find button');
+    });
+  });
+
+  describe('with error', () => {
+    beforeAll(async () => {
+      await request(app).post(PAGE_URL).send({courtId: '5', artefactId: 'foo'}).then(res => {
+        htmlRes = new DOMParser().parseFromString(res.text, 'text/html');
+      });
+    });
+
+    it('should display error summary', () => {
+      const dialog = htmlRes.getElementsByClassName('govuk-error-summary');
+      expect(dialog[0].getElementsByClassName('govuk-error-summary__title')[0].innerHTML)
+        .contains('There is a problem', 'Could not find error dialog title');
+    });
+
+    it('should display error messages in th the summary', () => {
+      const list = htmlRes.getElementsByClassName(' govuk-error-summary__list')[0];
+      const listItems = list.getElementsByTagName('a');
+      expect(listItems[0].innerHTML).contains('Please select an option', 'Could not find error');
+    });
+  });
+});
---
+++ src/test/unit/views/remove-list-summary.test.ts
@@ -0,0 +1,81 @@
+import { CourtService } from '../../../main/service/courtService';
+import { SummaryOfPublicationsService } from '../../../main/service/summaryOfPublicationsService';
+import { ManualUploadService } from '../../../main/service/manualUploadService';
+import { request as expressRequest } from 'express';
+import sinon from 'sinon';
+import request from 'supertest';
+import { app } from '../../../main/app';
+import { expect } from 'chai';
+
+const PAGE_URL = '/remove-list-summary?courtId=5';
+const mockCourt = {
+  courtId: '5',
+  name: 'The court',
+};
+const mockPublications = [
+  {
+    listItem: 'SJP_PUBLIC_LIST',
+    displayFrom: '2022-02-08T12:26:42.908',
+    displayTo: '2024-02-08T12:26:42.908',
+    dateRange: '8 Feb 2022 to 8 Feb 2024',
+    listTypeName: 'SJP Public List',
+    courtId: '5',
+    artefactId: 'valid-artefact',
+  },
+  {
+    listItem: 'SJP_PUBLIC_LIST',
+    displayFrom: '2022-02-16T12:26:42.908',
+    displayTo: '2024-02-08T12:26:42.908',
+    dateRange: '8 Feb 2022 to 8 Feb 2024',
+    listTypeName: 'SJP Public List',
+    courtId: '5',
+    artefactId: 'valid-artefact-777',
+  },
+];
+const tableHeaders = ['List type', 'Court', 'Date', 'Actions'];
+sinon.stub(CourtService.prototype, 'getCourtById').resolves(mockCourt);
+sinon.stub(SummaryOfPublicationsService.prototype, 'getPublications').withArgs('5', true, true).resolves(mockPublications);
+sinon.stub(ManualUploadService.prototype, 'formatListRemovalValues').returns(mockPublications);
+sinon.stub(expressRequest, 'isAuthenticated').returns(true);
+
+let htmlRes: Document;
+
+describe('Remove List Summary Page', () => {
+  beforeAll(async () => {
+    await request(app).get(PAGE_URL).then(res => {
+      htmlRes = new DOMParser().parseFromString(res.text, 'text/html');
+    });
+  });
+
+  it('should display header', () => {
+    const header = htmlRes.getElementsByClassName('govuk-heading-l');
+    expect(header[0].innerHTML).contains('Select content to remove', 'Could not find correct value in header');
+  });
+
+  it('should display results count', () => {
+    const resultsCount = htmlRes.getElementsByClassName('govuk-body')[0];
+    expect(resultsCount.innerHTML).contains('Showing 2 result(s)', 'Could not find results paragraph');
+  });
+
+  it('should display correct table headers', () => {
+    const headerNames = htmlRes.getElementsByClassName('govuk-table__header');
+    for (let i=0; i < tableHeaders.length; i++) {
+      expect(headerNames[i].innerHTML).contains(tableHeaders[i], `Could not find correct header (${tableHeaders[i]})`);
+    }
+  });
+
+  it('should display correct row values', () => {
+    const tableRows = htmlRes.getElementsByClassName('govuk-table__body')[0].getElementsByClassName('govuk-table__row');
+    expect(tableRows.length).equal(2, 'Incorrect table rows count');
+    for(let i = 0; i < tableRows.length; i++) {
+      const rowCells = tableRows[i].getElementsByClassName('govuk-table__cell');
+      const removeActionHref = htmlRes.getElementsByClassName('unsubscribe-action')[i].getAttribute('href').valueOf();
+      expect(rowCells[0].innerHTML).contains(mockPublications[i].listTypeName, 'Could not find valid list type name');
+      expect(rowCells[1].innerHTML).contains(mockCourt.name, 'Could not find valid court name');
+      expect(rowCells[2].innerHTML).contains(mockPublications[i].dateRange, 'Could not find valid list date range');
+      expect(rowCells[3].innerHTML).contains('Remove', 'Could not find valid action');
+      expect(removeActionHref).contains(`remove-list-confirmation?artefact=${mockPublications[i].artefactId}&court=${mockCourt.courtId}`,
+        'Could not find valid action href');
+    }
+  });
+});
