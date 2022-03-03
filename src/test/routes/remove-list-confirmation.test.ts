7d5e2b1e261a0f5a788f0403e8a4daa6fb967672
commitBlob <maro.radovic12@gmail.com>
2022-03-03 09:32:50 +0000

    PUB-1037: add route tests

---
+++ src/test/routes/remove-list-confirmation.test.ts
@@ -0,0 +1,74 @@
+import { request as expressRequest } from 'express';
+import request from 'supertest';
+import sinon from 'sinon';
+import { app } from '../../main/app';
+import { expect } from 'chai';
+import { PublicationService } from '../../main/service/publicationService';
+
+const URL = '/remove-list-confirmation';
+
+sinon.stub(expressRequest, 'isAuthenticated').returns(true);
+const removePublicationStub = sinon.stub(PublicationService.prototype, 'removePublication');
+removePublicationStub.withArgs('valid-artefact').resolves(true);
+removePublicationStub.withArgs('invalid-artefact').resolves(false);
+
+describe('Remove List Confirmation', () => {
+  describe('on GET', () => {
+    test('should return remove list confirmation page', async () => {
+      await request(app)
+        .get(URL+ '?artefact=foo&court=1')
+        .expect((res) => expect(res.status).to.equal(200));
+    });
+
+    test('should return error page', async () => {
+      await request(app)
+        .get(URL)
+        .expect((res) => expect(res.status).to.equal(200));
+    });
+  });
+
+  describe('on POST', () => {
+    test('should redirect to remove list success page choice if yes and request is success', async () => {
+      await request(app)
+        .post(URL).send({
+          'remove-choice': 'yes',
+          'artefactId': 'valid-artefact',
+        })
+        .expect((res) => {
+          expect(res.status).to.equal(302);
+          expect(res.header['location']).to.equal('/remove-list-success');
+        });
+    });
+
+    test('should return error page if choice is yes and request fails', async () => {
+      await request(app)
+        .post(URL).send({
+          'remove-choice': 'yes',
+          'artefactId': 'invalid-artefact',
+        })
+        .expect((res) => expect(res.status).to.equal(200));
+    });
+
+    test('should redirect to remove list summary page if choice is no', async () => {
+      await request(app)
+        .post(URL).send({
+          'remove-choice': 'no',
+          'artefactId': 'valid-artefact',
+          'courtId': '1',
+        })
+        .expect((res) => {
+          expect(res.status).to.equal(302);
+          expect(res.header['location']).to.equal('/remove-list-summary?courtId=1');
+        });
+    });
+
+    test('should return remove list confirmation page if choice is blank', async () => {
+      await request(app)
+        .post(URL).send({
+          'artefactId': 'valid-artefact',
+          'courtId': '1',
+        })
+        .expect((res) => expect(res.status).to.equal(200));
+    });
+  });
+});
