00ea5d7ba929d89202c81280d488787f8f36d56f
commitBlob <maro.radovic12@gmail.com>
2022-03-08 14:31:51 +0000

    PUB-1077: admin account form validation

--- src/main/controllers/CreateAdminAccountController.ts
+++ src/main/controllers/CreateAdminAccountController.ts
@@ -1,12 +1,39 @@
+import { cloneDeep } from 'lodash';
+import { CreateAccountService } from '../service/createAccountService';
 import { PipRequest } from '../models/request/PipRequest';
 import { Response } from 'express';
 
+const createAccountService = new CreateAccountService();
+let formCookie;
+
 export default class CreateAdminAccountController {
   public get(req: PipRequest, res: Response): void {
-    res.render('create-admin-account', req.i18n.getDataByLanguage(req.lng)['create-admin-account']);
+    formCookie = req.cookies['createAdminAccountCookie'];
+    const formData = formCookie ? JSON.parse(formCookie) : null;
+    const options = {
+      formData,
+      radios: createAccountService.buildRadiosList(),
+      ...cloneDeep(req.i18n.getDataByLanguage(req.lng)['create-admin-account']),
+    };
+    res.render('create-admin-account', options);
   }
 
   public post(req: PipRequest, res: Response): void {
-    res.render('create-admin-account', req.i18n.getDataByLanguage(req.lng)['create-admin-account']);
+    const formData = req.body;
+    const formValidation = createAccountService.validateAdminFormFields(formData);
+    const options = {
+      formData,
+      radios: createAccountService.buildRadiosList(formData['user-role']),
+      ...cloneDeep(req.i18n.getDataByLanguage(req.lng)['create-admin-account']),
+      formErrors: formValidation,
+    };
+    const isValidForm = Object.values(formValidation).every(o => o.message === null);
+    if (isValidForm) {
+      formData.userRoleObject = createAccountService.getRoleByKey(formData['user-role']);
+      res.cookie('createAdminAccount', JSON.stringify(formData));
+      res.redirect('create-admin-account-summary');
+    } else {
+      res.render('create-admin-account', options);
+    }
   }
 }
--- src/main/service/createAccountService.ts
+++ src/main/service/createAccountService.ts
@@ -1,5 +1,28 @@
 import { allowedImageTypes } from '../models/consts';
 
+const adminRolesList = [
+  {
+    key: 'super-admin-ctsc',
+    text: 'Internal - Super Administrator - CTSC',
+    mapping: 'INTERNAL_SUPER_ADMIN_CTSC',
+  },
+  {
+    key: 'super-admin-local',
+    text: 'Internal - Super Administrator - Local',
+    mapping: 'INTERNAL_SUPER_ADMIN_LOCAL',
+  },
+  {
+    key: 'admin-ctsc',
+    text: 'Internal - Administrator - CTSC',
+    mapping: 'INTERNAL_ADMIN_CTSC',
+  },
+  {
+    key: 'admin-local',
+    text: 'Internal - Administrator - Local',
+    mapping: 'INTERNAL_ADMIN_LOCAL',
+  },
+];
+
 export class CreateAccountService {
   public validateFormFields(formValues: object): object {
     return {
@@ -22,6 +45,43 @@ export class CreateAccountService {
     };
   }
 
+  public validateAdminFormFields(formValues: object): object {
+    return {
+      firstNameError: {
+        message: this.isNotBlank(formValues['firstName']) ? null : 'Enter first name',
+        href: '#firstName',
+      },
+      lastNameError: {
+        message: this.isNotBlank(formValues['lastName']) ? null : 'Enter last name',
+        href: '#lastName',
+      },
+      emailError: {
+        message: this.validateEmail(formValues['emailAddress'], true),
+        href: '#emailAddress',
+      },
+      radioError: {
+        message: formValues['user-role'] ? null : 'Select a role',
+        href: '#user-role',
+      },
+    };
+  }
+
+  public getRoleByKey(key: string): object {
+    return adminRolesList.find(item => item.key === key);
+  }
+
+  public buildRadiosList(checkedRadio = ''): any[] {
+    const radios = [];
+    adminRolesList.forEach((role) => {
+      radios.push({
+        value: role.key,
+        text: role.text,
+        checked: checkedRadio === role.key,
+      });
+    });
+    return radios;
+  }
+
   isValidImageType(imageName: string): boolean {
     const imageType = imageName.split('.')[1]?.toLocaleLowerCase();
     return allowedImageTypes.includes(imageType);
@@ -36,14 +96,14 @@ export class CreateAccountService {
     return emailRegex.test(email);
   }
 
-  validateEmail(email: string): string {
+  validateEmail(email: string, isAdmin = false): string {
     let message = null;
     if (this.isNotBlank(email)) {
       if (!this.isValidEmail(email)) {
         message = 'Enter an email address in the correct format, like name@example.com';
       }
     } else {
-      message = 'Enter your email address';
+      message = isAdmin ? 'Enter email address' : 'Enter your email address';
     }
     return message;
   }
