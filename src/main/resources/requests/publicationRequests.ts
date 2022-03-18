f4ff13dee508973ea0e6a927bd20df6e781a6198
commitBlob <maro.radovic12@gmail.com>
2022-03-18 15:39:42 +0000

    PUB-1037: put missing request back

--- src/main/resources/requests/publicationRequests.ts
+++ src/main/resources/requests/publicationRequests.ts
@@ -19,6 +19,23 @@ export class PublicationRequests {
     return null;
   }
 
+  public async getPublicationByCaseValue(searchQuery: string, searchValue: string, verified: boolean): Promise<Artefact[]> {
+    try {
+      const response = await dataManagementApi.get(`/publication/search/${searchQuery}/${searchValue}`,
+        {headers: {verification: verified}});
+      return response.data;
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.data);
+      } else if (error.request) {
+        console.log(`Request failed. ${error.request}`);
+      } else {
+        console.log(`ERROR: ${error.message}`);
+      }
+    }
+    return [];
+  }
+
   public async getIndividualPublicationJson(artefactId, verification): Promise<JSON> {
     try {
       const response = await dataManagementApi.get('/publication/' + artefactId + '/payload',
@@ -50,23 +67,6 @@ export class PublicationRequests {
       }
     }
     return null;
-  }
-
-  public async getPublicationsByCourt(courtId: string, verified: boolean): Promise<Artefact[]> {
-    try {
-      const response = await dataManagementApi.get(`/publication/courtId/${courtId}`,
-        {headers: {verification: verified}});
-      return response.data;
-    } catch (error) {
-      if (error.response) {
-        console.log(error.response.data);
-      } else if (error.request) {
-        console.log(`Request failed. ${error.request}`);
-      } else {
-        console.log(`ERROR: ${error.message}`);
-      }
-    }
-    return [];
   }
 
   public async getPublicationsByCourt(courtId: string, verified: boolean, admin: boolean): Promise<Artefact[]> {
