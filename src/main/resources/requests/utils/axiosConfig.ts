8fb098d2ec34cb230fbee1a5b66b5f9656db1f18
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2022-04-26 18:51:12 +0100

    Added chart values

--- src/main/resources/requests/utils/axiosConfig.ts
+++ src/main/resources/requests/utils/axiosConfig.ts
@@ -1,29 +1,50 @@
 import axios from 'axios';
 import oauth from 'axios-oauth-client';
 import tokenProvider from 'axios-token-interceptor';
+import config from 'config';
+
+const tokenUrl = process.env.TOKEN_URL ? process.env.TOKEN_URL : config.get('secrets.pip-ss-kv.TOKEN_URL');
+
+const clientId =
+  process.env.CLIENT_ID_INTERNAL ? process.env.CLIENT_ID_INTERNAL : config.get('secrets.pip-ss-kv.CLIENT_ID_INTERNAL');
+
+const clientSecret =
+  process.env.CLIENT_SECRET_INTERNAL ?
+    process.env.CLIENT_SECRET_INTERNAL : config.get('secrets.pip-ss-kv.CLIENT_SECRET_INTERNAL');
+
+const dataManagementUrl =
+  process.env.DATA_MANAGEMENT_AZ_API
+    ? process.env.DATA_MANAGEMENT_AZ_API : config.get('secrets.pip-ss-kv.DATA_MANAGEMENT_AZ_API');
+
+const subscriptionManagementUrl =
+  process.env.SUBSCRIPTION_MANAGEMENT_AZ_API ?
+    process.env.SUBSCRIPTION_MANAGEMENT_AZ_API : config.get('secrets.pip-ss-kv.SUBSCRIPTION_MANAGEMENT_AZ_API');
+
+const accountManagementUrl = process.env.ACCOUNT_MANAGEMENT_AZ_API ?
+  process.env.ACCOUNT_MANAGEMENT_AZ_API : config.get('secrets.pip-ss-kv.ACCOUNT_MANAGEMENT_AZ_API');
 
 export const getDataManagementCredentials = oauth.client(axios.create(), {
-  url: process.env.TOKEN_URL,
+  url: tokenUrl,
   GRANT_TYPE: 'client_credentials',
-  CLIENT_ID: process.env.CLIENT_ID_NEW,
-  CLIENT_SECRET: process.env.CLIENT_SECRET,
-  SCOPE: process.env.DATA_MANAGEMENT_AZ_API,
+  CLIENT_ID: clientId,
+  CLIENT_SECRET: clientSecret,
+  SCOPE: dataManagementUrl,
 });
 
 export const getSubscriptionManagementCredentials = oauth.client(axios.create(), {
-  url: process.env.TOKEN_URL,
+  url: tokenUrl,
   GRANT_TYPE: 'client_credentials',
-  CLIENT_ID: process.env.CLIENT_ID_NEW,
-  CLIENT_SECRET: process.env.CLIENT_SECRET,
-  SCOPE: process.env.SUBSCRIPTION_MANAGEMENT_AZ_API,
+  CLIENT_ID: clientId,
+  CLIENT_SECRET: clientSecret,
+  SCOPE: subscriptionManagementUrl,
 });
 
 export const getAccountManagementCredentials = oauth.client(axios.create(), {
-  url: process.env.TOKEN_URL,
+  url: tokenUrl,
   GRANT_TYPE: 'client_credentials',
-  CLIENT_ID: process.env.CLIENT_ID_NEW,
-  CLIENT_SECRET: process.env.CLIENT_SECRET,
-  SCOPE: process.env.ACCOUNT_MANAGEMENT_AZ_API,
+  CLIENT_ID: clientId,
+  CLIENT_SECRET: clientSecret,
+  SCOPE: accountManagementUrl,
 });
 
 export const dataManagementApi = axios.create({baseURL: (process.env.DATA_MANAGEMENT_URL || 'https://pip-data-management.staging.platform.hmcts.net/'), timeout: 10000});
