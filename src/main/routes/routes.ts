5feb8cf0e019b779765c99e98d8168e5a4e5dacc
Josh Blackmoor <joshblackmoor01@icloud.com>
2022-04-27 13:18:01 +0100

    Work in progress

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -106,6 +106,7 @@ export default function(app: Application): void {
   app.get('/summary-of-publications', app.locals.container.cradle.summaryOfPublicationsController.get);
   app.get('/file-publication', app.locals.container.cradle.flatFileController.get);
   app.get('/sjp-public-list', app.locals.container.cradle.sjpPublicListController.get);
+  app.get('/accessibility-statement', app.locals.container.cradle.accessibilityStatementController.get);
 
   // Restricted paths
   app.get('/account-home', ensureAuthenticated, app.locals.container.cradle.accountHomeController.get);
