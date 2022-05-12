e63a95a86763e7933f58236256468c49803797a2
Josh <joshblackmoor@users.noreply.github.com>
2022-05-12 11:48:38 +0100

    Pub 1160 accessibility statement page (#368)
    * Work in progress
    
    * page written with current draft
    
    * Unit tests in progress
    
    * Unit tests in progress
    
    * Push for review
    
    * Fixed lint issue
    
    * Fixed unit tests
    
    * Fixed footer test
    
    * Added id'd divs to split sections
    
    * Updated unit tests to use div IDs for readability
    
    * rearranged new route alphabetically
    
    * Layout changes from testing fixed
    
    * Fixed spacing
    
    * Re-trigger build
    
    * PUB-1160 - Re-triggered build
    
    Co-authored-by: ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
    Co-authored-by: ChrisS1512 <Christopher.Stacey@justice.gov.uk>

---
+++ src/test/routes/accessibility-statement.test.ts
@@ -0,0 +1,14 @@
+import { expect } from 'chai';
+import request from 'supertest';
+
+import { app } from '../../main/app';
+
+describe('Accessibility Statement Page', () => {
+  describe('on GET', () => {
+    test('should return accessibility statement page', async () => {
+      await request(app)
+        .get('/accessibility-statement')
+        .expect((res) => expect(res.status).to.equal(200));
+    });
+  });
+});
