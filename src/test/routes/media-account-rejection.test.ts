f038662c02f86804d466e36d1903c997e7c2caec
Daniel Furnivall <danielfurnivall@gmail.com>
2022-05-13 16:45:30 +0100

    routes tests added

---
+++ src/test/routes/media-account-rejection.test.ts
@@ -0,0 +1,54 @@
+import request from 'supertest';
+import {app} from '../../main/app';
+import {expect} from 'chai';
+import { MediaAccountApplicationService } from '../../main/service/mediaAccountApplicationService';
+import sinon from 'sinon';
+import {request as expressRequest} from 'express';
+
+sinon.stub(expressRequest, 'isAuthenticated').returns(true);
+
+describe('Media Account Rejection', () => {
+
+  const applicationID = '1234';
+
+  const dummyApplication = {
+    'id': '1234',
+    'fullName': 'Test Name',
+    'email': 'a@b.com',
+    'employer': 'Employer',
+    'image': '12345',
+    'imageName': 'ImageName',
+    'requestDate': '2022-05-09T00:00:01',
+    'status': 'PENDING',
+    'statusDate': '2022-05-09T00:00:01',
+  };
+
+  const getApplicationByIdStub = sinon.stub(MediaAccountApplicationService.prototype, 'getApplicationByIdAndStatus');
+
+  getApplicationByIdStub.withArgs(applicationID, 'PENDING').resolves(dummyApplication);
+
+  describe('on view approval', () => {
+    test('should return the media account rejection page', () => {
+      request(app)
+        .get('/media-account-rejection?applicantId=' + applicationID)
+        .expect((res) => expect(res.status).to.equal(200));
+    });
+  });
+
+  describe('on submit approval', () => {
+    test('should return success when rejection is accept', () => {
+      request(app)
+        .post('/media-account-rejection?applicantId=' + applicationID)
+        .send({'reject-confirmation': 'Yes'})
+        .expect((res) => expect(res.status).to.equal(200));
+    });
+
+    test('should return success when approval is reject', () => {
+      request(app)
+        .post('/media-account-rejection?applicantId=' + applicationID)
+        .send({'reject-confirmation': 'No'})
+        .expect((res) => expect(res.status).to.equal(200));
+    });
+  });
+
+});
