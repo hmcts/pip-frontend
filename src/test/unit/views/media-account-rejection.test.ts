995711eeac318becea6b936b2391c4e6e242720e
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2022-05-24 18:19:58 +0100

    PUB-1065 - Updated to summary table on all pages

--- src/test/unit/views/media-account-rejection.test.ts
+++ src/test/unit/views/media-account-rejection.test.ts
@@ -9,9 +9,10 @@ const applicationId = '1234';
 
 const PAGE_URL = '/media-account-rejection?applicantId=' + applicationId;
 const headingClass = 'govuk-heading-l';
-const tableCaptionClass = 'govuk-table__caption';
-const tableHeader = 'govuk-table__header';
-const tableCell = 'govuk-table__cell';
+const tableCaptionClass = 'govuk-heading-m';
+const summaryHeader = 'govuk-summary-list__key';
+const summaryCell = 'govuk-summary-list__value';
+const summaryActions = 'govuk-summary-list__actions';
 const radioLabel = 'govuk-radios__label';
 const buttonTag = 'button';
 const errorSummaryClass = 'govuk-error-summary__title';
@@ -74,84 +75,64 @@ describe('Media Account Rejection Page', () => {
   });
 
   it('should display name header', () => {
-    const header = htmlRes.getElementsByClassName(tableHeader);
+    const header = htmlRes.getElementsByClassName(summaryHeader);
     expect(header[0].innerHTML).contains(nameHeader, 'Could not find the name header');
   });
 
   it('should display name value', () => {
-    const value = htmlRes.getElementsByClassName(tableCell);
+    const value = htmlRes.getElementsByClassName(summaryCell);
     expect(value[0].innerHTML).contains(nameValue, 'Could not find the name value');
   });
 
-  it('should hide last column for display name', () => {
-    const value = htmlRes.getElementsByClassName(tableCell);
-    expect(value[1].hasAttribute('aria-hidden')).to.be.true;
-  });
-
   it('should display email header', () => {
-    const header = htmlRes.getElementsByClassName(tableHeader);
+    const header = htmlRes.getElementsByClassName(summaryHeader);
     expect(header[1].innerHTML).contains(emailHeader, 'Could not find the email header');
   });
 
   it('should display email value', () => {
-    const value = htmlRes.getElementsByClassName(tableCell);
-    expect(value[2].innerHTML).contains(emailValue, 'Could not find the email value');
-  });
-
-  it('should hide last column for email', () => {
-    const value = htmlRes.getElementsByClassName(tableCell);
-    expect(value[3].hasAttribute('aria-hidden')).to.be.true;
+    const value = htmlRes.getElementsByClassName(summaryCell);
+    expect(value[1].innerHTML).contains(emailValue, 'Could not find the email value');
   });
 
   it('should display employer header', () => {
-    const header = htmlRes.getElementsByClassName(tableHeader);
+    const header = htmlRes.getElementsByClassName(summaryHeader);
     expect(header[2].innerHTML).contains(employerHeader, 'Could not find the employer header');
   });
 
   it('should display employer value', () => {
-    const value = htmlRes.getElementsByClassName(tableCell);
-    expect(value[4].innerHTML).contains(employerValue, 'Could not find the employer value');
-  });
-
-  it('should hide last column for employer', () => {
-    const value = htmlRes.getElementsByClassName(tableCell);
-    expect(value[5].hasAttribute('aria-hidden')).to.be.true;
+    const value = htmlRes.getElementsByClassName(summaryCell);
+    expect(value[2].innerHTML).contains(employerValue, 'Could not find the employer value');
   });
 
   it('should display date applied header', () => {
-    const header = htmlRes.getElementsByClassName(tableHeader);
+    const header = htmlRes.getElementsByClassName(summaryHeader);
     expect(header[3].innerHTML).contains(appliedHeader, 'Could not find the date applied header');
   });
 
   it('should display date applied value', () => {
-    const value = htmlRes.getElementsByClassName(tableCell);
-    expect(value[6].innerHTML).contains(appliedValue, 'Could not find the date applied value');
-  });
-
-  it('should hide last column for date applied', () => {
-    const value = htmlRes.getElementsByClassName(tableCell);
-    expect(value[7].hasAttribute('aria-hidden')).to.be.true;
+    const value = htmlRes.getElementsByClassName(summaryCell);
+    expect(value[3].innerHTML).contains(appliedValue, 'Could not find the date applied value');
   });
 
   it('should display proof of id header', () => {
-    const header = htmlRes.getElementsByClassName(tableHeader);
+    const header = htmlRes.getElementsByClassName(summaryHeader);
     expect(header[4].innerHTML).contains(proofOfIdHeader, 'Could not find the proof of id header');
   });
 
   it('should display proof of id value', () => {
-    const value = htmlRes.getElementsByClassName(tableCell);
-    expect(value[8].innerHTML).contains(proofOfIdValue, 'Could not find the proof of id value');
+    const value = htmlRes.getElementsByClassName(summaryCell);
+    expect(value[4].innerHTML).contains(proofOfIdValue, 'Could not find the proof of id value');
   });
 
   it('should display proof of id view text', () => {
-    const value = htmlRes.getElementsByClassName(tableCell);
-    const anchor = value[9].getElementsByTagName('a')[0];
+    const value = htmlRes.getElementsByClassName(summaryActions);
+    const anchor = value[0].getElementsByTagName('a')[0];
     expect(anchor.innerHTML).contains(proofOfIdView, 'Could not find the proof of id view text');
   });
 
   it('should display proof of id view link', () => {
-    const value = htmlRes.getElementsByClassName(tableCell);
-    const anchor = value[9].getElementsByTagName('a')[0];
+    const value = htmlRes.getElementsByClassName(summaryActions);
+    const anchor = value[0].getElementsByTagName('a')[0];
     expect(anchor.getAttribute('href')).contains(proofOfIdViewLink, 'Could not find the proof of id view text');
   });
 
