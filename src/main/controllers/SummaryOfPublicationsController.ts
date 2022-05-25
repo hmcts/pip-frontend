9d8fc272312b465cd1d4053b7159e4428eccdfb5
Daniel Furnivall <danielfurnivall@gmail.com>
2022-05-25 16:29:35 +0100

    initial commit - passing locally

--- src/main/controllers/SummaryOfPublicationsController.ts
+++ src/main/controllers/SummaryOfPublicationsController.ts
@@ -3,10 +3,7 @@ import { Response } from 'express';
 import {cloneDeep} from 'lodash';
 import {CourtService} from '../service/courtService';
 import {SummaryOfPublicationsService} from '../service/summaryOfPublicationsService';
-import fs from 'fs';
-import path from 'path';
 
-const urlLookup = JSON.parse(fs.readFileSync(path.resolve(__dirname,'../modules/nunjucks/listUrlLookup.json'), 'utf-8'));
 const summaryOfPublicationsService = new SummaryOfPublicationsService();
 const courtService = new CourtService();
 
@@ -19,21 +16,13 @@ export default class SummaryOfPublicationsController {
       const court = await courtService.getCourtById(parseInt(courtId.toString()));
       const courtName = (court == null ? 'Missing Court' : court.name);
       const publications = await summaryOfPublicationsService.getPublications(parseInt(courtId.toString()), (!!req.user));
-      if (publications.length === 1){
-        if (publications[0].isFlatFile){
-          res.redirect(`file-publication?artefactId=${publications[0].artefactId}`);
-        }
-        else {
-          res.redirect(`${urlLookup[publications[0].listType]}?artefactId=${publications[0].artefactId}`);
-        }
-      }
-      else {
-        res.render('summary-of-publications', {
-          ...cloneDeep(req.i18n.getDataByLanguage(req.lng)['summary-of-publications']),
-          publications,
-          courtName,
-        });
-      }
+
+      res.render('summary-of-publications', {
+        ...cloneDeep(req.i18n.getDataByLanguage(req.lng)['summary-of-publications']),
+        publications,
+        courtName,
+      });
+
     } else {
       res.render('error', req.i18n.getDataByLanguage(req.lng).error);
     }
