1f9cae82b213fd07a197e05a43d842311008b28e
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2022-05-27 16:46:46 +0100

    fix route test

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -76,7 +76,8 @@ export default function(app: Application): void {
     const encodedAdminSignOutRedirect = encodeURIComponent(`${FRONTEND_URL}/login?p=`+ authenticationConfig.ADMIN_POLICY);
     logger.info('B2C_URL', B2C_URL);
     logger.info('encodedSignOutRedirect', encodedSignOutRedirect);
-    if(_req?.user?.isAdmin) {
+    const adminRole = adminAuthentication.isAdminUser(_req);
+    if(adminRole) {
       res.redirect(`${B2C_URL}${authenticationConfig.POLICY}/oauth2/v2.0/logout?post_logout_redirect_uri=${encodedAdminSignOutRedirect}`);
     } else {
       res.redirect(`${B2C_URL}${authenticationConfig.POLICY}/oauth2/v2.0/logout?post_logout_redirect_uri=${encodedSignOutRedirect}`);
--- src/test/routes/logout.test.ts
+++ src/test/routes/logout.test.ts
@@ -1,8 +1,18 @@
 import request from 'supertest';
 import {app} from '../../main/app';
+import sinon from 'sinon';
+import {AdminAuthentication} from '../../main/authentication/adminAuthentication';
+
+sinon.stub(AdminAuthentication.prototype, 'isAdminUser').returns(true);
 
 describe('Logout', () => {
   test('should redirect to the homepage', async () => {
+    await request(app)
+      .get('/logout')
+      .expect((res) => expect(res.redirect).toBeTruthy);
+  });
+
+  test('should redirect to the admin login page', async () => {
     await request(app)
       .get('/logout')
       .expect((res) => expect(res.redirect).toBeTruthy);
