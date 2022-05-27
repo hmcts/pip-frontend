3a797bdf2fc7b9e42afc1ea4ecf0ba09b8b58a23
Josh Blackmoor <joshblackmoor01@icloud.com>
2022-05-27 17:48:19 +0100

    Added in temp fix for unit tests

--- jestEnvVars.js
+++ jestEnvVars.js
@@ -1,3 +1,4 @@
 process.env.CLIENT_SECRET='1234';
 process.env.FRONTEND_URL='https://localhost:8080';
 process.env.SESSION_SECRET='abcd';
+process.env.INSTRUMENTATION_KEY='00000000-0000-0000-0000-000000000000';
--- src/main/modules/appinsights/index.ts
+++ src/main/modules/appinsights/index.ts
@@ -1,11 +1,14 @@
 import config from 'config';
+import process from 'process';
 
 const appInsights = require('applicationinsights');
 
 export class AppInsights {
 
   enable(): void {
-    if (config.get('secrets.pip-ss-kv.INSTRUMENTATION_KEY')) {
+    if(process.env.INSTRUMENTATION_KEY) {
+      console.log('Do not start app insights');
+    } else {
       appInsights.setup(config.get('secrets.pip-ss-kv.INSTRUMENTATION_KEY'))
         .setSendLiveMetrics(true)
         .start();
@@ -14,5 +17,4 @@ export class AppInsights {
       appInsights.defaultClient.trackTrace({message: 'App insights activated'});
     }
   }
-
 }
