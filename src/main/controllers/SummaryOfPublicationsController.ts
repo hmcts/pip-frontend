5b886e9a077be182a4ad46a93664ed773b08d480
furnivall <danielfurnivall@gmail.com>
2022-06-08 17:04:12 +0100

    PUB-1299 - remove single publication SoP behaviour. (#402)
    * initial commit - passing locally
    
    * fixed lint
    
    Co-authored-by: ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>

--- src/main/controllers/SummaryOfPublicationsController.ts
+++ src/main/controllers/SummaryOfPublicationsController.ts
@@ -1,12 +1,9 @@
 import {PipRequest} from '../models/request/PipRequest';
-import { Response } from 'express';
+import {Response} from 'express';
 import {cloneDeep} from 'lodash';
 import {LocationService} from '../service/locationService';
 import {SummaryOfPublicationsService} from '../service/summaryOfPublicationsService';
-import fs from 'fs';
-import path from 'path';
 
-const urlLookup = JSON.parse(fs.readFileSync(path.resolve(__dirname,'../modules/nunjucks/listUrlLookup.json'), 'utf-8'));
 const summaryOfPublicationsService = new SummaryOfPublicationsService();
 const courtService = new LocationService();
 
@@ -19,21 +16,12 @@ export default class SummaryOfPublicationsController {
       const court = await courtService.getLocationById(parseInt(locationId.toString()));
       const locationName = (court == null ? 'Missing Court' : court.name);
       const publications = await summaryOfPublicationsService.getPublications(parseInt(locationId.toString()), (!!req.user));
-      if (publications.length === 1){
-        if (publications[0].isFlatFile){
-          res.redirect(`file-publication?artefactId=${publications[0].artefactId}`);
-        }
-        else {
-          res.redirect(`${urlLookup[publications[0].listType]}?artefactId=${publications[0].artefactId}`);
-        }
-      }
-      else {
-        res.render('summary-of-publications', {
-          ...cloneDeep(req.i18n.getDataByLanguage(req.lng)['summary-of-publications']),
-          publications,
-          locationName,
-        });
-      }
+
+      res.render('summary-of-publications', {
+        ...cloneDeep(req.i18n.getDataByLanguage(req.lng)['summary-of-publications']),
+        publications,
+        locationName,
+      });
     } else {
       res.render('error', req.i18n.getDataByLanguage(req.lng).error);
     }
