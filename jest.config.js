cd8785c96af2d3f53ba423d1de691845f9b33a5f
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2022-06-16 18:41:37 +0100

    Updated cookies

--- jest.config.js
+++ jest.config.js
@@ -6,11 +6,15 @@ module.exports = {
     "js"
   ],
   modulePathIgnorePatterns: ["<rootDir>/src/test/unit/mocks"],
+  preset: 'ts-jest/presets/js-with-ts',
   "testEnvironment": "jsdom",
   "testTimeout": 10000,
   transform: {
     '^.+\\.ts?$': 'ts-jest',
   },
+  "transformIgnorePatterns": [
+    "!node_modules/@hmcts/cookie-manager"
+  ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 
   collectCoverageFrom: [ "!**/modules/**" ],
--- src/test/unit/assets/cookie-preferences.test.ts
+++ src/test/unit/assets/cookie-preferences.test.ts
@@ -1,50 +1,18 @@
-// import sinon from 'sinon';
-//
-// const stub = sinon.stub(document, 'getElementById');
-//
-// // const cookieBanner = document.createElement('div');
-// // const cookieBannerMessage = document.createElement('div');
-// const acceptButton = document.createElement('button');
-// // const acceptedMessage = document.createElement('div');
-// // const rejectedButton = document.createElement('button');
-// // const rejectedMessage = document.createElement('div');
-// // const hideAcceptMessage = document.createElement('button');
-// // const hideRejectMessage = document.createElement('button');
-// // const cookiePolicyPageButton = document.createElement('button');
-// // const analyticsCookiesRadioButton = document.createElement('input');
-// // const performanceCookiesRadioButton = document.createElement('input');
-//
-// // cookieBanner.setAttribute('id', 'cookie-banner');
-// // cookieBannerMessage.setAttribute('id', 'cookie-banner-message');
-// acceptButton.setAttribute('classname', 'cookie-banner-accept-button');
-// // acceptedMessage.setAttribute('id', 'accept-message');
-// // rejectedButton.setAttribute('id', 'cookie-reject-analytics');
-// // rejectedMessage.setAttribute('id', 'reject-message');
-// // hideAcceptMessage.setAttribute('id', 'hide-message-accept');
-// // hideRejectMessage.setAttribute('id', 'hide-message-reject');
-// // cookiePolicyPageButton.setAttribute('id', 'cookie-save-button');
-// // analyticsCookiesRadioButton.setAttribute('id', 'analytics-cookies-options');
-// // performanceCookiesRadioButton.setAttribute('id', 'performance-cookies-options');
-//
-// // stub.withArgs('cookie-banner').returns(cookieBanner);
-// // stub.withArgs('cookie-banner-message').returns(cookieBannerMessage);
-// stub.withArgs('cookie-banner-accept-button').returns(acceptButton);
-// // stub.withArgs('accept-message').returns(acceptedMessage);
-// // stub.withArgs('cookie-reject-analytics').returns(rejectedButton);
-// // stub.withArgs('reject-message').returns(rejectedMessage);
-// // stub.withArgs('hide-message-accept').returns(hideAcceptMessage);
-// // stub.withArgs('hide-message-reject').returns(hideRejectMessage);
-// // stub.withArgs('cookie-save-button').returns(cookiePolicyPageButton);
-// // stub.withArgs('analytics-cookies-options').returns(analyticsCookiesRadioButton);
-// // stub.withArgs('performance-cookies-options').returns(performanceCookiesRadioButton);
-//
 describe('Cookie policy', () => {
 
   afterEach(() => {
     jest.resetModules();
   });
 
-  it('should display cookie banner if no cookie is set', async () => {
-    await import('../../../main/bundles/alphabetical');
+  it('should contain default preferences on page load', async () => {
+    window.dataLayer = [];
+
+    await import('../../../main/bundles/cookie-preferences');
+
+    expect(window.dataLayer.length).toEqual(1);
+
+    const defaultSetting = window.dataLayer[0];
+    expect(defaultSetting['event']).toEqual('Cookie Preferences');
+    expect(defaultSetting['cookiePreferences']).toEqual({'analytics': 'off', 'apm': 'off'});
   });
 });
