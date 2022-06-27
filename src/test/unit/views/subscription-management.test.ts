edd3dbf759581c401ce95d71087ed572853dd4ed
junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>
2022-06-27 17:04:58 +0100

    PUB-1303 Header changes to Email Subs page (#428)
    * Update Node.js to v16.15.1 (#415)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * fixed content and tests (#403)
    
    Co-authored-by: Josh <joshblackmoor@users.noreply.github.com>
    
    * Updated the footer (#419)
    
    Co-authored-by: ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
    
    * PUB-1253 Change DateTime format on different pages (#422)
    
    * change date format
    
    * fix date formatting
    
    * fix lint issue
    
    * fix merge conflict
    
    * fix build
    
    Co-authored-by: ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
    
    * PUB-1311 Remove collections page (#420)
    
    * Interstitial page removed and changed to home page
    
    * Added unit test
    
    * Fix conflicts
    
    * complete dev
    
    * add unit tests
    
    * fix lint styling issue
    
    * PUB-1286 - Removal of select all checkbox and alignment/text fixes (#401)
    
    * fixed all except alignment
    
    * done with no tests
    
    * added redis flag
    
    * added windows option
    
    * decapitalised result
    
    * Switched to a hard R again
    
    * removed result casename from checkbox label
    
    Co-authored-by: Josh <joshblackmoor@users.noreply.github.com>
    Co-authored-by: ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
    
    * Pub 1281 accessibility statement (#421)
    
    * marked up in a logical way
    
    * english content done
    
    * added welsh and fixed tests
    
    * new window
    
    Co-authored-by: ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
    
    * Updated multer dependency (#426)
    
    * Updated email within 5 days (#427)
    
    * Updated new courts (#430)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: furnivall <danielfurnivall@gmail.com>
    Co-authored-by: Josh <joshblackmoor@users.noreply.github.com>
    Co-authored-by: ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>

--- src/test/unit/views/subscription-management.test.ts
+++ src/test/unit/views/subscription-management.test.ts
@@ -12,6 +12,9 @@ const PAGE_URL = '/subscription-management';
 const expectedAllSubsTitle = 'All subscriptions (5)';
 const expectedCaseSubsTitle = 'Subscriptions by case (2)';
 const expectedCourtSubsTitle = 'Subscriptions by court or tribunal (3)';
+const expectedAllSubsTitleWithSingleSubs = 'All subscriptions (1)';
+const expectedCaseSubsTitleWithNoLocationSubs = 'Subscriptions by case (1)';
+const expectedCaseSubsTitleWithNoCaseSubs = 'Subscriptions by court or tribunal (1)';
 const expectedAddSubscriptionButton = 'Add email subscription';
 const tabsClass = 'moj-sub-navigation__link';
 const caseNameColumn = 'Case name';
@@ -24,14 +27,35 @@ const expectedRowCaseReference = 'T485913';
 const expectedRowDateAdded = moment('2022-01-14T11:30:12.357299').format('DD MMMM YYYY');
 const expectedRowCourtName = 'Court 1';
 const expectedCaseRowsCount = 2;
+const expectedCaseRowsCountWithoutLocation = 1;
 const expectedCourtRowsCount = 3;
+const expectedCourtRowsCountWithoutCaseSubs = 1;
 const expectedUnsubscribeLink = 'delete-subscription?subscription=5a45699f-47e3-4283-904a-581afe624155';
 const pageHeader = 'Your email subscriptions';
+
 const rawData = fs.readFileSync(path.resolve(__dirname, '../../../test/unit/mocks/userSubscriptions.json'), 'utf-8');
 const subscriptionsData = JSON.parse(rawData);
 const userSubscriptionsStub = sinon.stub(SubscriptionRequests.prototype, 'getUserSubscriptions');
+
 userSubscriptionsStub.withArgs('2').returns({caseSubscriptions:[], locationSubscriptions:[]});
 userSubscriptionsStub.withArgs('1').returns(subscriptionsData.data);
+
+userSubscriptionsStub.withArgs('3').returns(
+  {caseSubscriptions:[{
+    subscriptionId: '5a45699f-47e3-4283-904a-581afe624155',
+    caseName: 'Tom Clancy',
+    caseNumber: 'T485913',
+    urn: 'N363N6R4OG',
+    dateAdded: '2022-01-14T11:30:12.357299',
+  }], locationSubscriptions:[]});
+
+userSubscriptionsStub.withArgs('4').returns(
+  {caseSubscriptions:[], locationSubscriptions:[{
+    subscriptionId: 'f038b7ea-2972-4be4-a5ff-70abb4f78686',
+    locationName: 'Court 1',
+    dateAdded: '2022-01-14T11:42:57.847708',
+  }]});
+
 sinon.stub(expressRequest, 'isAuthenticated').returns(true);
 
 let htmlRes: Document;
@@ -193,5 +217,203 @@ describe('Subscriptions Management Page', () => {
       .getElementsByClassName('govuk-table__cell');
     expect(subscriptionCaseRowCells[0].innerHTML).contains(expectedRowCourtName);
     expect(subscriptionCaseRowCells[1].innerHTML).contains(expectedRowDateAdded);
+  });
+});
+
+describe('Subscriptions Management Page with case subscription but without location', () => {
+  beforeAll(async () => {
+    app.request['user'] = {piUserId: '3'};
+    await request(app).get(PAGE_URL).then(res => {
+      htmlRes = new DOMParser().parseFromString(res.text, 'text/html');
+      htmlRes.getElementsByTagName('div')[0].remove();
+    });
+  });
+
+  it('should display header', () => {
+    const header = htmlRes.getElementsByClassName('govuk-heading-l')[0];
+    expect(header.innerHTML)
+      .contains(pageHeader, 'Could not find correct value in header');
+  });
+
+  it('should have correct page title', () => {
+    const pageTitle = htmlRes.title;
+    expect(pageTitle).contains(pageHeader, 'Page title does not match header');
+  });
+
+  it('should display add subscription button', () => {
+    const newSubsButton = htmlRes.getElementsByClassName('govuk-button');
+    expect(newSubsButton[0].innerHTML)
+      .contains(expectedAddSubscriptionButton, 'Could not find new subscription button');
+  });
+
+  it('should display all subscriptions tab with proper link', () => {
+    const subscriptionsTabs = htmlRes.getElementsByClassName('moj-sub-navigation')[1]
+      .getElementsByClassName(tabsClass);
+    expect(subscriptionsTabs[0].innerHTML)
+      .contains(expectedAllSubsTitleWithSingleSubs, 'Could not find all subscriptions tab');
+    expect(subscriptionsTabs[0].getAttribute('href'))
+      .equal('?all', 'Tab does not contain proper link');
+  });
+
+  it('should display case subscriptions tab with proper link', () => {
+    const subscriptionsTabs = htmlRes.getElementsByClassName('moj-sub-navigation')[1]
+      .getElementsByClassName(tabsClass);
+    expect(subscriptionsTabs[1].innerHTML)
+      .contains(expectedCaseSubsTitleWithNoLocationSubs, 'Could not find case subscriptions tab');
+    expect(subscriptionsTabs[1].getAttribute('href'))
+      .equal('?case', 'Tab does not contain proper link');
+  });
+
+  it('should display first tab as active', () => {
+    const subscriptionsTabs = htmlRes.getElementsByClassName('moj-sub-navigation')[1]
+      .getElementsByClassName(tabsClass);
+    expect(subscriptionsTabs[0].getAttribute('aria-current'))
+      .equal('page', 'All subscriptions tab does not have active attribute');
+  });
+
+  it('should display case subscriptions table with 4 columns', () => {
+    const casesHeaders = htmlRes.getElementById('cases-table')
+      .getElementsByClassName('govuk-table__header');
+    expect(casesHeaders.length).equal(4);
+  });
+
+  it('should have correct columns in the cases table', () => {
+    const caseHeaders = htmlRes.getElementById('cases-table')
+      .getElementsByClassName('govuk-table__header');
+    expect(caseHeaders[0].innerHTML).contains(caseNameColumn, 'Case name header is not present');
+    expect(caseHeaders[1].innerHTML).contains(caseReferenceColumn, 'Case reference header is not present');
+    expect(caseHeaders[2].innerHTML).contains(dateAddedColumn, 'Date added header is not present');
+    expect(caseHeaders[3].innerHTML).contains(actionsColumn, 'Actions header is not present');
+  });
+
+  it('should not display court subscriptions table with 3 columns', () => {
+    const courtHeaders = htmlRes.getElementById('locations-table');
+    expect(courtHeaders).equal(null);
+  });
+
+  it('should have correct columns in the locations table', () => {
+    const courtHeaders = htmlRes.getElementById('locations-table');
+    expect(courtHeaders).equal(null, 'Court name header is not present');
+  });
+
+  it('requests cell should contain a link to delete subscription page', () => {
+    const actionsCell = htmlRes.getElementsByClassName('govuk-table__body')[0]
+      .getElementsByClassName('govuk-table__cell')[3];
+    expect(actionsCell.innerHTML).contains('Unsubscribe');
+    expect(actionsCell.querySelector('a').getAttribute('href')).equal(expectedUnsubscribeLink);
+  });
+
+  it('case table should have correct number of rows', () => {
+    const subscriptionsCaseRows = htmlRes.getElementsByClassName('govuk-table__body')[0]
+      .getElementsByClassName('govuk-table__row');
+    expect(subscriptionsCaseRows.length).equal(expectedCaseRowsCountWithoutLocation);
+  });
+
+  it('case table should have correct column values', () => {
+    const subscriptionCaseRowCells = htmlRes.getElementsByClassName('govuk-table__body')[0]
+      .getElementsByClassName('govuk-table__cell');
+    expect(subscriptionCaseRowCells[0].innerHTML).contains(expectedRowCaseName);
+    expect(subscriptionCaseRowCells[1].innerHTML).contains(expectedRowCaseReference);
+    expect(subscriptionCaseRowCells[2].innerHTML).contains(expectedRowDateAdded);
+  });
+
+  it('court table should have correct number of rows', () => {
+    const subscriptionsLocationRows = htmlRes.getElementsByClassName('govuk-table__body')[1];
+    expect(subscriptionsLocationRows).equal(undefined);
+  });
+});
+
+describe('Subscriptions Management Page with location subscription but without case', () => {
+  beforeAll(async () => {
+    app.request['user'] = {piUserId: '4'};
+    await request(app).get(PAGE_URL).then(res => {
+      htmlRes = new DOMParser().parseFromString(res.text, 'text/html');
+      htmlRes.getElementsByTagName('div')[0].remove();
+    });
+  });
+
+  it('should display header', () => {
+    const header = htmlRes.getElementsByClassName('govuk-heading-l')[0];
+    expect(header.innerHTML)
+      .contains(pageHeader, 'Could not find correct value in header');
+  });
+
+  it('should have correct page title', () => {
+    const pageTitle = htmlRes.title;
+    expect(pageTitle).contains(pageHeader, 'Page title does not match header');
+  });
+
+  it('should display add subscription button', () => {
+    const newSubsButton = htmlRes.getElementsByClassName('govuk-button');
+    expect(newSubsButton[0].innerHTML)
+      .contains(expectedAddSubscriptionButton, 'Could not find new subscription button');
+  });
+
+  it('should display all subscriptions tab with proper link', () => {
+    const subscriptionsTabs = htmlRes.getElementsByClassName('moj-sub-navigation')[1]
+      .getElementsByClassName(tabsClass);
+    expect(subscriptionsTabs[0].innerHTML)
+      .contains(expectedAllSubsTitleWithSingleSubs, 'Could not find all subscriptions tab');
+    expect(subscriptionsTabs[0].getAttribute('href'))
+      .equal('?all', 'Tab does not contain proper link');
+  });
+
+  it('should display court subscriptions tab with proper link', () => {
+    const subscriptionsTabs = htmlRes.getElementsByClassName('moj-sub-navigation')[1]
+      .getElementsByClassName(tabsClass);
+    expect(subscriptionsTabs[2].innerHTML)
+      .contains(expectedCaseSubsTitleWithNoCaseSubs, 'Could not find court subscriptions tab');
+    expect(subscriptionsTabs[2].getAttribute('href'))
+      .equal('?location', 'Tab does not contain proper link');
+  });
+
+  it('should display first tab as active', () => {
+    const subscriptionsTabs = htmlRes.getElementsByClassName('moj-sub-navigation')[1]
+      .getElementsByClassName(tabsClass);
+    expect(subscriptionsTabs[0].getAttribute('aria-current'))
+      .equal('page', 'All subscriptions tab does not have active attribute');
+  });
+
+  it('should not display case subscriptions table with 4 columns', () => {
+    const casesHeaders = htmlRes.getElementById('cases-table');
+    expect(casesHeaders).equal(null);
+  });
+
+  it('should not have columns in the cases table', () => {
+    const caseHeaders = htmlRes.getElementById('cases-table');
+    expect(caseHeaders).equal(null, 'Case name header is not present');
+  });
+
+  it('should display court subscriptions table with 3 columns', () => {
+    const courtHeaders = htmlRes.getElementById('locations-table')
+      .getElementsByClassName('govuk-table__header');
+    expect(courtHeaders.length).equal(3);
+  });
+
+  it('should have correct columns in the courts table', () => {
+    const courtHeaders = htmlRes.getElementById('locations-table')
+      .getElementsByClassName('govuk-table__header');
+    expect(courtHeaders[0].innerHTML).contains(courtNameColumn, 'Court name header is not present');
+    expect(courtHeaders[1].innerHTML).contains(dateAddedColumn, 'Date added header is not present');
+    expect(courtHeaders[2].innerHTML).contains(actionsColumn, 'Actions header is not present');
+  });
+
+  it('requests cell should contain a link to delete subscription page', () => {
+    const actionsCell = htmlRes.getElementsByClassName('govuk-table__body')[0]
+      .getElementsByClassName('govuk-table__cell')[2];
+    expect(actionsCell.innerHTML).contains('Unsubscribe');
+  });
+
+  it('court table should have correct number of rows', () => {
+    const subscriptionsLocationRows = htmlRes.getElementsByClassName('govuk-table__body')[0]
+      .getElementsByClassName('govuk-table__row');
+    expect(subscriptionsLocationRows.length).equal(expectedCourtRowsCountWithoutCaseSubs);
+  });
+
+  it('court table should have correct column values', () => {
+    const subscriptionLocationRowCells = htmlRes.getElementsByClassName('govuk-table__body')[0]
+      .getElementsByClassName('govuk-table__cell');
+    expect(subscriptionLocationRowCells[0].innerHTML).contains(expectedRowCourtName);
+    expect(subscriptionLocationRowCells[1].innerHTML).contains(expectedRowDateAdded);
   });
 });
