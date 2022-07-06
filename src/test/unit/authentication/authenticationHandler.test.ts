c9a5c02e736742c12adc53a6a7938169c5b05876
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2022-07-06 19:24:28 +0100

    Fixed authentication handler tests

---
+++ src/test/unit/authentication/authenticationHandler.test.ts
@@ -0,0 +1,242 @@
+import {expect} from 'chai';
+import {
+  checkRoles,
+  manualUploadRoles,
+  mediaAccountCreationRoles,
+  verifiedRoles,
+  checkAuthenticatedMedia,
+  checkAuthenticatedAdmin,
+  isPermittedMedia,
+  isPermittedAdmin, isPermittedAccountCreation, isPermittedManualUpload, isPermittedMediaAccount
+}
+  from '../../../main/authentication/authenticationHandler'
+
+describe('Test checking user roles', () => {
+
+  it('check that check roles returns true when matched', () => {
+    let req = {'user': {'_json': {'extension_UserRole': 'SYSTEM_ADMIN'}}}
+    expect(checkRoles(req, manualUploadRoles)).to.be.true;
+  });
+
+  it('check that check roles returns false when matched', () => {
+    let req = {'user': {'_json': {'extension_UserRole': 'SYSTEM_ADMIN'}}}
+    expect(checkRoles(req, mediaAccountCreationRoles)).to.be.false;
+  });
+
+  it('check that roles returns false when no user', () => {
+    expect(checkRoles({}, mediaAccountCreationRoles)).to.be.false;
+  });
+
+  it('check that roles returns false when no json', () => {
+    expect(checkRoles({'user': {}}, mediaAccountCreationRoles)).to.be.false;
+  });
+});
+
+describe('Test Authenticated Admin', () => {
+
+  it('check next is called if roles match', () => {
+    const mockNextFunction = jest.fn(() => 4);
+    let req = {'user': {'_json': {'extension_UserRole': 'SYSTEM_ADMIN'}}}
+    expect(checkAuthenticatedAdmin(req, {}, mockNextFunction, manualUploadRoles)).to.equal(4);
+  });
+
+  it('check redirect to admin dashboard if admin role but not permitted', () => {
+    const mockRedirectFunction = jest.fn((argument) => 4);
+
+    let req = {'user': {'_json': {'extension_UserRole': 'SYSTEM_ADMIN'}}}
+    let res = {'redirect': mockRedirectFunction}
+    checkAuthenticatedAdmin(req, res, mockRedirectFunction, mediaAccountCreationRoles);
+
+    expect(mockRedirectFunction.mock.calls.length).to.equal(1);
+    expect(mockRedirectFunction.mock.calls[0][0]).to.equal('/admin-dashboard');
+  });
+
+  it('check redirect to account home if verified role', () => {
+    const mockRedirectFunction = jest.fn((argument) => 4);
+
+    let req = {'user': {'_json': {'extension_UserRole': 'VERIFIED'}}}
+    let res = {'redirect': mockRedirectFunction}
+    checkAuthenticatedAdmin(req, res, mockRedirectFunction, mediaAccountCreationRoles);
+
+    expect(mockRedirectFunction.mock.calls.length).to.equal(1);
+    expect(mockRedirectFunction.mock.calls[0][0]).to.equal('/account-home');
+  });
+
+  it('check redirect to login if not authenticated', () => {
+    const mockRedirectFunction = jest.fn((argument) => 4);
+
+    let req = {};
+    let res = {'redirect': mockRedirectFunction}
+    checkAuthenticatedAdmin(req, res, mockRedirectFunction, mediaAccountCreationRoles);
+
+    expect(mockRedirectFunction.mock.calls.length).to.equal(1);
+    expect(mockRedirectFunction.mock.calls[0][0]).to.contains('/login?p=');
+  });
+
+});
+
+
+describe('Test Authenticated Media', () => {
+
+  it('check next is called if roles match', () => {
+    const mockNextFunction = jest.fn(() => 4);
+    let req = {'user': {'_json': {'extension_UserRole': 'VERIFIED'}}}
+    expect(checkAuthenticatedMedia(req, {}, mockNextFunction, verifiedRoles)).to.equal(4);
+  });
+
+  it('check redirect to admin dashboard if actually an admin role', () => {
+    const mockRedirectFunction = jest.fn((argument) => 4);
+
+    let req = {'user': {'_json': {'extension_UserRole': 'SYSTEM_ADMIN'}}}
+    let res = {'redirect': mockRedirectFunction}
+    checkAuthenticatedMedia(req, res, mockRedirectFunction, verifiedRoles);
+
+    expect(mockRedirectFunction.mock.calls.length).to.equal(1);
+    expect(mockRedirectFunction.mock.calls[0][0]).to.equal('/admin-dashboard');
+  });
+
+  it('check redirect to login if not authenticated', () => {
+    const mockRedirectFunction = jest.fn((argument) => 4);
+
+    let req = {};
+    let res = {'redirect': mockRedirectFunction}
+    checkAuthenticatedMedia(req, res, mockRedirectFunction, verifiedRoles);
+
+    expect(mockRedirectFunction.mock.calls.length).to.equal(1);
+    expect(mockRedirectFunction.mock.calls[0][0]).to.contains('/login?p=');
+  });
+
+});
+
+describe('Test IsPermittedMedia', () => {
+
+  it('check returns next function if permitted', () => {
+    const mockRedirectFunction = jest.fn((argument) => 4);
+    let req = {'user': {'_json': {'extension_UserRole': 'VERIFIED'}}}
+
+    expect(isPermittedMedia(req, {}, mockRedirectFunction)).to.equal(4);
+  });
+
+  it('check redirect to admin-dashboard is called if not matched', () => {
+    const mockRedirectFunction = jest.fn((argument) => 4);
+    let req = {'user': {'_json': {'extension_UserRole': 'SYSTEM_ADMIN'}}}
+    let res = {'redirect': mockRedirectFunction}
+
+    isPermittedMedia(req, res, mockRedirectFunction);
+
+    expect(mockRedirectFunction.mock.calls.length).to.equal(1);
+    expect(mockRedirectFunction.mock.calls[0][0]).to.equal('/admin-dashboard');
+  });
+
+});
+
+describe('Test IsPermittedAdmin', () => {
+
+  it('check returns next function if permitted', () => {
+    const mockRedirectFunction = jest.fn((argument) => 4);
+    let req = {'user': {'_json': {'extension_UserRole': 'SYSTEM_ADMIN'}}}
+
+    expect(isPermittedAdmin(req, {}, mockRedirectFunction)).to.equal(4);
+  });
+
+  it('check redirect to admin-dashboard is called if not matched', () => {
+    const mockRedirectFunction = jest.fn((argument) => 4);
+    let req = {'user': {'_json': {'extension_UserRole': 'VERIFIED'}}}
+    let res = {'redirect': mockRedirectFunction}
+
+    isPermittedAdmin(req, res, mockRedirectFunction);
+
+    expect(mockRedirectFunction.mock.calls.length).to.equal(1);
+    expect(mockRedirectFunction.mock.calls[0][0]).to.equal('/account-home');
+  });
+
+});
+
+describe('Test IsPermittedAccountCreation', () => {
+
+  it('check returns next function if permitted', () => {
+    const mockRedirectFunction = jest.fn((argument) => 4);
+    let req = {'user': {'_json': {'extension_UserRole': 'INTERNAL_SUPER_ADMIN_CTSC'}}}
+
+    expect(isPermittedAccountCreation(req, {}, mockRedirectFunction)).to.equal(4);
+  });
+
+  it('check redirect to admin-dashboard is called if not matched', () => {
+    const mockRedirectFunction = jest.fn((argument) => 4);
+    let req = {'user': {'_json': {'extension_UserRole': 'INTERNAL_ADMIN_LOCAL'}}}
+    let res = {'redirect': mockRedirectFunction}
+
+    isPermittedAccountCreation(req, res, mockRedirectFunction);
+
+    expect(mockRedirectFunction.mock.calls.length).to.equal(1);
+    expect(mockRedirectFunction.mock.calls[0][0]).to.equal('/admin-dashboard');
+  });
+
+});
+
+describe('Test IsPermittedAccountCreation', () => {
+
+  it('check returns next function if permitted', () => {
+    const mockRedirectFunction = jest.fn((argument) => 4);
+    let req = {'user': {'_json': {'extension_UserRole': 'INTERNAL_SUPER_ADMIN_CTSC'}}}
+
+    expect(isPermittedAccountCreation(req, {}, mockRedirectFunction)).to.equal(4);
+  });
+
+  it('check redirect to admin-dashboard is called if not matched', () => {
+    const mockRedirectFunction = jest.fn((argument) => 4);
+    let req = {'user': {'_json': {'extension_UserRole': 'INTERNAL_ADMIN_LOCAL'}}}
+    let res = {'redirect': mockRedirectFunction}
+
+    isPermittedAccountCreation(req, res, mockRedirectFunction);
+
+    expect(mockRedirectFunction.mock.calls.length).to.equal(1);
+    expect(mockRedirectFunction.mock.calls[0][0]).to.equal('/admin-dashboard');
+  });
+
+});
+
+describe('Test IsPermittedManualUpload', () => {
+
+  it('check returns next function if permitted', () => {
+    const mockRedirectFunction = jest.fn((argument) => 4);
+    let req = {'user': {'_json': {'extension_UserRole': 'INTERNAL_SUPER_ADMIN_CTSC'}}}
+
+    expect(isPermittedManualUpload(req, {}, mockRedirectFunction)).to.equal(4);
+  });
+
+  it('check redirect to admin-dashboard is called if not matched', () => {
+    const mockRedirectFunction = jest.fn((argument) => 4);
+    let req = {'user': {'_json': {'extension_UserRole': 'VERIFIED'}}}
+    let res = {'redirect': mockRedirectFunction}
+
+    isPermittedManualUpload(req, res, mockRedirectFunction);
+
+    expect(mockRedirectFunction.mock.calls.length).to.equal(1);
+    expect(mockRedirectFunction.mock.calls[0][0]).to.equal('/account-home');
+  });
+
+});
+
+describe('Test IsPermittedMediaAccount', () => {
+
+  it('check returns next function if permitted', () => {
+    const mockRedirectFunction = jest.fn((argument) => 4);
+    let req = {'user': {'_json': {'extension_UserRole': 'INTERNAL_ADMIN_CTSC'}}}
+
+    expect(isPermittedMediaAccount(req, {}, mockRedirectFunction)).to.equal(4);
+  });
+
+  it('check redirect to admin-dashboard is called if not matched', () => {
+    const mockRedirectFunction = jest.fn((argument) => 4);
+    let req = {'user': {'_json': {'extension_UserRole': 'INTERNAL_SUPER_ADMIN_LOCAL'}}}
+    let res = {'redirect': mockRedirectFunction}
+
+    isPermittedMediaAccount(req, res, mockRedirectFunction);
+
+    expect(mockRedirectFunction.mock.calls.length).to.equal(1);
+    expect(mockRedirectFunction.mock.calls[0][0]).to.equal('/admin-dashboard');
+  });
+
+});
+
