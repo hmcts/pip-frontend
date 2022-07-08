ed0ddb19ebd9e056a71cdfd4a5c8fdd4bcab5641
Daniel Furnivall <danielfurnivall@gmail.com>
2022-07-08 01:06:17 +0100

    added tests

---
+++ src/test/unit/views/password-change-confirmation.test.ts
@@ -0,0 +1,34 @@
+import { expect } from 'chai';
+import request from 'supertest';
+
+import { app } from '../../../main/app';
+
+const PAGE_URL = '/password-change-confirmation';
+const largeHeadingClass = 'govuk-heading-l';
+
+let htmlRes: Document;
+
+describe('password-change-confirmation', () => {
+  beforeAll(async () => {
+    await request(app).get(PAGE_URL).then(res => {
+      htmlRes = new DOMParser().parseFromString(res.text, 'text/html');
+    });
+  });
+
+  it('should display the page header', () => {
+    const header = htmlRes.getElementsByClassName(largeHeadingClass);
+    expect(header[0].innerHTML).contains('Password changed successfully', 'Could not find the header');
+  });
+
+  it('should display the body text', () => {
+    const bodyText = htmlRes.getElementsByClassName('govuk-body');
+    expect(bodyText[4].innerHTML).contains('You can now sign in using your new credentials using the button below.',
+      'Could not find body text');
+  });
+
+  it('should display the button', () => {
+    const button = htmlRes.getElementsByClassName('govuk-button');
+    expect(button[4].textContent).contains('Sign in', 'Could not find button');
+    expect(button[4].outerHTML).contains('B2C_1_SignInUserFlow');
+  });
+});
