ee1eaf9132fed359295bce67c6968695c4275a4d
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2022-07-27 14:57:03 +0100

    PUB-1478 - Removed error logs that could contain emails

--- src/main/resources/requests/accountManagementRequests.ts
+++ src/main/resources/requests/accountManagementRequests.ts
@@ -15,16 +15,16 @@ export class AccountManagementRequests {
   public async createAzureAccount(payload, requester): Promise<object | null> {
     try {
       const response = await accountManagementApi.post('/account/add/azure', payload, {headers: {'x-issuer-id': requester}});
-      logger.info('azure account created', response);
+      logger.info('Azure account created');
       return response.data;
     }
     catch (error) {
       if (error.response) {
-        logger.error('failed to create azure account on response', error.response.data);
+        logger.error('Failed to create azure account on response');
       } else if (error.request) {
-        logger.error('failed to create azure account on request', error.request);
+        logger.error('Failed to create azure account on request');
       } else {
-        logger.error('failed to create azure account with message', error.message);
+        logger.error('Failed to create azure account with message');
       }
       return null;
     }
@@ -38,16 +38,16 @@ export class AccountManagementRequests {
   public async createPIAccount(payload, requester): Promise<boolean> {
     try {
       const response = await accountManagementApi.post('/account/add/pi', payload, {headers: {'x-issuer-id': requester}});
-      logger.info('P&I account created', response);
+      logger.info('P&I account created');
       return response.status === 201;
     }
     catch (error) {
       if (error.response) {
-        logger.error('failed to create admin P&I on response', error.response.data);
+        logger.error('Failed to create admin P&I on response');
       } else if (error.request) {
-        logger.error('failed to create admin P&I on request', error.request);
+        logger.error('Failed to create admin P&I on request');
       } else {
-        logger.error('failed to create admin P&I with message', error.message);
+        logger.error('Failed to create admin P&I with message');
       }
       return false;
     }
@@ -67,11 +67,11 @@ export class AccountManagementRequests {
       return true;
     } catch (error) {
       if (error.response) {
-        logger.error('failed to create media account on response', error.response.data);
+        logger.error('Failed to create media account on response');
       } else if (error.request) {
-        logger.error('failed to create media account on request', error.request);
+        logger.error('Failed to create media account on request');
       } else {
-        logger.error('failed to create media account with message', error.message);
+        logger.error('Failed to create media account with message');
       }
       return false;
     }
@@ -80,16 +80,16 @@ export class AccountManagementRequests {
   public async getMediaApplicationById(applicationId): Promise<MediaAccountApplication | null> {
     try {
       const response = await accountManagementApi.get('/application/' + applicationId);
-      logger.info('Media Application accessed - ' + applicationId, response);
+      logger.info('Media Application accessed - ' + applicationId);
       return response.data;
     }
     catch (error) {
       if (error.response) {
-        logger.error('failed to retrieve media application', error.response.data);
+        logger.error('Failed to retrieve media application', error.response.data);
       } else if (error.request) {
-        logger.error('failed to retrieve media application', error.request);
+        logger.error('Failed to retrieve media application', error.request);
       } else {
-        logger.error('failed to retrieve media application', error.message);
+        logger.error('Failed to retrieve media application', error.message);
       }
       return null;
     }
@@ -98,15 +98,15 @@ export class AccountManagementRequests {
   public async getMediaApplicationImageById(imageId): Promise<Blob> {
     try{
       const response = await accountManagementApi.get('/application/image/' + imageId, {responseType: 'arraybuffer'});
-      logger.info('Media Application image access with ID - ' + imageId, response);
+      logger.info('Media Application image access with ID - ' + imageId);
       return response.data;
     } catch (error) {
       if (error.response) {
-        logger.error('failed to retrieve media application image - response', error.response.data);
+        logger.error('Failed to retrieve media application image - response', error.response.data);
       } else if (error.request) {
-        logger.error('failed to retrieve media application image - request', error.request);
+        logger.error('Failed to retrieve media application image - request', error.request);
       } else {
-        logger.error('failed to retrieve media application image - message', error.message);
+        logger.error('Failed to retrieve media application image - message', error.message);
       }
     }
     return null;
@@ -115,15 +115,15 @@ export class AccountManagementRequests {
   public async updateMediaApplicationStatus(applicantId, status): Promise<MediaAccountApplication | null> {
     try {
       const response = await accountManagementApi.put('/application/' + applicantId + '/' + status);
-      logger.info('Media Application updated - ' + applicantId, response);
+      logger.info('Media Application updated - ' + applicantId);
       return response.data;}
     catch (error) {
       if (error.response) {
-        logger.error('failed to update media application', error.response.data);
+        logger.error('Failed to update media application', error.response.data);
       } else if (error.request) {
-        logger.error('failed to update media application', error.request);
+        logger.error('Failed to update media application', error.request);
       } else {
-        logger.error('failed to update media application', error.message);
+        logger.error('Failed to update media application', error.message);
       }
     }
     return null;
