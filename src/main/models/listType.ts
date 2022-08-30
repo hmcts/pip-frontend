ec51e5478b130beeaad94cba23d9e6f4106adfe5
KianKwa <kian.kwa@justice.gov.uk>
2022-08-30 11:20:03 +0100

    Add SJP press register

---
+++ src/main/models/listType.ts
@@ -0,0 +1,9 @@
+export interface ListType {
+  friendlyName: string;
+  shortenedFriendlyName: string;
+  url: string;
+  jurisdictions: Array<string>;
+  restrictedProvenances: Array<string>;
+  artefactType: string;
+  checked: boolean;
+}
