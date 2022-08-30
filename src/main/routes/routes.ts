da77e329a916129981ea171dd10d6ac8ca7cb427
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2022-08-30 14:40:50 +0100

    PUB-1430 - Fix Language switch

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -58,7 +58,7 @@ export default function(app: Application): void {
     logger.info('logout FE URL', FRONTEND_URL);
 
     const B2C_URL = config.get('secrets.pip-ss-kv.B2C_URL');
-    const encodedSignOutRedirect = encodeURIComponent(redirectUrl);
+    const encodedSignOutRedirect = encodeURIComponent(redirectUrl + '?lng=' + _req.lng);
     logger.info('B2C_URL', B2C_URL);
     logger.info('encodedSignOutRedirect', encodedSignOutRedirect);
     res.redirect(`${B2C_URL}/${authenticationConfig.POLICY}/oauth2/v2.0/logout?post_logout_redirect_uri=${encodedSignOutRedirect}`);
