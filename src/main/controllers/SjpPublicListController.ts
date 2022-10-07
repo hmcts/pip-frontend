2c853a73348d3098b0ce7ca49a59eabed8bca690
KianKwa <80688761+KianKwa@users.noreply.github.com>
2022-10-07 11:58:17 +0100

    PUB-1550 Handle array of SJP public list cases (#541)
    * Handle array of SJP public list cases
    
    * Add missing town and county to mock SJP test file
    
    Co-authored-by: ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>

--- src/main/controllers/SjpPublicListController.ts
+++ src/main/controllers/SjpPublicListController.ts
@@ -2,13 +2,12 @@ import {PipRequest} from '../models/request/PipRequest';
 import {Response} from 'express';
 import {cloneDeep} from 'lodash';
 import {PublicationService} from '../service/publicationService';
-import { DataManipulationService } from '../service/dataManipulationService';
+import {DataManipulationService} from '../service/dataManipulationService';
 
 const publicationService = new PublicationService();
 const dataManipulationService = new DataManipulationService();
 
 export default class SjpPublicListController {
-
   public async get(req: PipRequest, res: Response): Promise<void> {
     const artefactId = req.query['artefactId'];
     const fileData = await publicationService.getIndividualPublicationJson(artefactId, req.user?.['piUserId']);
@@ -17,14 +16,13 @@ export default class SjpPublicListController {
     if (fileData && metaData) {
       const publishedTime = dataManipulationService.publicationTimeInBst(fileData['document']['publicationDate']);
       const publishedDate = dataManipulationService.publicationDateInBst(fileData['document']['publicationDate']);
+      const casesCount = SjpPublicListController.getCasesCount(fileData);
 
-      const data = fileData['courtLists'][0]['courtHouse']['courtRoom'][0]['session'][0]['sittings'];
-      const length = data.length;
       res.render('single-justice-procedure', {
         ...cloneDeep(req.i18n.getDataByLanguage(publicationService.languageToLoadPageIn(metaData.language,
           req.lng))['single-justice-procedure']),
-        casesList: data,
-        length: length,
+        sjpData: fileData,
+        length: casesCount,
         publishedDateTime: publishedDate,
         publishedTime: publishedTime,
       });
@@ -32,5 +30,19 @@ export default class SjpPublicListController {
       res.render('error',
         req.i18n.getDataByLanguage(req.lng).error);
     }
+  }
+
+  private static getCasesCount(sjpData: object): number {
+    let totalCases = 0;
+    sjpData['courtLists'].forEach(courtList => {
+      courtList['courtHouse']['courtRoom'].forEach(courtRoom => {
+        courtRoom['session'].forEach(session => {
+          session['sittings'].forEach(sitting => {
+            totalCases += sitting['hearing'].length;
+          });
+        });
+      });
+    });
+    return totalCases;
   }
 }
