56c7258a97ac2d2b66f567e449ac752eb3a64450
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2022-10-25 16:25:19 +0100

    PUB-1608 - Initial Super Admin portal

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -10,6 +10,7 @@ import {
   isPermittedMediaAccount,
   isPermittedAccountCreation,
   isPermittedManualUpload,
+  isPermittedSystemAdmin,
   forgotPasswordRedirect,
   mediaVerificationHandling,
   processAdminAccountSignIn,
@@ -161,6 +162,10 @@ export default function(app: Application): void {
   app.post('/remove-list-search', isPermittedManualUpload, app.locals.container.cradle.removeListSearchController.post);
   app.get('/remove-list-search-results', isPermittedManualUpload, app.locals.container.cradle.removeListSearchResultsController.get);
   app.get('/remove-list-success', isPermittedManualUpload, app.locals.container.cradle.removeListSuccessController.get);
+
+  //system-admin-restricted-paths
+  app.get('/system-admin-dashboard', isPermittedSystemAdmin, app.locals.container.cradle.systemAdminDashboardController.get);
+
 
   app.get('/info', infoRequestHandler({
     extraBuildInfo: {
