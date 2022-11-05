eb99ee4cd9ebba17aa280a2af8940d8fdd827d14
Daniel Furnivall <danielfurnivall@gmail.com>
2022-11-05 21:16:08 +0000

    finished for the day - matching data with outside

---
+++ src/main/controllers/CrownFirmListController.ts
@@ -0,0 +1,42 @@
+import {Response} from 'express';
+import {PipRequest} from '../models/request/PipRequest';
+import {cloneDeep} from 'lodash';
+import moment from 'moment';
+import { PublicationService } from '../service/publicationService';
+import { LocationService } from '../service/locationService';
+import { DataManipulationService } from '../service/dataManipulationService';
+
+const publicationService = new PublicationService();
+const locationService = new LocationService();
+const dataManipulationService = new DataManipulationService();
+
+export default class CrownFirmListController {
+  public async get(req: PipRequest, res: Response): Promise<void> {
+    const artefactId = req.query.artefactId as string;
+    const jsonData = await publicationService.getIndividualPublicationJson(artefactId, req.user?.['piUserId']);
+    const metaData = await publicationService.getIndividualPublicationMetadata(artefactId, req.user?.['piUserId']);
+
+    if (jsonData && metaData) {
+      const outputData = dataManipulationService.manipulatedDailyListData(JSON.stringify(jsonData));
+      const publishedTime = dataManipulationService.publicationTimeInBst(jsonData['document']['publicationDate']);
+      const publishedDate = dataManipulationService.publicationDateInBst(jsonData['document']['publicationDate']);
+      const location = await locationService.getLocationById(metaData['locationId']);
+      const pageLanguage = publicationService.languageToLoadPageIn(metaData.language, req.lng);
+
+      res.render('crown-firm-list', {
+        ...cloneDeep(req.i18n.getDataByLanguage(pageLanguage)['crown-firm-list']),
+        listData: outputData,
+        contentDate: moment.utc(Date.parse(metaData['contentDate'])).format('DD MMMM YYYY'),
+        publishedDate: publishedDate,
+        publishedTime: publishedTime,
+        provenance: metaData['provenance'],
+        version: jsonData['document']['version'],
+        courtName: location.name,
+        bill: pageLanguage === 'bill',
+      });
+    } else {
+      res.render('error',
+        req.i18n.getDataByLanguage(req.lng).error);
+    }
+  }
+}
