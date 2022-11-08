616508e038d98fdc4cbce16557f8214f3274a80b
junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>
2022-11-08 10:35:13 +0000

    PUB-1609 Reference Data Manual Upload (#561)
    * merge reference manual upload code
    
    * add a11y tests
    
    * add unit tests
    
    * fix build
    
    * fix build
    
    * add test reference data file
    
    * fix the link
    
    Co-authored-by: ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>

---
+++ src/main/controllers/ManualReferenceDataUploadController.ts
@@ -0,0 +1,44 @@
+import {PipRequest} from '../models/request/PipRequest';
+import {Response} from 'express';
+import {cloneDeep} from 'lodash';
+import {FileHandlingService} from '../service/fileHandlingService';
+import {uploadType} from '../models/consts';
+
+const fileHandlingService = new FileHandlingService();
+export default class ManualReferenceDataUploadController {
+  public async get(req: PipRequest, res: Response): Promise<void> {
+    const formValues = {
+      ...cloneDeep(req.i18n.getDataByLanguage(req.lng)['manual-reference-data-upload']),
+    };
+    res.render('manual-reference-data-upload', formValues);
+  }
+
+  public async post(req: PipRequest, res: Response): Promise<void> {
+    if (req.query?.showerror === 'true') {
+      res.render('error', req.i18n.getDataByLanguage(req.lng).error);
+    } else {
+      const errors = {
+        fileErrors: fileHandlingService.validateFileUpload(req.file, req.lng as string, 'manual-reference-data-upload',
+          uploadType.REFERENCE_DATE),
+      };
+
+      const formValues = {
+        ...cloneDeep(req.i18n.getDataByLanguage(req.lng)['manual-reference-data-upload']),
+        errors,
+        formData: req.body,
+      };
+
+      if (errors.fileErrors) {
+        res.render('manual-reference-data-upload', formValues);
+      } else {
+        const originalFileName = req.file['originalname'];
+        const sanitisedFileName = fileHandlingService.sanitiseFileName(originalFileName);
+        await fileHandlingService.storeFileIntoRedis(req.user['oid'], originalFileName, sanitisedFileName);
+
+        req.body['fileName'] = originalFileName;
+        res.cookie('formCookie', JSON.stringify(req.body));
+        res.redirect('/manual-reference-data-upload-summary?check=true');
+      }
+    }
+  }
+}
