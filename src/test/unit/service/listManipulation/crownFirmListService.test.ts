c82cc0bfa59a97fe8562f2492e4e06c65db23e96
Daniel Furnivall <danielfurnivall@gmail.com>
2022-11-09 20:09:41 +0000

    tests galore

--- src/test/unit/service/listManipulation/crownFirmListService.test.ts
+++ src/test/unit/service/listManipulation/crownFirmListService.test.ts
@@ -4,40 +4,51 @@ import path from 'path';
 import {CrownFirmListService} from '../../../../main/service/listManipulation/crownFirmListService';
 
 const crownFirmListService = new CrownFirmListService();
-const rawCrownFirmData = fs.readFileSync(path.resolve(__dirname, '../../mocks/crownFirmList.json'), 'utf-8');
-
+const rawCrownFirmData = fs.readFileSync(path.resolve(__dirname, '../../mocks/crownFirmSemiDigested.json'), 'utf-8');
+const row = JSON.parse( `{"caseNumber": "I4Y416QE", "caseSeparator": "[7 of 9]",
+"courtName": "Glasgow Social Security and Child Support",
+"courtRoom": "to be allocated [Glasgow Social Security and Child Support]",
+"defendant": "SAUSAGE, Nigel", "defendantRepresentative": "Daniel Furnivall", "durationAsHours": 2,
+"durationAsMinutes": 23, "formattedDuration": "2 hours 23 mins", "hearingNotes": "",
+"hearingPlatform": "Teams, Attended", "hearingType": "Deposition", "joh": "",
+"linkedCases": "YRYCTRR3, YRYCTRR4, YRYCTRR5", "prosecutingAuthority": "The Queen",
+"sittingDate": "Saturday 15 April 2023", "sittingTime": "2:41pm"}
+`);
 const lng = 'en';
 const languageFile = 'crown-firm-list';
 
-describe('Crown firm list splitter service', async () => {
+describe('Crown firm list splitter service', () => {
 
-  const data = await crownFirmListService.splitOutFirmListData(JSON.stringify(JSON.parse(rawCrownFirmData)), lng, languageFile);
   describe('Crown firm list splitter service', () => {
 
-    it('should format the case time in 12 hour format', async () => {
-      expect(data['courtLists'][0]['courtHouse']['courtRoom'][0]['session'][0]['sittings'][0]['time']).to.equal('10:40am');
-      expect(data['courtLists'][2]['courtHouse']['courtRoom'][0]['session'][0]['sittings'][0]['time']).to.equal('1:00pm');
+    it('should split the data down to the day, courtroom and courthouse level', async () => {
+      const data = await crownFirmListService.splitOutFirmListData(rawCrownFirmData, lng, languageFile);
+      expect(data[1]['days'][1][2]['data'][0]).to.deep.equal(row);
     });
 
-    it('should formatted the party information correctly for prosecution authority and defendant', async () => {
-      expect(data['courtLists'][0]['courtHouse']['courtRoom'][0]['session'][0]['sittings'][0]['hearing'][0]['defendant']).to.equal('Defendant_SN, Defendant_FN');
-      expect(data['courtLists'][0]['courtHouse']['courtRoom'][0]['session'][0]['sittings'][0]['hearing'][0]['prosecutingAuthority']).to.equal('Pro_Auth_SN, Pro_Auth_FN');
+    it('should split the initial json file down to two courtHouses', async () => {
+      const data = await crownFirmListService.splitOutFirmListData(JSON.stringify(JSON.parse(rawCrownFirmData)), lng, languageFile);
+      expect(data.length).to.equal(2);
+      expect(data[0].courtName).equal('Colchester Social Security and Child Support');
+      expect(data[1].courtName).equal('Glasgow Social Security and Child Support');
     });
 
-    it('should be able to find linked cases for a particular case', async () => {
-      expect(data['courtLists'][0]['courtHouse']['courtRoom'][0]['session'][0]['sittings'][0]['hearing'][0]['case'][0]['linkedCases']).to.equal('caseid111, caseid222');
-      expect(data['courtLists'][0]['courtHouse']['courtRoom'][0]['session'][0]['sittings'][0]['hearing'][0]['case'][1]['linkedCases']).to.equal('');
+    it('should split by date on each courtHouse object', async () => {
+      const data = await crownFirmListService.splitOutFirmListData(JSON.stringify(JSON.parse(rawCrownFirmData)), lng, languageFile);
+      expect(data[0].days.length).to.equal(1);
+      expect(data[1].days.length).to.equal(2);
     });
 
-    it('should be able to find listing notes for a particular hearing', async () => {
-      expect(data['courtLists'][0]['courtHouse']['courtRoom'][0]['session'][0]['sittings'][0]['hearing'][0]['listingNotes']).to.equal('Listing details text');
-      expect(data['courtLists'][0]['courtHouse']['courtRoom'][0]['session'][0]['sittings'][0]['hearing'][1]['listingNotes']).to.equal('');
+    it('should capture the correct courtRoom name at the day level', async () => {
+      const data = await crownFirmListService.splitOutFirmListData(JSON.stringify(JSON.parse(rawCrownFirmData)), lng, languageFile);
+      expect(data[0].days[0][0].courtRoom).equal('Courtroom 2');
+      expect(data[0].days[0][1].courtRoom).equal('to be allocated [Colchester Social Security and Child Support]');
     });
 
-    it('should append the unallocated cases at the bottom of the courtList', async () => {
-      expect(data['courtLists'].length).to.equal(5);
-      expect(data['courtLists'][4]['unallocatedCases']).to.equal(true);
-      expect(data['courtLists'][4]['courtHouse']['courtRoom'][0]['courtRoomName']).to.equal('to be allocated');
+    it("should append the unallocated cases at the bottom of a given day's cases", async () => {
+      const data = await crownFirmListService.splitOutFirmListData(JSON.stringify(JSON.parse(rawCrownFirmData)), lng, languageFile);
+      expect(data[0].days[0][1].courtRoom).contains('to be allocated');
+      expect(data[1].days[1][2].courtRoom).contains('to be allocated');
     });
   });
 });
---
+++ src/test/unit/views/crown-firm-list.test.ts
@@ -0,0 +1,107 @@
+import { expect } from 'chai';
+import request from 'supertest';
+import { app } from '../../../main/app';
+import fs from 'fs';
+import path from 'path';
+import sinon from 'sinon';
+import {PublicationService} from '../../../main/service/publicationService';
+import {LocationService} from '../../../main/service/locationService';
+
+const PAGE_URL = '/crown-firm-list?artefactId=abc';
+const headingClass = 'govuk-heading-l';
+const restrictionHeading = 'govuk-grid restriction-list-section';
+const accordionClass='govuk-accordion__section-button';
+const siteAddressClass = 'site-address';
+
+const courtName = 'Abergavenny Magistrates\' Court';
+const expectedHeader = courtName + ': Firm List';
+const restrictionHeadingText = 'Restrictions on publishing or writing about these cases';
+
+let htmlRes: Document;
+
+const rawData = fs.readFileSync(path.resolve(__dirname, '../mocks/crownFirmList.json'), 'utf-8');
+const crownFirmListData = JSON.parse(rawData);
+const rawMetaData = fs.readFileSync(path.resolve(__dirname, '../mocks/returnedArtefacts.json'), 'utf-8');
+const metaData = JSON.parse(rawMetaData)[0];
+
+const rawDataCourt = fs.readFileSync(path.resolve(__dirname, '../mocks/courtAndHearings.json'), 'utf-8');
+const courtData = JSON.parse(rawDataCourt);
+
+sinon.stub(PublicationService.prototype, 'getIndividualPublicationJson').returns(crownFirmListData);
+sinon.stub(PublicationService.prototype, 'getIndividualPublicationMetadata').returns(metaData);
+sinon.stub(LocationService.prototype, 'getLocationById').resolves(courtData[0]);
+
+describe('Crown firm List page', () => {
+  beforeAll(async () => {
+    await request(app).get(PAGE_URL).then(res => {
+      htmlRes = new DOMParser().parseFromString(res.text, 'text/html');
+    });
+  });
+
+  it('should display header',  () => {
+    const header = htmlRes.getElementsByClassName(headingClass);
+    expect(header[0].innerHTML).contains(expectedHeader, 'Could not find the header');
+  });
+
+  it('should display restriction heading',  () => {
+    const restriction = htmlRes.getElementsByClassName(restrictionHeading);
+    expect(restriction[0].innerHTML).contains(restrictionHeadingText, 'Could not find the display restriction heading');
+  });
+
+  it('should display the site name for both courtHouses',  () => {
+    const siteAddress = htmlRes.getElementsByClassName(siteAddressClass);
+    expect(siteAddress[0].innerHTML).contains('Colchester', 'Could not find the site name in section 1');
+    expect(siteAddress[1].innerHTML).contains('Glasgow', 'Could not find the site name in section 1');
+  });
+
+  it('should display the "sitting at" section for both sections',  () => {
+    const siteAddress = htmlRes.getElementsByClassName(siteAddressClass);
+    expect(siteAddress[1].innerHTML).contains('Sitting at', 'Could not find the Court name with Sitting at text');
+    expect(siteAddress[0].innerHTML).contains('Sitting at', 'Could not find the Court name with Sitting at text');
+  });
+
+  it('should display accordion court name',  () => {
+    const accordion = htmlRes.getElementsByClassName(accordionClass);
+    expect(accordion[0].innerHTML).to.contain('Courtroom 2:', 'Could not find the accordion heading');
+  });
+
+  it('should display second half of accordion title',  () => {
+    const accordion = htmlRes.getElementsByClassName(accordionClass);
+    expect(accordion[0].innerHTML).to.contains('Thomas Athorne, Reginald Cork', 'Could not find the accordion heading');
+  });
+
+  it('should display Sitting at time',  () => {
+    const cell = htmlRes.getElementsByClassName('govuk-table__cell');
+    expect(cell[0].innerHTML).contains('9am');
+  });
+
+  it('should display Case Reference',  () => {
+    const cell = htmlRes.getElementsByClassName('govuk-table__cell');
+    expect(cell[1].innerHTML).contains('I4Y416QE');
+  });
+
+  it('should display Defendant Name',  () => {
+    const cell = htmlRes.getElementsByClassName('govuk-table__cell');
+    expect(cell[2].innerHTML).equal('CORA, Mckinley');
+  });
+
+  it('should display Hearing Type',  () => {
+    const cell = htmlRes.getElementsByClassName('govuk-table__cell');
+    expect(cell[3].innerHTML).contains('Directions');
+  });
+
+  it('should display Case Sequence Indicator if it is there',  () => {
+    const cell = htmlRes.getElementsByClassName('govuk-table__cell');
+    expect(cell[4].innerHTML).contains('[4 of 5]');
+  });
+
+  it('should display Case name with Case Sequence Indicator if it is there',  () => {
+    const cell = htmlRes.getElementsByClassName('govuk-table__cell');
+    expect(cell[4].innerHTML).equal('8 hours [4 of 5]');
+  });
+
+  it('should display Prosecuting Authority',  () => {
+    const cell = htmlRes.getElementsByClassName('govuk-table__cell');
+    expect(cell[6].innerHTML).contains('Queen');
+  });
+});
