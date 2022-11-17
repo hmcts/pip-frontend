9cdfdb457fbce7312ee9291af12e0ba717020b14
Daniel Furnivall <danielfurnivall@gmail.com>
2022-11-17 13:38:17 +0000

    fix on routes?

--- src/test/routes/blob-view-locations.test.ts
+++ src/test/routes/blob-view-locations.test.ts
@@ -1,9 +1,11 @@
 import { expect } from 'chai';
 import request from 'supertest';
-
+import sinon from 'sinon';
 import { app } from '../../main/app';
+import { PublicationRequests } from '../../main/resources/requests/publicationRequests';
 
 describe('Admin rejected login page', () => {
+  sinon.stub(PublicationRequests.prototype, 'getPubsPerLocation').returns('location,count\n1,2\n3,1\n');
   describe('on GET', () => {
     test('should return blob-view-locations page', async () => {
       app.request['user'] = {
