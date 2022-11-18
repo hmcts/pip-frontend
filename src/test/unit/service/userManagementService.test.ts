60c20f7aa5c0a3e7c8e462e18ca4bbf1fc8de82f
Josh Blackmoor <joshblackmoor01@icloud.com>
2022-11-18 20:23:20 +0000

    Service test

--- src/test/unit/service/userManagementService.test.ts
+++ src/test/unit/service/userManagementService.test.ts
@@ -3,10 +3,18 @@ import {UserManagementService} from '../../../main/service/userManagementService
 
 const userManagementService = new UserManagementService();
 
+const testReqBody = {
+  email: 'josh',
+  userId: '',
+  userProvenanceId: '',
+  roles: ['VERIFIED', 'INTERNAL_ADMIN_CTSC'],
+  provenances: 'PI_AAD',
+};
+
 describe('User management service', () => {
   it('should return the correct table headers', () => {
     const response = userManagementService.getTableHeaders();
-    
+
     expect(response[0].text).to.equal('Email');
     expect(response[0].classes).to.equal('govuk-!-padding-top-0');
     expect(response[1].text).to.equal('Role');
@@ -26,5 +34,13 @@ describe('User management service', () => {
     expect(response[5].value).to.equal('SYSTEM_ADMIN');
     expect(response[5].text).to.equal('System Admin');
     expect(response[5].selected).to.equal(true);
+  });
+
+  it('should generate the filter key values', () => {
+    // Have to parse JSON in this way to fully replicate req.body
+    const response = userManagementService.generateFilterKeyValues(JSON.parse(JSON.stringify(testReqBody)));
+    expect(response).to.contain('email=josh');
+    expect(response).to.contain('&roles=VERIFIED,INTERNAL_ADMIN_CTSC');
+    expect(response).to.contain('&provenances=PI_AAD');
   });
 });
