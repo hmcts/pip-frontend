8bbb51a6f05d2480184da34102fcc94cebcce239
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2022-11-21 09:49:52 +0000

    Added two more route tests

---
+++ src/test/routes/manage-third-party-users-subscription.test.ts
@@ -0,0 +1,41 @@
+import {app} from '../../main/app';
+import request from 'supertest';
+import {expect} from 'chai';
+import {request as expressRequest} from 'express';
+import {ThirdPartyService} from '../../main/service/thirdPartyService';
+import sinon from 'sinon';
+import {SubscriptionService} from '../../main/service/subscriptionService';
+import {PublicationService} from '../../main/service/publicationService';
+
+describe('Manage third party users subscription', () => {
+
+  const userId = '1234-1234';
+
+  expressRequest['user'] = {'_json': {
+    'extension_UserRole': 'SYSTEM_ADMIN',
+  }};
+
+  const getThirdPartyUserByIdStub = sinon.stub(ThirdPartyService.prototype, 'getThirdPartyUserById');
+  const getListTypesStub = sinon.stub(PublicationService.prototype, 'getListTypes');
+  const getSubscriptionsByUserStub = sinon.stub(SubscriptionService.prototype, 'getSubscriptionsByUser');
+  const getChannelsListStub = sinon.stub(SubscriptionService.prototype, 'retrieveChannels');
+  const generateListTypesStub = sinon.stub(ThirdPartyService.prototype, 'generateListTypes');
+  const generateAvailableChannelsStub = sinon.stub(ThirdPartyService.prototype, 'generateAvailableChannels');
+
+  getThirdPartyUserByIdStub.withArgs(userId).resolves({'userId': userId});
+  getListTypesStub.resolves(['LIST_A', 'LIST_B']);
+  getSubscriptionsByUserStub.withArgs(userId).resolves({listTypeSubscriptions: []});
+  getChannelsListStub.resolves(['CHANNEL_A', 'EMAIL']);
+  generateListTypesStub.withArgs(['LIST_A', 'LIST_B'], {listTypeSubscriptions: []}).returns({});
+  generateAvailableChannelsStub.withArgs(['CHANNEL_A'], {listTypeSubscriptions: []}).returns({});
+
+  describe('on GET', () => {
+
+    test('should return manage third party users subscription page', async () => {
+      await request(app)
+        .get('/manage-third-party-users/subscriptions?userId=1234-1234')
+        .expect((res) => expect(res.status).to.equal(200));
+    });
+  });
+
+});
---
+++ src/test/routes/manage-third-party-users-view.test.ts
@@ -0,0 +1,34 @@
+import {app} from '../../main/app';
+import request from 'supertest';
+import {expect} from 'chai';
+import {request as expressRequest} from 'express';
+import {ThirdPartyService} from '../../main/service/thirdPartyService';
+import sinon from 'sinon';
+import {SubscriptionService} from '../../main/service/subscriptionService';
+
+describe('Manage third party users view', () => {
+
+  describe('on GET', () => {
+
+    const userId = '1234-1234';
+
+    const mockUser = {'userId': userId};
+    const mockSubscriptions = {listTypeSubscriptions: []};
+
+    expressRequest['user'] = {'_json': {
+      'extension_UserRole': 'SYSTEM_ADMIN',
+    }};
+
+    const getThirdPartyByUserIdStub = sinon.stub(ThirdPartyService.prototype, 'getThirdPartyUserById');
+    const getSubscriptionsByUserStub = sinon.stub(SubscriptionService.prototype, 'getSubscriptionsByUser');
+    getThirdPartyByUserIdStub.withArgs(userId).resolves(mockUser);
+    getSubscriptionsByUserStub.withArgs(userId).resolves(mockSubscriptions);
+
+    test('should return manage third party users view page', async () => {
+      await request(app)
+        .get('/manage-third-party-users/view?userId=1234-1234')
+        .expect((res) => expect(res.status).to.equal(200));
+    });
+  });
+
+});
