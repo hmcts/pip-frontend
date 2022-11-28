549ba3ab85c5593dd569cdf52c75a8255cb55bf0
KianKwa <kian.kwa@justice.gov.uk>
2022-11-28 16:24:03 +0000

    Update selector for e2e test

--- src/test/e2e/Helpers/Selectors.ts
+++ src/test/e2e/Helpers/Selectors.ts
@@ -213,7 +213,7 @@ module.exports = {
   ManageThirdPartyUsers: '#card-manage-third-party-users',
   referenceDataFileUpload: '#manual-reference-data-upload',
   userManagement: '#card-user-management',
-  BulkCreateMediaAccounts: '#bulk-create-media-accounts',
+  BulkCreateMediaAccounts: '#card-bulk-create-media-accounts',
 
   //UserManagement screens selectors
   manageLink: '#manage-link',
