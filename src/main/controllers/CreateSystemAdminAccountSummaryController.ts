3b3c431e2c992d14fab2ce65b78c55d5bc09a86e
ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
2022-12-14 12:54:19 +0000

    PUB-1624 System Admin Controls (#645)
    * Add initial requests
    
    * PUB-1624 - Updated frontend with new pages
    
    * Updated messages
    
    * Updated messages
    
    * Fixed linting, removed uneeded imports
    
    * PUB-1624 - Added unit tests
    
    Co-authored-by: Josh <joshblackmoor@users.noreply.github.com>

--- src/main/controllers/CreateSystemAdminAccountSummaryController.ts
+++ src/main/controllers/CreateSystemAdminAccountSummaryController.ts
@@ -21,17 +21,25 @@ export default class CreateSystemAdminAccountSummaryController {
 
     formData['userRoleObject'] = {mapping: 'SYSTEM_ADMIN'};
 
-    const response = await createAccountService.createAdminAccount(formData, req.user?.['userId']);
+    const response = await createAccountService.createSystemAdminAccount(formData, req.user?.['userId']);
     if (response) {
-      res.cookie('createAdminAccount', '');
+      if (response['error'] && !response['duplicate'] && !response['aboveMaxSystemAdmin']) {
+        res.render('create-system-admin-account-summary', {
+          formData,
+          displayError: true,
+          ...cloneDeep(req.i18n.getDataByLanguage(req.lng)['create-system-admin-account-summary']),
+        });
+      } else {
+        res.cookie('createAdminAccount', '');
+        res.render('create-system-admin-account-confirm', {
+          formData,
+          accountCreated: !response['error'] && !response['duplicate'] && !response['aboveMaxSystemAdmin'],
+          isDuplicateError: response['duplicate'],
+          isAboveMaxError: response['aboveMaxSystemAdmin'],
+          ...cloneDeep(req.i18n.getDataByLanguage(req.lng)['create-system-admin-account-confirm']),
+        });
+      }
     }
-
-    res.render('create-system-admin-account-summary', {
-      formData,
-      accountCreated: response,
-      displayError: !response,
-      ...cloneDeep(req.i18n.getDataByLanguage(req.lng)['create-system-admin-account-summary']),
-    });
 
   }
 }
