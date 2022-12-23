bc378c44bd33c068ffbb3414c6764eb99df09f8e
ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
2022-12-23 15:34:55 +0000

    PUB-1721 - Upgrade Axios (#672)
    * PUB-1721 - Upgrade Axios
    
    * Updated jest
    
    * Update to route tests
    
    * Update accessibility tests

--- jest.a11y.config.js
+++ jest.a11y.config.js
@@ -1,9 +1,13 @@
 module.exports = {
+  transformIgnorePatterns: ['/node_modules/(?!(axios)/)'],
   roots: ['<rootDir>/src/test/a11y'],
   testRegex: "(/src/test/.*|\\.(test|spec))\\.(ts|js)$",
   testEnvironment: "node",
   transform: {
     '^.+\\.ts?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^axios$': require.resolve('axios'),
   },
   moduleFileExtensions: ['ts', 'js'],
   setupFiles: ["<rootDir>/jestEnvVars.js"],
--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,14 @@
 module.exports = {
+  transformIgnorePatterns: ['/node_modules/(?!(axios)/)'],
   roots: ['<rootDir>/src/test/unit'],
   "testRegex": "(/src/test/.*|\\.(test|spec))\\.(ts|js)$",
    "moduleFileExtensions": [
     "ts",
     "js"
   ],
+  moduleNameMapper: {
+    '^axios$': require.resolve('axios'),
+  },
   modulePathIgnorePatterns: ["<rootDir>/src/test/unit/mocks"],
   "testEnvironment": "jsdom",
   "testTimeout": 10000,
