2af0f45946ba633d1b484b6db4adcc182c439f95
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2023-01-17 09:23:48 +0000

    Updated cookies flag

--- src/main/app.ts
+++ src/main/app.ts
@@ -60,6 +60,7 @@ logger.info('SESSION Secret', config.get('secrets.pip-ss-kv.SESSION_SECRET'));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use((req, res, next) => {
+  req['sessionCookies'].secure = true;
   res.setHeader(
     'Cache-Control',
     'no-cache, max-age=0, must-revalidate, no-store',
