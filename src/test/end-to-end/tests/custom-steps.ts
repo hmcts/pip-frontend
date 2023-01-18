7d336973db1e7797a98141082643aa20a4d41671
ashwini.venkatesha <ashwini.venkatesha@justice.gov.uk>
2023-01-18 12:12:28 +0000

    Added functional tests for admin login feature

--- src/test/end-to-end/tests/custom-steps.ts
+++ src/test/end-to-end/tests/custom-steps.ts
@@ -8,6 +8,15 @@ export = function () {
       this.fillField('#password', password);
       this.click('Sign in');
     },
+
+    loginAsAdmin: function (username=testConfig.ADMIN_USERNAME, password=testConfig.ADMIN_PASSWORD) {
+      this.amOnPage('/admin-dashboard');
+      this.see('Sign in with your email address');
+      this.fillField('#email', username);
+      this.fillField('#password', password);
+      this.click('Sign in');
+    },
+
     logout: function () {
       this.click('Sign out');
       this.see('You have been signed out');
