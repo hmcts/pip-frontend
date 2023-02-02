96f0351c4390b21db43c14fe2f13f9e59baef831
Daniel Furnivall <danielfurnivall@gmail.com>
2023-02-02 18:23:13 +0000

    supps python script

---
+++ .lefthook/pre-push/check-for-unsuppressed-vulns.py
@@ -0,0 +1,32 @@
+import subprocess
+import json
+
+print("Script running: Checking current vulnerabilities and comparing with current suppressions.")
+
+suppressions = "cat yarn-audit-known-issues"
+audit = "yarn audit --json"
+def read_in_json(audit, cmd):
+  current_script = subprocess.Popen([cmd], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  x = current_script.stdout.read().splitlines()
+  current_ids = {}
+  iterator_final_val = len(x)
+  if audit:
+    # yarn audit returns a final json lump at the end of all the advisories - a summary we don't want to use.
+    iterator_final_val = -1
+    if len(x) == 0:
+      print("No existing vulnerabilities found - Nice job!")
+      exit()
+  for i in x[:iterator_final_val]:
+    current_advisory = json.loads(i.decode('utf-8'))
+    current_advisory_id = current_advisory['data']['resolution']['id']
+    current_advisory_cve = current_advisory['data']['advisory']['cves']
+    if current_advisory_id not in current_ids:
+      current_ids[current_advisory_id] = current_advisory_cve
+  return(current_ids)
+
+audit_results = read_in_json(True, audit)
+suppression_results = read_in_json(False, suppressions)
+
+for i, j in audit_results.items():
+  if i not in suppression_results or j not in suppression_results.values():
+    print(f"New unsuppressed advisory - {i} -> {j}")
