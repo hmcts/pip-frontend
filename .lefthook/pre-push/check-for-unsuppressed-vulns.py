31a1b806822fe74354c0aab063e6ab0facfe00dd
Daniel Furnivall <danielfurnivall@gmail.com>
2023-02-02 19:10:14 +0000

    supps

--- .lefthook/pre-push/check-for-unsuppressed-vulns.py
+++ .lefthook/pre-push/check-for-unsuppressed-vulns.py
@@ -7,16 +7,16 @@ suppressions = "cat yarn-audit-known-issues"
 audit = "yarn audit --json"
 def read_in_json(audit, cmd):
   current_script = subprocess.Popen([cmd], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-  x = current_script.stdout.read().splitlines()
+  output = current_script.stdout.read().splitlines()
   current_ids = {}
-  iterator_final_val = len(x)
+  iterator_final_val = len(output)
   if audit:
-    # yarn audit returns a final json lump at the end of all the advisories - a summary we don't want to use.
-    iterator_final_val = -1
-    if len(x) == 0:
+    if iterator_final_val == 0:
       print("No existing vulnerabilities found - Nice job!")
       exit()
-  for i in x[:iterator_final_val]:
+    # yarn audit returns a final json lump at the end of all the advisories - a summary we don't want to use.
+    iterator_final_val = -1
+  for i in output[:iterator_final_val]:
     current_advisory = json.loads(i.decode('utf-8'))
     current_advisory_id = current_advisory['data']['resolution']['id']
     current_advisory_cve = current_advisory['data']['advisory']['cves']
