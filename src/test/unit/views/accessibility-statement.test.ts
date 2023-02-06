ba1e9afe6b742db12540c802d05d1b23f85979bc
Daniel Furnivall <danielfurnivall@gmail.com>
2023-02-06 14:53:18 +0000

    new line for bump

--- src/test/unit/views/accessibility-statement.test.ts
+++ src/test/unit/views/accessibility-statement.test.ts
@@ -14,6 +14,7 @@ describe('accessibility-statement', () => {
             .get(PAGE_URL)
             .then(res => {
                 htmlRes = new DOMParser().parseFromString(res.text, 'text/html');
+
             });
     });
 
