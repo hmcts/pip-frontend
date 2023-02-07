00d13b0e60c535eaf7498cb8c93540ab6a543926
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2023-02-07 16:07:10 +0000

    remove code duplication

--- src/main/controllers/DeleteCourtSubscriptionConfirmationController.ts
+++ src/main/controllers/DeleteCourtSubscriptionConfirmationController.ts
@@ -11,35 +11,23 @@ export default class DeleteCourtSubscriptionConfirmationController {
     public async post(req: PipRequest, res: Response): Promise<void> {
         const formData = req.body;
         const court = await locationService.getLocationById(formData.locationId as unknown as number);
-        switch (formData['delete-choice']) {
-            case 'yes': {
-                const response = await subscriptionsService.deleteLocationSubscription(
-                    formData.locationId,
-                    req.user?.['provenanceUserId']
-                );
-                if (response === null) {
-                    res.render('delete-court-subscription-confirmation', {
-                        ...cloneDeep(req.i18n.getDataByLanguage(req.lng)['delete-court-subscription-confirmation']),
-                        court: locationService.formatCourtValue(court),
-                        apiError: true,
-                        errorMessage: 'Unknown error when attempting to delete all the subscription for the court',
-                    });
-                } else {
-                    res.redirect('/delete-court-subscription-success');
-                }
-                break;
-            }
-            case 'no': {
-                res.redirect('/delete-court-reference-data');
-                break;
-            }
-            default:
-                res.render('delete-court-subscription-confirmation', {
-                    ...cloneDeep(req.i18n.getDataByLanguage(req.lng)['delete-court-subscription-confirmation']),
-                    court: locationService.formatCourtValue(court),
-                    apiError: false,
-                    displayError: true,
-                });
+        if (formData['delete-choice'] == 'yes') {
+          const response = await subscriptionsService.deleteLocationSubscription(
+            formData.locationId,
+            req.user?.['provenanceUserId']
+          );
+          if (response === null) {
+            res.render('delete-court-subscription-confirmation', {
+              ...cloneDeep(req.i18n.getDataByLanguage(req.lng)['delete-court-subscription-confirmation']),
+              court: locationService.formatCourtValue(court),
+              apiError: true,
+              errorMessage: 'Unknown error when attempting to delete all the subscription for the court',
+            });
+          } else {
+            res.redirect('/delete-court-subscription-success');
+          }
+        } else {
+          res.redirect('/delete-court-reference-data');
         }
     }
 }
