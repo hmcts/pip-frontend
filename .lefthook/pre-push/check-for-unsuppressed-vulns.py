75dc398667b51f995b77a4d52fb508c885496807
Daniel Furnivall <danielfurnivall@gmail.com>
2023-02-08 15:56:35 +0000

    Python refactored

--- .lefthook/pre-push/check-for-unsuppressed-vulns.py
+++ .lefthook/pre-push/check-for-unsuppressed-vulns.py
@@ -12,7 +12,8 @@ header = '''
 
 '''
 colors = ['\033[94m', '\033[96m', '\033[92m', '\033[93m', '\033[91m', '\033[95m']
-print(f"\n{colors[5]}{header}\nChecking all your code for currently known vulnerabilities and comparing with those you've elected to already suppress...")
+print(
+  f"\n{colors[5]}{header}\nChecking all your code for currently known vulnerabilities and comparing with those you've elected to already suppress...")
 print("This should only take a few seconds.")
 
 suppressions = "cat yarn-audit-known-issues"
@@ -43,67 +44,61 @@ class advisory:
           sep=" || ")
 
 
-def read_in_json_yarn_v3(audit, cmd):
-  current_script = subprocess.Popen([cmd], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-  output = current_script.stdout.read()
-  advisoryList = []
+def handle_empty_bodies(audit, output):
   if len(output) == 0:
     if audit:
       print(f"{colors[2]}Nice one! All your dependencies are up to date!")
+      return False
     else:
       print(f"\n{colors[3]}Note: You have an empty suppressions file.{colors[5]}")
-      return []
+      return True
+  return False
 
+
+def getRawData(cmd):
+  current_script = subprocess.Popen([cmd], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  return current_script.stdout.read()
+
+
+def build_advisory(data):
+  return advisory(module_name=data['module_name'],
+                  severity=data['severity'].title(),
+                  vulnerable_versions=data['vulnerable_versions'],
+                  cves=data['cves'],
+                  id=data['id'])
+
+
+def read_in_json_yarn_v3(audit, cmd):
+  output = getRawData(cmd)
+  advisoryList = []
+  empty_files = handle_empty_bodies(audit, output)
+  if empty_files:
+    return advisoryList
   current_advisory = json.loads(output.decode('utf-8'))['advisories']
   for i in current_advisory.keys():
     data = current_advisory[i]
-    this_advisory = advisory(module_name=data['module_name'],
-                             severity=data['severity'].title(),
-                             vulnerable_versions=data['vulnerable_versions'],
-                             cves=data['cves'],
-                             id=data['id'])
-    advisoryList.append(this_advisory)
+    advisoryList.append(build_advisory(data))
   return advisoryList
 
 
-def read_in_json(audit, cmd):
-  current_script = subprocess.Popen([cmd], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-  output = current_script.stdout.read().splitlines()
-  current_ids = {}
-  iterator_final_val = len(output)
-  if audit:
-    if iterator_final_val == 0:
-      print("No existing vulnerabilities found - Nice job!")
-      exit()
-    # yarn audit returns a final json lump at the end of all the advisories - a summary we don't want to use.
-    iterator_final_val = -1
-  for i in output[:iterator_final_val]:
-    decoded_advisory = i.decode('utf-8')
-    current_advisory = json.loads(decoded_advisory)
-    current_advisory_id = current_advisory['data']['resolution']['id']
-    current_advisory_cve = current_advisory['data']['advisory']['cves']
-    if current_advisory_id not in current_ids:
-      current_ids[current_advisory_id] = current_advisory_cve
-  return (current_ids)
+def run_audit_and_suppression_checks():
+  audit_results = read_in_json_yarn_v3(True, audit)
+  suppression_results = read_in_json_yarn_v3(False, suppressions)
+
+  unsuppressed = [i for i in audit_results if i not in suppression_results]
+  unused = [i for i in suppression_results if i not in audit_results]
+  return unsuppressed, unused
+
+def print_outputs(unsuppressed, unused):
+  for i in unused:
+    print(f'\n{colors[0]}Note: You have {len(unused)} unused suppressions - you can probably delete those safely.')
+    i.ugly_print()
+
+  print(f'\n{colors[4]}Warning! You have {len(unsuppressed)} unsuppressed vulnerabilities:')
+  for i, j in enumerate(unsuppressed):
+    j.pretty_print(colors[0] + str(i))
+
+print_outputs(*run_audit_and_suppression_checks())
 
 
-audit_results = read_in_json_yarn_v3(True, audit)
-suppression_results = read_in_json_yarn_v3(False, suppressions)
 
-unsuppressed = [i for i in audit_results if i not in suppression_results]
-unused = [i for i in suppression_results if i not in audit_results]
-for i in unused:
-  i.ugly_print()
-
-print(f'\n{colors[4]}Warning! You have {len(unsuppressed)} unsuppressed vulnerabilities:')
-for i, j in enumerate(unsuppressed):
-  j.pretty_print(colors[0] + str(i))
-
-
-# def yarn_v1_steps():
-# for i, j in audit_results.items()
-# print(f"New unsuppressed advisory - {i} -> {j}")
-
-# for i, j in suppression_results.items():
-#   if i not in audit_results or j not in audit_results.values():
-#     print(f"Unused suppression found - {i} -> {j}")
