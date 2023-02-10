8a5e7fca815a6e0d374489a5a84bda175ad83173
KianKwa <kian.kwa@justice.gov.uk>
2023-02-10 17:28:02 +0000

    Update SJP press controller tests

--- src/test/unit/controllers/SjpPressListController.test.ts
+++ src/test/unit/controllers/SjpPressListController.test.ts
@@ -6,6 +6,8 @@ import path from 'path';
 import { PublicationService } from '../../../main/service/publicationService';
 import { mockRequest } from '../mocks/mockRequest';
 import { DateTime } from 'luxon';
+import {FilterService} from '../../../main/service/filterService';
+import {SjpFilterService} from '../../../main/service/sjpFilterService';
 
 const rawData = fs.readFileSync(path.resolve(__dirname, '../mocks/SJPMockPage.json'), 'utf-8');
 const sjpData = JSON.parse(rawData);
@@ -17,6 +19,9 @@ const sjpPressListController = new SjpPressListController();
 
 const sjpPressListJsonStub = sinon.stub(PublicationService.prototype, 'getIndividualPublicationJson');
 const sjpPressListMetaDataStub = sinon.stub(PublicationService.prototype, 'getIndividualPublicationMetadata');
+
+const filter = {sjpCases: ['1', '2'], filterOptions: {}};
+sinon.stub(SjpFilterService.prototype, 'generateFilters').returns(filter);
 
 const artefactId = 'abc';
 
@@ -31,126 +36,85 @@ const i18n = {
     'list-template': {},
 };
 
-const sjpCases = [
-    {
-        name: 'Danny Thomas',
-        dob: '25 July 1985',
-        age: '37',
-        caseUrn: 'ABC12345',
-        address: 'Swansea, SA1 1AA',
-        postcode: 'SA1 1AA',
-        organisationName: 'qU8QlEo',
-        offences: [
-            {
-                reportingRestrictionFlag: 'True',
-                offenceTitle: 'This is an offence title',
-                offenceWording: 'This is offence wording',
-            },
-        ],
-    },
-    {
-        name: 'Mr Christopher W Robin',
-        dob: '25 July 1985',
-        age: '37',
-        caseUrn: 'ABC12345',
-        address: 'London, London, E15 3AR',
-        postcode: 'E15 3AR',
-        organisationName: 'qU8QlEo@',
-        offences: [
-            {
-                reportingRestrictionFlag: 'True',
-                offenceTitle: 'This is an offence title',
-                offenceWording: 'This is offence wording',
-            },
-        ],
-    },
-];
-
-const filters = {
-    postcodes: [
-        {
-            value: 'E15 3AR',
-            text: 'E15 3AR',
-            checked: false,
-        },
-        {
-            value: 'SA1 1AA',
-            text: 'SA1 1AA',
-            checked: false,
-        },
-    ],
-    prosecutors: [
-        {
-            value: 'qU8QlEo',
-            text: 'qU8QlEo',
-            checked: false,
-        },
-        {
-            value: 'qU8QlEo@',
-            text: 'qU8QlEo@',
-            checked: false,
-        },
-    ],
-};
-
 describe('SJP Press List Controller', () => {
     const response = {
         render: () => {
             return '';
         },
+      redirect: function () {
+        return '';
+      },
     } as unknown as Response;
 
+  describe('get', () => {
     it('should render the SJP press list page', async () => {
-        const request = mockRequest(i18n);
-        request.user = { userId: '1' };
+      const request = mockRequest(i18n);
+      request.user = {userId: '1'};
 
-        request.query = { artefactId: artefactId };
+      request.query = {artefactId: artefactId};
 
-        const responseMock = sinon.mock(response);
+      const responseMock = sinon.mock(response);
 
-        const expectedData = {
-            ...i18n['single-justice-procedure-press'],
-            ...i18n['list-template'],
-            sjpData: sjpCases,
-            totalHearings: 2,
-            publishedDateTime: '14 September 2016',
-            publishedTime: '12:30am',
-            contactDate: DateTime.fromISO(metaData['contentDate'], {
-                zone: 'utc',
-            }).toFormat('d MMMM yyyy'),
-            artefactId: 'abc',
-            user: request.user,
-            filters: filters,
-        };
+      const expectedData = {
+        ...i18n['single-justice-procedure-press'],
+        ...i18n['list-template'],
+        sjpData: filter.sjpCases,
+        totalHearings: 2,
+        publishedDateTime: '14 September 2016',
+        publishedTime: '12:30am',
+        contactDate: DateTime.fromISO(metaData['contentDate'], {
+          zone: 'utc',
+        }).toFormat('d MMMM yyyy'),
+        artefactId: 'abc',
+        user: request.user,
+        filters: filter.filterOptions,
+      };
 
-        responseMock.expects('render').once().withArgs('single-justice-procedure-press', expectedData);
+      responseMock.expects('render').once().withArgs('single-justice-procedure-press', expectedData);
 
-        await sjpPressListController.get(request, response);
-        return responseMock.verify();
+      await sjpPressListController.get(request, response);
+      return responseMock.verify();
     });
 
     it('should render error page is query param is empty', async () => {
-        const request = mockRequest(i18n);
-        request.query = {};
-        request.user = { userId: '1' };
+      const request = mockRequest(i18n);
+      request.query = {};
+      request.user = {userId: '1'};
 
-        const responseMock = sinon.mock(response);
+      const responseMock = sinon.mock(response);
 
-        responseMock.expects('render').once().withArgs('error', request.i18n.getDataByLanguage(request.lng).error);
+      responseMock.expects('render').once().withArgs('error', request.i18n.getDataByLanguage(request.lng).error);
 
-        await sjpPressListController.get(request, response);
-        return responseMock.verify();
+      await sjpPressListController.get(request, response);
+      return responseMock.verify();
     });
 
     it('should render error page if list is not allowed to view by the user', async () => {
-        const request = mockRequest(i18n);
-        request.query = {};
+      const request = mockRequest(i18n);
+      request.query = {};
 
-        const responseMock = sinon.mock(response);
+      const responseMock = sinon.mock(response);
 
-        responseMock.expects('render').once().withArgs('error', request.i18n.getDataByLanguage(request.lng).error);
+      responseMock.expects('render').once().withArgs('error', request.i18n.getDataByLanguage(request.lng).error);
 
-        await sjpPressListController.get(request, response);
-        return responseMock.verify();
+      await sjpPressListController.get(request, response);
+      return responseMock.verify();
     });
+  });
+
+  describe('post', () => {
+    it('should redirect to configure list page with correct filters', () => {
+      const request = mockRequest(i18n);
+      request.query = {artefactId: artefactId};
+
+      sinon.stub(FilterService.prototype, 'generateFilterKeyValues').withArgs(request.body).returns('TestValue');
+
+      const responseMock = sinon.mock(response);
+      responseMock.expects('redirect').once().withArgs('sjp-press-list?artefactId=abc&filterValues=TestValue');
+
+      return sjpPressListController.filterValues(request, response).then(() => {
+        responseMock.verify();
+      });
+    });
+  });
 });
