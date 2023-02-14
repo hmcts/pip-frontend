76d1df309b8454c0d5120d39e99c811538a3a8d3
KianKwa <kian.kwa@justice.gov.uk>
2023-02-14 22:28:07 +0000

    Add new POST route test for SJP press list

--- src/test/routes/single-justice-procedure-press.test.ts
+++ src/test/routes/single-justice-procedure-press.test.ts
@@ -7,6 +7,7 @@ import { PublicationService } from '../../main/service/publicationService';
 import fs from 'fs';
 import path from 'path';
 import { SjpPressListService } from '../../main/service/listManipulation/SjpPressListService';
+import {SjpFilterService} from '../../main/service/sjpFilterService';
 
 const rawData = fs.readFileSync(path.resolve(__dirname, '../unit/mocks/SJPMockPage.json'), 'utf-8');
 const sjpPressData = JSON.parse(rawData);
@@ -14,7 +15,9 @@ const sjpPressData = JSON.parse(rawData);
 sinon.stub(PublicationService.prototype, 'getIndividualPublicationJson').resolves(sjpPressData);
 sinon.stub(PublicationService.prototype, 'getIndividualPublicationMetadata').resolves(sjpPressData);
 sinon.stub(SjpPressListService.prototype, 'formatSJPPressList').resolves([]);
-sinon.stub(SjpPressListService.prototype, 'generateFilters').resolves({});
+
+const filter = { sjpCases: ['1', '2'], filterOptions: {} };
+sinon.stub(SjpFilterService.prototype, 'generateFilters').returns(filter);
 
 describe('Single Justice Procedure Press Page', () => {
     describe('on GET', () => {
@@ -23,6 +26,18 @@ describe('Single Justice Procedure Press Page', () => {
             await request(app)
                 .get('/sjp-press-list?artefactId=test')
                 .expect(res => expect(res.status).to.equal(200));
+        });
+    });
+
+    describe('on POST', () => {
+        test('should redirect to Single Justice Procedure Press page', async () => {
+            app.request['user'] = { userId: '2' };
+            await request(app)
+                .post('/sjp-press-list?artefactId=test&filterValues=AA1')
+                .expect(res => {
+                    expect(res.status).to.equal(302);
+                    expect(res.header['location']).contains('sjp-press-list');
+                });
         });
     });
 });
