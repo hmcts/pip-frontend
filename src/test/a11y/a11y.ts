faf91010b012b51cb3e42af0cd5408980df10e59
Daniel Furnivall <danielfurnivall@gmail.com>
2023-02-22 15:01:31 +0000

    test

--- src/test/a11y/a11y.ts
+++ src/test/a11y/a11y.ts
@@ -176,6 +176,7 @@ export function ensurePageCallWillSucceed(url: string): Promise<void> {
 export function runPally(url: string): Pa11yResult {
     return pa11y(url, {
         hideElements: '.govuk-footer__licence-logo, .govuk-header__logotype-crown',
+	log: {debug: console.log, error: console.error, info: console.info}
     });
 }
 
