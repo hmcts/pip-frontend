de4873f58219c12f76879a4e06fc1652808e48f0
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2023-03-07 16:57:50 +0000

    PUB-1807 - Updated test

---
+++ src/test/end-to-end/tests/change-own-user-test.ts
@@ -0,0 +1,13 @@
+import { config as testConfig } from '../../config';
+
+Feature('Change own user role');
+
+Scenario('I as an admin should be able to see an error message if I try to change my own user', async ({ I }) => {
+    I.loginAsAdmin();
+    I.click('#card-admin-management');
+    I.fillField('#search-input', testConfig.ADMIN_USERNAME as string);
+    I.click('Continue');
+    I.click('Change');
+    I.click('Continue');
+    I.see('You are unable to update the role for the same user you are logged in as');
+}).tag('@Nightly');
