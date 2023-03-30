97cb558426b2e08baae3fdbbb70b4cc605b5fe0c
ashwini-mv <84072035+ashwini-mv@users.noreply.github.com>
2023-03-30 14:18:56 +0100

    Implemented cross-browser testing (#843)

---
+++ src/test/end-to-end/crossbrowser.conf.ts
@@ -0,0 +1,27 @@
+import * as path from 'path';
+import { config as testConfig } from '../config';
+import { container, event } from 'codeceptjs';
+
+export const config: CodeceptJS.MainConfig = {
+  name: 'cross-browser',
+  tests: './tests/*-test.ts',
+  output: path.join(testConfig.TestFunctionalOutputPath, 'cross-browser/reports'),
+    include: {
+        I: './tests/custom-steps.ts',
+    },
+  helpers: testConfig.helpers,
+  plugins: testConfig.plugins,
+  multiple: {
+    chromium: { browsers: [{ browser: 'chromium' }] },
+    webkit: { browsers: [{ browser: 'webkit' }] },
+    firefox: { browsers: [{ browser: 'firefox' }] },
+  },
+};
+
+event.dispatcher.on(event.test.after, () => {
+  const browser = container.helpers().Playwright.browser._initializer;
+  const { allure } = container.plugins();
+  allure.epic(browser.name);
+  allure.addParameter('environment', 'Browser', browser.name);
+  allure.addParameter('environment', 'Version', browser.version);
+});
