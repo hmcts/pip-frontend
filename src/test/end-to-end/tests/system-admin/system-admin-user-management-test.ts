1e98f5a492070bf103b0c70bf69cb2f19e5da03c
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2023-04-14 16:08:07 +0100

    PUB-1834 - Updated tags

--- src/test/end-to-end/tests/system-admin/system-admin-user-management-test.ts
+++ src/test/end-to-end/tests/system-admin/system-admin-user-management-test.ts
@@ -1,28 +1,20 @@
 import { config as testConfig } from '../../../config';
+import {randomData} from "../../shared/random-data";
 
 Feature('System admin User Management');
 
 const testFirstName = 'System Admin Test First Name';
 const testLastName = 'System Admin Test Surname';
-const testEmailAddress = 'pip-e2e-test-admin-management@hmcts.net';
 
+
+const testEmailAddress = 'pip-e2e-test-admin-management-' + randomData.getRandomNumber(1, 10000) + '@hmcts.net';
 const systemAdminUsername = testConfig.SYSTEM_ADMIN_USERNAME as string;
 
 Scenario('I as a system admin should be able to update a users role and delete a user', async ({ I }) => {
     I.loginAsSystemAdmin();
-    I.click(locate('a').withAttr({ href: '/admin-dashboard' }));
-    I.see('Your Dashboard');
-    I.click('#card-create-admin-account');
-    I.fillField('#firstName', testFirstName);
-    I.fillField('#lastName', testLastName);
-    I.fillField('#emailAddress', testEmailAddress);
-    I.click('#user-role-4');
-    I.click('Continue');
-    I.see('Check account details');
-    I.click('Confirm');
-    I.see('Account has been created');
-
-    I.click(locate('a').withAttr({ href: '/system-admin-dashboard' }));
+    I.click('Admin Dashboard')
+    I.createAdminAccount(testFirstName, testLastName, testEmailAddress, 'Internal - Administrator - Local');
+    I.click('Home');
     I.see('System Admin Dashboard');
     I.click('#card-user-management');
     I.see('User Management');
@@ -59,7 +51,53 @@ Scenario('I as a system admin should be able to update a users role and delete a
     I.fillField('#email', testEmailAddress);
     I.click('Apply filters');
     I.see('There is a problem');
-}).tag('@Testing');
+});
+
+Scenario('I as a system admin should be able to filter users correctly on the User Management page', async ({ I }) => {
+    I.loginAsSystemAdmin();
+    I.click('#card-user-management');
+    I.see('User Management');
+
+    I.fillField('#email', systemAdminUsername);
+    I.click('Apply filters');
+    I.see(systemAdminUsername);
+    I.seeNumberOfElements('.govuk-table__body .govuk-table__row', 1);
+    I.click('Manage');
+
+    const userId = (await I.grabTextFrom(locate('.govuk-summary-list__value').first())).trim();
+    const provenanceId = (await I.grabTextFrom(locate('.govuk-summary-list__value').at(5))).trim();
+
+    I.click('Back');
+    I.see('User Management');
+    I.click('Clear filters');
+    I.fillField('#userId', userId);
+    I.click('Apply filters');
+    I.see(systemAdminUsername);
+    I.seeNumberOfElements('.govuk-table__body .govuk-table__row', 1);
+    I.click('Clear filters');
+
+    I.fillField('#userProvenanceId', provenanceId);
+    I.click('Apply filters');
+    I.see(systemAdminUsername);
+    I.seeNumberOfElements('.govuk-table__body .govuk-table__row', 1);
+    I.click('Clear filters');
+
+    I.click('System Admin');
+    I.click('Apply filters');
+    I.see(systemAdminUsername);
+    I.click('Clear filters');
+
+    I.click('B2C');
+    I.click('Apply filters');
+    I.see(systemAdminUsername);
+    I.click('Clear filters');
+
+    I.click('CFT IdAM');
+    I.click('Apply filters');
+    I.dontSee(systemAdminUsername);
+    I.click('.moj-filter__tag');
+    I.see(systemAdminUsername);
+});
 
 Scenario('I as a system admin should not be able to change my own role', async ({ I }) => {
     I.loginAsSystemAdmin();
@@ -75,4 +113,4 @@ Scenario('I as a system admin should not be able to change my own role', async (
     I.click('Continue');
     I.see('There is a problem');
     I.see('You are unable to update the role for the same user you are logged in as');
-}).tag('@Testing');
+});
