8d1f9f0bc00ef859bc5883a4ff497a3725989aaa
Daniel Furnivall <danielfurnivall@gmail.com>
2023-04-17 11:18:43 +0100

    newline

--- src/main/controllers/AccountHomeController.ts
+++ src/main/controllers/AccountHomeController.ts
@@ -2,6 +2,7 @@ import { PipRequest } from '../models/request/PipRequest';
 import { Response } from 'express';
 import { cloneDeep } from 'lodash';
 
+
 export default class AccountHomeController {
     public get(req: PipRequest, res: Response): void {
         res.render('account-home', {
