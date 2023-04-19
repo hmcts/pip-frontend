d43714d243018527dfea12ff91c7f38be36d8e19
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2023-04-19 13:42:03 +0100

    PUB-1834 - Added pagination tests + config

--- src/test/end-to-end/tests/system-admin/system-admin-user-management-test.ts
+++ src/test/end-to-end/tests/system-admin/system-admin-user-management-test.ts
@@ -14,53 +14,53 @@ Scenario('I as a system admin should be able to update a users role and delete a
     I.click('Admin Dashboard');
     I.createAdminAccount(testFirstName, testLastName, testEmailAddress, 'Internal - Administrator - Local');
     I.click('Home');
-    I.see('System Admin Dashboard');
+    I.waitForText('System Admin Dashboard');
     I.click('#card-user-management');
-    I.see('User Management');
+    I.waitForText('User Management');
     I.fillField('#email', testEmailAddress);
     I.click('Apply filters');
-    I.see(testEmailAddress);
+    I.waitForText(testEmailAddress);
     I.click('#manage-link');
-    I.see('Manage ' + testEmailAddress);
+    I.waitForText('Manage ' + testEmailAddress);
     I.click('Change');
     I.selectOption('#updatedRole', 'CTSC Admin');
     I.click('Continue');
-    I.see('User Updated');
-    I.see('This user has been updated to a CTSC Admin');
+    I.waitForText('User Updated');
+    I.waitForText('This user has been updated to a CTSC Admin');
 
     I.click('Home');
     I.click('#card-user-management');
     I.fillField('#email', testEmailAddress);
     I.click('Apply filters');
-    I.see('CTSC Admin');
+    I.waitForText('CTSC Admin');
     I.click('#manage-link');
-    I.see('CTSC Admin');
+    I.waitForText('CTSC Admin');
 
     I.click('Delete user');
-    I.see('Are you sure you want to delete ' + testEmailAddress + '?');
+    I.waitForText('Are you sure you want to delete ' + testEmailAddress + '?');
     I.click('No');
     I.click('Continue');
-    I.see('Manage ' + testEmailAddress);
+    I.waitForText('Manage ' + testEmailAddress);
     I.click('Delete user');
     I.click('Yes');
     I.click('Continue');
-    I.see('User Deleted');
+    I.waitForText('User Deleted');
 
     I.click('Home');
     I.click('#card-user-management');
     I.fillField('#email', testEmailAddress);
     I.click('Apply filters');
-    I.see('There is a problem');
+    I.waitForText('There is a problem');
 });
 
 Scenario('I as a system admin should be able to filter users correctly on the User Management page', async ({ I }) => {
     I.loginAsSystemAdmin();
     I.click('#card-user-management');
-    I.see('User Management');
+    I.waitForText('User Management');
 
     I.fillField('#email', systemAdminUsername);
     I.click('Apply filters');
-    I.see(systemAdminUsername);
+    I.waitForText(systemAdminUsername);
     I.seeNumberOfElements('.govuk-table__body .govuk-table__row', 1);
     I.click('Manage');
 
@@ -68,29 +68,29 @@ Scenario('I as a system admin should be able to filter users correctly on the Us
     const provenanceId = (await I.grabTextFrom(locate('.govuk-summary-list__value').at(5))).trim();
 
     I.click('Back');
-    I.see('User Management');
+    I.waitForText('User Management');
     I.click('Clear filters');
     I.fillField('#userId', userId);
     I.click('Apply filters');
-    I.see(systemAdminUsername);
+    I.waitForText(systemAdminUsername);
     I.seeNumberOfElements('.govuk-table__body .govuk-table__row', 1);
     I.click('Clear filters');
 
     I.fillField('#userProvenanceId', provenanceId);
     I.click('Apply filters');
-    I.see(systemAdminUsername);
+    I.waitForText(systemAdminUsername);
     I.seeNumberOfElements('.govuk-table__body .govuk-table__row', 1);
     I.click('Clear filters');
 
     I.click('System Admin');
     I.click('Apply filters');
-    I.see(systemAdminUsername);
+    I.waitForText(systemAdminUsername);
     I.click('Clear filters');
 
     I.click('B2C');
     I.fillField('#userId', userId);
     I.click('Apply filters');
-    I.see(systemAdminUsername);
+    I.waitForText(systemAdminUsername);
     I.click('Clear filters');
 
     I.click('CFT IdAM');
@@ -99,35 +99,39 @@ Scenario('I as a system admin should be able to filter users correctly on the Us
     I.click(locate('.moj-filter__tag').at(1));
     I.fillField('#userId', userId);
     I.click('Apply filters');
-    I.see(systemAdminUsername);
+    I.waitForText(systemAdminUsername);
 });
 
 Scenario('I as a system admin should be able to use the pagination on the user management page', async ({ I }) => {
     I.loginAsSystemAdmin();
     I.click('#card-user-management');
-    I.see('User Management');
+    I.waitForText('User Management');
 
     I.see('Next');
     I.see('2 of');
     I.click('.govuk-pagination__link');
 
-    I.see('Previous');
+    I.waitForText('Previous');
     I.see('1 of');
+    I.see('Next');
+    I.see('3 of');
+
     I.click('.govuk-pagination__link');
+    I.waitForText('2 of')
 });
 
 Scenario('I as a system admin should not be able to change my own role', async ({ I }) => {
     I.loginAsSystemAdmin();
     I.click('#card-user-management');
-    I.see('User Management');
+    I.waitForText('User Management');
     I.fillField('#email', systemAdminUsername);
     I.click('Apply filters');
-    I.see(systemAdminUsername);
+    I.waitForText(systemAdminUsername);
     I.click('Manage');
-    I.see('Manage ' + systemAdminUsername);
+    I.waitForText('Manage ' + systemAdminUsername);
     I.click('Change');
     I.selectOption('#updatedRole', 'Local Admin');
     I.click('Continue');
-    I.see('There is a problem');
+    I.waitForText('There is a problem');
     I.see('You are unable to update the role for the same user you are logged in as');
 });
