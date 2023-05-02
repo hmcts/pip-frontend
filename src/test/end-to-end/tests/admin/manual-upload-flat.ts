95e3a27f039b8e2524a0e8040d0bd61916d020d0
ashwini.venkatesha <ashwini.venkatesha@justice.gov.uk>
2023-05-02 16:50:42 +0100

    Added functional test for flat file upload

---
+++ src/test/end-to-end/tests/admin/manual-upload-flat.ts
@@ -0,0 +1,59 @@
+import {generateTestLocation, padFormatted, removeTestLocationFile} from '../../shared/shared-functions';
+import {createLocation} from '../../shared/testingSupportApi';
+
+Feature('Manual upload flat file');
+
+const flatFileName = new DataTable(['flatFileName']);
+flatFileName.add(['testFlatFile.pdf']);
+flatFileName.add(['testFlatFile.docx']);
+
+Data(flatFileName).Scenario('I as a admin user should be able to upload flat file successfully',
+    async ({I, current}) => {
+        const listType = 'Civil And Family Daily Cause List';
+        const [locationId, locationName, locationFileName] = generateTestLocation();
+        const date = new Date();
+        const dayAfter = new Date();
+        dayAfter.setDate(dayAfter.getDate() + 1);
+
+        await createLocation(locationFileName);
+
+        I.loginAsAdmin();
+        I.click('#card-manual-upload');
+        I.waitForText('Manual upload');
+        I.see('Manually upload a csv, doc, docx, htm, html, json, or pdf file, max size 2MB');
+        I.attachFile('#manual-file-upload', './shared/mocks/' + current.flatFileName);
+        I.fillField('#search-input', locationName);
+        I.selectOption('#listType', listType);
+
+        I.fillField('#content-date-from-day', padFormatted(date.getDate()));
+        I.fillField('#content-date-from-month', padFormatted(date.getMonth() + 1));
+        I.fillField('#content-date-from-year', date.getFullYear());
+        I.selectOption('#classification', 'Public');
+        I.fillField('#display-date-from-day', padFormatted(date.getDate()));
+        I.fillField('#display-date-from-month', padFormatted(date.getMonth() + 1));
+        I.fillField('#display-date-from-year', date.getFullYear());
+        I.fillField('#display-date-to-day', padFormatted(dayAfter.getDate()));
+        I.fillField('#display-date-to-month', padFormatted(dayAfter.getMonth() + 1));
+        I.fillField('#display-date-to-year', dayAfter.getFullYear());
+        I.click('Continue');
+        I.waitForText('Check upload details');
+        I.see(locationName);
+        I.see(listType);
+        I.see(current.flatFileName);
+        I.click('Confirm');
+        I.waitForText('Success');
+        I.see('Your file has been uploaded');
+
+        I.click('Court and tribunal hearings');
+        I.click('Continue');
+        I.click('#view-choice');
+        I.click('Continue');
+        I.fillField('#search-input', locationName);
+        I.click('Continue');
+        I.waitForText('What do you want to view from ' + locationName);
+        I.see('Civil and Family Daily Cause List');
+
+        I.deletePublicationForCourt(locationId);
+        I.deleteLocation(locationId);
+        removeTestLocationFile(locationFileName);
+    }).tag('@CrossBrowser');
