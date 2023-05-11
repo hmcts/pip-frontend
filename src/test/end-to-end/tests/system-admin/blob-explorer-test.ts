6db90eda296ac0a96330c6c34fcb07d581b66850
ashwini.venkatesha <ashwini.venkatesha@justice.gov.uk>
2023-05-11 16:40:51 +0100

    small changes

--- src/test/end-to-end/tests/system-admin/blob-explorer-test.ts
+++ src/test/end-to-end/tests/system-admin/blob-explorer-test.ts
@@ -4,42 +4,39 @@ import {generateTestLocation, removeTestLocationFile} from '../../shared/shared-
 
 Feature('System admin blob explorer');
 
-Scenario(
-    'I as a system admin should be able to discover content uploaded to all locations.',
-    async ({I}) => {
-        const displayFrom = DateTime.now().toISO({includeOffset: false});
-        const displayTo = DateTime.now().plus({days: 1}).toISO({includeOffset: false});
+Scenario('I as a system admin should be able to discover content uploaded to all locations.', async ({I}) => {
+    const displayFrom = DateTime.now().toISO({includeOffset: false});
+    const displayTo = DateTime.now().plus({days: 1}).toISO({includeOffset: false});
 
-        const [locationId, locationName, locationFileName] = generateTestLocation();
+    const [locationId, locationName, locationFileName] = generateTestLocation();
 
-        await createLocation(locationFileName);
-        const artefactId = await uploadPublication('PUBLIC', locationId, displayFrom, displayTo, 'ENGLISH');
+    await createLocation(locationFileName);
+    const artefactId = await uploadPublication('PUBLIC', locationId, displayFrom, displayTo, 'ENGLISH');
 
-        I.loginAsSystemAdmin();
-        I.click('#card-blob-view-locations');
-        I.waitForText('Blob Explorer - Locations');
-        I.see('Choose a location to see all publications associated with it.');
-        I.see(locationName);
-        I.click(locationName);
-        I.waitForText('Blob Explorer - Publications');
-        I.see(locationName);
-        I.see('Choose a publication from the list.');
-        I.click(artefactId);
-        I.waitForText('Blob Explorer - JSON file');
-        I.see('Metadata');
-        I.see(locationId);
-        I.see(locationName);
-        I.see(artefactId);
-        I.see('Public');
-        I.see('English');
-        I.see('Link to rendered template');
-        I.see('View Raw JSON Content');
+    I.loginAsSystemAdmin();
+    I.click('#card-blob-view-locations');
+    I.waitForText('Blob Explorer - Locations');
+    I.see('Choose a location to see all publications associated with it.');
+    I.see(locationName);
+    I.click(locationName);
+    I.waitForText('Blob Explorer - Publications');
+    I.see(locationName);
+    I.see('Choose a publication from the list.');
+    I.click(artefactId);
+    I.waitForText('Blob Explorer - JSON file');
+    I.see('Metadata');
+    I.see(locationId);
+    I.see(locationName);
+    I.see(artefactId);
+    I.see('Public');
+    I.see('English');
+    I.see('Link to rendered template');
+    I.see('View Raw JSON Content');
 
-        I.click('Link to rendered template');
-        I.waitForText('Civil and Family Daily Cause List for '+locationName);
+    I.click('Link to rendered template');
+    I.waitForText('Civil and Family Daily Cause List for ' + locationName);
 
-        I.deletePublicationForCourt(locationId);
-        I.deleteLocation(locationId);
-        removeTestLocationFile(locationFileName);
-    }
-);
+    I.deletePublicationForCourt(locationId);
+    I.deleteLocation(locationId);
+    removeTestLocationFile(locationFileName);
+});
