789de9eeb544236e9f567f5f4a77d3f2278f96d3
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2023-05-24 13:27:14 +0100

    Testing pipeline

--- wdio.conf.js
+++ wdio.conf.js
@@ -123,7 +123,7 @@ exports.config = {
     baseUrl: process.env.TEST_URL || 'https://localhost:8080',
     //
     // Default timeout for all waitFor* commands.
-    waitforTimeout: 10000,
+    waitforTimeout: 15000,
     //
     // Default timeout in milliseconds for request
     // if browser driver or grid doesn't send response
