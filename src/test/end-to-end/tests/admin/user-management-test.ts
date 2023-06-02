d9d63955774d67a09a1afadfaeff4b7d313adc5b
Natasha Alker <NatashaAlker@yahoo.co.uk>
2023-06-02 13:19:47 +0100

    add functional tests for admin user management

--- src/test/end-to-end/tests/admin/user-management-test.ts
+++ src/test/end-to-end/tests/admin/user-management-test.ts
@@ -1,36 +1,84 @@
-import {config as testConfig} from "../../../config";
+import { config as testConfig } from '../../../config';
+import {randomData} from "../../shared/random-data";
 
 Feature('Manage admin user accounts');
 
+const TEST_WORKER_NUMBER = randomData.getRandomNumber(10000000, 99999999);
 const TEST_FIRST_NAME = testConfig.TEST_SUITE_PREFIX + 'FirstName';
 const TEST_LAST_NAME = testConfig.TEST_SUITE_PREFIX + 'Surname';
-const EMAIL = testConfig.TEST_SUITE_PREFIX + 'email@test.com';
-const ROLE = 'Local Admin';
+const TEST_EMAIL = randomData.getRandomEmailAddress(TEST_WORKER_NUMBER);
+const TEST_ROLE = 'Internal - Administrator - Local';
+const TEST_INVALID_EMAIL = 'invalid_email@test.com';
+
 Scenario('I as an admin user should be able to modify a user account', async ({ I }) => {
-    // I.amOnPage('/');
-    // I.click('Continue');
     I.loginAsAdmin();
-    // I.amOnPage('/admin-dashboard');
+    I.waitForText('Your Dashboard');
+    I.createAdminAccount(TEST_FIRST_NAME, TEST_LAST_NAME, TEST_EMAIL, TEST_ROLE);
 
-    I.createAdminAccount(TEST_FIRST_NAME, TEST_LAST_NAME, EMAIL, ROLE);
-    // I.waitForText('Your Dashboard');
-    // I.see('Update and delete users.');
-    // I.click('#card-admin-management');
-    // I.waitForText('What is the users email address?');
-    // I.fillField('#search-input', EMAIL);
-    // I.click('Continue');
+    I.click('Home');
+    I.see('Update and delete users.');
+    I.click('#card-admin-management');
+    I.waitForText('What is the users email address?');
+    I.fillField('#search-input', TEST_EMAIL);
+    I.click('Continue');
+    I.waitForText('Manage ' + TEST_EMAIL);
+    I.click('Change');
+    I.waitForText('What role would you like ' + TEST_EMAIL + ' to have?');
+    I.selectOption('updatedRole', 'CTSC Super Admin');
+    I.click('Continue');
+    I.waitForText('User Updated');
 
+    I.click('Home');
+    I.see('Update and delete users.');
+    I.click('#card-admin-management');
+    I.waitForText('What is the users email address?');
+    I.fillField('#search-input', TEST_EMAIL);
+    I.click('Continue');
+    I.waitForText('Manage ' + TEST_EMAIL);
+    I.click('Change');
+    I.waitForText('What role would you like ' + TEST_EMAIL + ' to have?');
+    I.selectOption('updatedRole', 'Local Super Admin');
+    I.click('Continue');
+    I.waitForText('User Updated');
 
+    I.click('Home');
+    I.see('Update and delete users.');
+    I.click('#card-admin-management');
+    I.waitForText('What is the users email address?');
+    I.fillField('#search-input', TEST_EMAIL);
+    I.click('Continue');
+    I.waitForText('Manage ' + TEST_EMAIL);
+    I.click('Change');
+    I.waitForText('What role would you like ' + TEST_EMAIL + ' to have?');
+    I.selectOption('updatedRole', 'CTSC Admin');
+    I.click('Continue');
+    I.waitForText('User Updated');
 
+    I.click('Home');
+    I.deleteAccount(TEST_EMAIL);
+});
 
+Scenario('I as an admin user should be able to see all errors related to modify a user account', async ({ I }) => {
+    I.loginAsAdmin();
+    I.waitForText('Your Dashboard');
+    I.createAdminAccount(TEST_FIRST_NAME, TEST_LAST_NAME, TEST_EMAIL, TEST_ROLE);
 
+    I.click('Home');
+    I.see('Update and delete users.');
+    I.click('#card-admin-management');
+    I.waitForText('What is the users email address?');
+    I.fillField('#search-input', '');
+    I.click('Continue');
+    I.waitForText('There is a problem');
 
-    // I.see('Create accounts for: CTSC Super Admin, Local Super Admin, CTSC Admin, Local Admin.');
-    // I.click('#card-create-admin-account');
-    // I.waitForText('Create admin account');
-    // I.fillField('#firstName', TEST_FIRST_NAME);
-    // I.fillField('#lastName', TEST_LAST_NAME);
-    // I.fillField('#emailAddress', EMAIL);
-    // I.click('#user-role');
-    // I.click('Continue');
+    I.click('Home');
+    I.see('Update and delete users.');
+    I.click('#card-admin-management');
+    I.waitForText('What is the users email address?');
+    I.fillField('#search-input', TEST_INVALID_EMAIL);
+    I.click('Continue');
+    I.waitForText('There is a problem');
+
+    I.click('Home');
+    I.deleteAccount(TEST_EMAIL);
 });
