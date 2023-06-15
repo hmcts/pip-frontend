79b46f35f33eb1021552bc03b8deae6f37c30c75
Kian Kwa <kian.kwa@justice.gov.uk>
2023-06-15 16:58:08 +0100

    Remove setting of result count/indexes for IOS devices

--- webpack/accessible-autocomplete.js
+++ webpack/accessible-autocomplete.js
@@ -1,17 +1,9 @@
-const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-
-const packageJson = require.resolve('accessible-autocomplete/package.json');
-const root = path.resolve(packageJson, '..', 'govuk');
-const sass = path.resolve(root, 'all.scss');
-const javascript = path.resolve(root, 'all.js');
-const assets = path.resolve(root, 'assets');
 
 const copyGovukTemplateAssets = new CopyWebpackPlugin({
     patterns: [{ from: 'src/main/assets/js/', to: 'assets/js' }],
 });
 
 module.exports = {
-    paths: { template: root, sass, javascript, assets },
     plugins: [copyGovukTemplateAssets],
 };
