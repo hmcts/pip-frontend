e6ca62b4ea52123cee26c158f277c0e9c6f90876
ashwini.venkatesha <ashwini.venkatesha@justice.gov.uk>
2023-06-20 17:34:27 +0100

    Added new functional test for banner navigation links

---
+++ src/test/end-to-end/tests/banner-navigation-test.ts
@@ -0,0 +1,73 @@
+Feature('Banner navigation and links');
+
+Scenario('Unverified user - all banner navigation links should take user to the correct pages', async ({I}) => {
+    I.amOnPage('/view-option');
+    I.waitForText('Court and tribunal hearings');
+    I.click(locate('//li').withText('Home'));
+    I.waitForText('Court and tribunal hearings');
+    I.click('Continue');
+    I.click(locate('//li').withText('Find a court or tribunal'));
+    I.waitForText('What court or tribunal are you interested in?');
+    I.click(locate('//a').withText('Cymraeg'));
+    I.waitForText('Ym mha lys neu dribiwnlys y mae gennych ddiddordeb?');
+    I.click(locate('//a').withText('English'));
+    I.click(locate('//li').withText('Single Justice Procedure cases'));
+    I.waitForText('What do you want to view from Single Justice Procedure?');
+    I.click(locate('//a').withText('Court and tribunal hearings'));
+});
+
+Scenario('Verified user - all banner navigation links should take user to the correct pages', async ({I}) => {
+    I.loginAsMediaUser();
+    I.waitForText('Your account');
+    I.click(locate('//li').withText('Find a court or tribunal'));
+    I.waitForText('What court or tribunal are you interested in?');
+    I.click(locate('//li').withText('Single Justice Procedure cases'));
+    I.waitForText('What do you want to view from Single Justice Procedure?');
+    I.click(locate('//li').withText('Email subscriptions'));
+    I.waitForText('Your email subscriptions');
+    I.click(locate('//li').withText('Home'));
+    I.waitForText('Your account');
+    I.logout();
+});
+
+Scenario('Cft user - all banner navigation links should take user to the correct pages', async ({I}) => {
+    I.loginAsCftUser();
+    I.waitForText('Your account');
+    I.click(locate('//li').withText('Find a court or tribunal'));
+    I.waitForText('What court or tribunal are you interested in?');
+    I.click(locate('//li').withText('Single Justice Procedure cases'));
+    I.waitForText('What do you want to view from Single Justice Procedure?');
+    I.click(locate('//li').withText('Email subscriptions'));
+    I.waitForText('Your email subscriptions');
+    I.click(locate('//li').withText('Home'));
+    I.waitForText('Your account');
+    I.logout();
+});
+
+Scenario('Admin user - all banner navigation links should take user to the correct pages', async ({I}) => {
+    I.loginAsAdmin();
+    I.waitForText('Your Dashboard');
+    I.click(locate('//li').withText('Upload'));
+    I.waitForText('Manual upload');
+    I.click(locate('//li').withText('Review apps'));
+    I.waitForText('Select application to assess');
+    I.click(locate('//li').withText('Remove'));
+    I.waitForText('Find content to remove');
+    I.click(locate('//li').withText('Home'));
+    I.waitForText('Your Dashboard');
+    I.logout();
+});
+
+Scenario('System admin user - all banner navigation links should take user to the correct pages', async ({I}) => {
+    I.loginAsSystemAdmin();
+    I.waitForText('System Admin Dashboard');
+    I.click(locate('//li').withText('Admin Dashboard'));
+    I.waitForText('Your Dashboard');
+    I.click(locate('//li').withText('Upload'));
+    I.waitForText('Manual upload');
+    I.click(locate('//li').withText('Remove'));
+    I.waitForText('Find content to remove');
+    I.click(locate('//li').withText('Home'));
+    I.waitForText('System Admin Dashboard');
+    I.logout();
+});
