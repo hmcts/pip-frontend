082ba71f37c235212d82aa2fd5f211fda658d469
Natasha Alker <NatashaAlker@yahoo.co.uk>
2023-06-20 16:38:10 +0100

    add logic to enable filtering by London postal area

--- src/main/service/sjpFilterService.ts
+++ src/main/service/sjpFilterService.ts
@@ -4,6 +4,8 @@ const filterService = new FilterService();
 
 const replaceRegex = /[\s,]/g;
 
+const londonPostalAreaCodes =[ 'N', 'NW', 'E', 'EC', 'SE', 'SW', 'W', 'WC'];
+
 export class SjpFilterService {
     /**
      * This method generates the filter options, and filters the cases based on the selected options.
@@ -43,9 +45,11 @@ export class SjpFilterService {
         });
 
         const formattedPostcodes = new Set<string>();
+        const postalAreaCodes = new Set<string>();
 
         postcodes.forEach(postcode => {
             formattedPostcodes.add(postcode.split(' ', 2)[0]);
+            postalAreaCodes.add(postcode.split(/\d/)[0]);
         });
 
         const sortedPostcodes = Array.from(formattedPostcodes).sort((a, b) =>
@@ -65,6 +69,16 @@ export class SjpFilterService {
                 checked: filterValues.includes(formattedPostcode),
             });
         });
+
+        const hasLondonPostalAreaCode = this.checkForLondonPostalAreaCodes(londonPostalAreaCodes, postalAreaCodes);
+
+        if (hasLondonPostalAreaCode) {
+            filterStructure.postcodes.push({
+                value: 'The City of London',
+                text: 'The City of London',
+                checked: filterValues.includes('The City of London'),
+            })
+        }
 
         sortedProsecutors.forEach(prosecutor => {
             const formattedProsecutor = prosecutor.replace(replaceRegex, '');
@@ -88,6 +102,7 @@ export class SjpFilterService {
     private filterCases(allCases, filterOptions) {
         return this.doFiltering(
             allCases,
+            londonPostalAreaCodes,
             this.getActiveFilters(filterOptions.postcodes),
             this.getActiveFilters(filterOptions.prosecutors)
         );
@@ -104,17 +119,20 @@ export class SjpFilterService {
         return activeFilters;
     }
 
-    private doFiltering(allCases, postcodeFilters, prosecutorFilters) {
+    private doFiltering(allCases, londonPostalAreaCodes, postcodeFilters, prosecutorFilters) {
         const filteredCases = [];
         allCases.forEach(item => {
             const formattedPostcode = item.postcode.split(' ', 2)[0];
+            const postalAreaCode = item.postcode.split(/\d/)[0];
             const formattedProsecutor = item.organisationName.replace(replaceRegex, '');
 
             if (postcodeFilters.length > 0 && prosecutorFilters.length > 0) {
-                if (postcodeFilters.includes(formattedPostcode) && prosecutorFilters.includes(formattedProsecutor)) {
+                if (postcodeFilters.includes('The City of London') && londonPostalAreaCodes.includes(postalAreaCode) && prosecutorFilters.includes(formattedProsecutor)) {
+                    filteredCases.push(item);
+                } else if (postcodeFilters.includes(formattedPostcode) && prosecutorFilters.includes(formattedProsecutor)) {
                     filteredCases.push(item);
                 }
-            } else if (postcodeFilters.length > 0 && postcodeFilters.includes(formattedPostcode)) {
+            } else if (postcodeFilters.length > 0 && postcodeFilters.includes('The City of London') && londonPostalAreaCodes.includes(postalAreaCode)) {
                 filteredCases.push(item);
             } else if (prosecutorFilters.length > 0 && prosecutorFilters.includes(formattedProsecutor)) {
                 filteredCases.push(item);
@@ -122,5 +140,10 @@ export class SjpFilterService {
         });
 
         return filteredCases;
+    }
+
+    private checkForLondonPostalAreaCodes(londonPostalAreaCodes, postalAreaCodes){
+        const postalAreaInLondon = new Set([...londonPostalAreaCodes].filter(element => postalAreaCodes.has(element)));
+        return postalAreaInLondon.size > 0;
     }
 }
