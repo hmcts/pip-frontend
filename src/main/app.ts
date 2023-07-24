cf55f95118556da3c87667351881b643158173ac
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2023-07-24 12:19:57 +0100

    PUB-1959 - Updated redis store

--- src/main/app.ts
+++ src/main/app.ts
@@ -53,8 +53,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const redisStore = new RedisStore({
-    client: redisClient,
-    prefix: 'pip-frontend-session:',
+    client: redisClient
 });
 
 app.set('trust proxy', 1);
