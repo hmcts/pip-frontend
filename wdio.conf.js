ef92d143b701eada9cc3072129d212e9accc91bd
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2023-07-24 10:57:10 +0100

    Fixed formatting

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,6 +1,9 @@
 const drivers = {
     chromiumedge: { version: 'latest' },
-    chrome: { fullUrl: 'https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/115.0.5790.102/linux64/chrome-linux64.zip' },
+    chrome: {
+        fullUrl:
+            'https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/115.0.5790.102/linux64/chrome-linux64.zip',
+    },
     firefox: { version: 'latest' },
 };
 
