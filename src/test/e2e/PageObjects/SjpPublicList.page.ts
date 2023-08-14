957d29f20864a4c1c3be2bbf88a9a63c09f8e786
ashwini.venkatesha <ashwini.venkatesha@justice.gov.uk>
2023-08-14 16:01:58 +0100

    Added new test to check SJP List Download File

--- src/test/e2e/PageObjects/SjpPublicList.page.ts
+++ src/test/e2e/PageObjects/SjpPublicList.page.ts
@@ -1,14 +1,5 @@
 import { CommonPage } from './Common.page';
-import { ListDownloadDisclaimerPage } from './ListDownloadDisclaimer.page';
-
-const helpers = require('../Helpers/Selectors');
 
 export class SjpPublicListPage extends CommonPage {
-    async clickDownloadACopyButton(): Promise<ListDownloadDisclaimerPage> {
-        $(helpers.DownloadACopyButton).catch(() => {
-            console.log(`${helpers.DownloadACopyButton} not found`);
-        });
-        await $(helpers.DownloadACopyButton).click();
-        return new ListDownloadDisclaimerPage();
-    }
+
 }
