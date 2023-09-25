20681759a4bce702912a7353d5674ce015126be9
KianKwa <80688761+KianKwa@users.noreply.github.com>
2023-09-25 12:24:54 +0100

    PUB-2068 Enforce authorised access to PDF/Excel download files page (#1113)
    * Enforce authorised access to PDF/Excel download files page
    
    * lint fixes
    
    * Update audit suppression file
    
    * Update unit test
    
    * Add logic for artifact ID not found
    
    * lint fixes
    
    * Render the list-not-found page when artefact does not exist
    
    * lint fixes
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/controllers/ListDownloadFilesController.ts
+++ src/main/controllers/ListDownloadFilesController.ts
@@ -6,39 +6,65 @@ import path from 'path';
 import mime from 'mime-types';
 import { cloneDeep } from 'lodash';
 import { FileType } from '../models/consts';
+import { PublicationService } from '../service/publicationService';
+import { HttpStatusCode } from 'axios';
 
 const url = 'list-download-files';
 const listDownloadService = new ListDownloadService();
+const publicationService = new PublicationService();
 
 export default class ListDownloadFilesController {
     public async get(req: PipRequest, res: Response): Promise<void> {
         const type = req.query.type as string;
         const artefactId = req.query.artefactId;
+        let publicationMetadata;
 
-        if (type === undefined) {
-            const pdfFileSize = listDownloadService.getFileSize(artefactId, FileType.PDF);
-            const excelFileSize = listDownloadService.getFileSize(artefactId, FileType.EXCEL);
+        if (artefactId) {
+            publicationMetadata = await publicationService.getIndividualPublicationMetadata(
+                artefactId,
+                req.user['userId'],
+                true
+            );
+            if (publicationMetadata && publicationMetadata !== HttpStatusCode.NotFound) {
+                const isAuthorised = await listDownloadService.checkUserIsAuthorised(
+                    req.user['userId'],
+                    publicationMetadata.listType,
+                    publicationMetadata.sensitivity
+                );
+                if (isAuthorised) {
+                    if (type === undefined) {
+                        const pdfFileSize = listDownloadService.getFileSize(artefactId, FileType.PDF);
+                        const excelFileSize = listDownloadService.getFileSize(artefactId, FileType.EXCEL);
 
-            res.render(url, {
-                ...cloneDeep(req.i18n.getDataByLanguage(req.lng)[url]),
-                artefactId: artefactId,
-                pdfFileSize: pdfFileSize,
-                excelFileSize: excelFileSize,
-            });
-        } else {
-            const file = listDownloadService.getFile(artefactId, FileType[type.toUpperCase()]);
-            if (file) {
-                const filename = path.basename(file);
-                const mimetype = mime.lookup(file);
+                        res.render(url, {
+                            ...cloneDeep(req.i18n.getDataByLanguage(req.lng)[url]),
+                            artefactId: artefactId,
+                            pdfFileSize: pdfFileSize,
+                            excelFileSize: excelFileSize,
+                        });
+                    } else {
+                        const file = listDownloadService.getFile(artefactId, FileType[type.toUpperCase()]);
+                        if (file) {
+                            const filename = path.basename(file);
+                            const mimetype = mime.lookup(file);
 
-                res.setHeader('Content-disposition', 'attachment; filename=' + filename);
-                res.setHeader('Content-type', mimetype);
+                            res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+                            res.setHeader('Content-type', mimetype);
 
-                const filestream = fs.createReadStream(file);
-                filestream.pipe(res);
+                            const filestream = fs.createReadStream(file);
+                            filestream.pipe(res);
+                        } else {
+                            res.render('error', req.i18n.getDataByLanguage(req.lng).error);
+                        }
+                    }
+                } else {
+                    res.render('unauthorised-access', req.i18n.getDataByLanguage(req.lng)['unauthorised-access']);
+                }
             } else {
-                res.render('error', req.i18n.getDataByLanguage(req.lng).error);
+                res.render('list-not-found', req.i18n.getDataByLanguage(req.lng)['list-not-found']);
             }
+        } else {
+            res.render('error', req.i18n.getDataByLanguage(req.lng).error);
         }
     }
 }
