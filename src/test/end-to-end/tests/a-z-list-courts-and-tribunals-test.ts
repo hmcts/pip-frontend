841d2de81be4c10ae21db34837c22e45fefc308f
ashwini.venkatesha <ashwini.venkatesha@justice.gov.uk>
2023-09-27 11:31:18 +0100

    Removed old functional test test for the feature A-Z list of courts and tribunals

--- src/test/end-to-end/tests/a-z-list-courts-and-tribunals-test.ts
+++ src/test/end-to-end/tests/a-z-list-courts-and-tribunals-test.ts
@@ -1,23 +1,17 @@
-import {DateTime} from 'luxon';
-import {randomData} from "../shared/random-data";
-import {config} from "../../config";
-import {createLocation, uploadPublication} from "../shared/testingSupportApi";
+import { DateTime } from 'luxon';
+import { randomData } from '../shared/random-data';
+import { config } from '../../config';
+import { createLocation, uploadPublication } from '../shared/testingSupportApi';
 
 Feature('A-Z list of courts and tribunals');
 
-Scenario('I as a user should be able to search and filter from an A-Z list of courts and tribunals', async ({I}) => {
-    const displayFrom = DateTime.now().toISO({includeOffset: false});
-    const displayTo = DateTime.now().plus({days: 1}).toISO({includeOffset: false});
+Scenario('I as a user should be able to search and filter from an A-Z list of courts and tribunals', async ({ I }) => {
+    const displayFrom = DateTime.now().toISO({ includeOffset: false });
+    const displayTo = DateTime.now().plus({ days: 1 }).toISO({ includeOffset: false });
     const locationId = randomData.getRandomLocationId();
     const locationName = config.TEST_SUITE_PREFIX + randomData.getRandomString();
     await createLocation(locationId, locationName);
-    await uploadPublication(
-        'PUBLIC',
-        locationId,
-        displayFrom,
-        displayTo,
-        'ENGLISH'
-    );
+    await uploadPublication('PUBLIC', locationId, displayFrom, displayTo, 'ENGLISH');
 
     I.amOnPage('/search');
     I.waitForText('Want to see all courts and tribunals?');
@@ -27,26 +21,26 @@ Scenario('I as a user should be able to search and filter from an A-Z list of co
     I.see('Type of court or tribunal');
     I.see('Region');
 
-    I.click(locate('//input').withAttr({value: 'Civil'}));
-    I.click(locate('//input').withAttr({value: 'South East'}));
+    I.click(locate('//input').withAttr({ value: 'Civil' }));
+    I.click(locate('//input').withAttr({ value: 'South East' }));
     I.click('Apply filters');
     I.see(locationName);
 
-    I.click(locate('//input').withAttr({value: 'South East'}));
-    I.click(locate('//input').withAttr({value: 'North West'}));
+    I.click(locate('//input').withAttr({ value: 'South East' }));
+    I.click(locate('//input').withAttr({ value: 'North West' }));
     I.click('Apply filters');
     I.dontSee(locationName);
 
-    I.click(locate('//input').withAttr({value: 'North West'}));
+    I.click(locate('//input').withAttr({ value: 'North West' }));
     I.click('Apply filters');
     I.see(locationName);
 
-    I.click(locate('//input').withAttr({value: 'Civil'}));
-    I.click(locate('//input').withAttr({value: 'Crown'}));
+    I.click(locate('//input').withAttr({ value: 'Civil' }));
+    I.click(locate('//input').withAttr({ value: 'Crown' }));
     I.click('Apply filters');
     I.dontSee(locationName);
 
-    I.click(locate('//input').withAttr({value: 'Crown'}));
+    I.click(locate('//input').withAttr({ value: 'Crown' }));
     I.click('Apply filters');
 
     I.click('#T-selector');
