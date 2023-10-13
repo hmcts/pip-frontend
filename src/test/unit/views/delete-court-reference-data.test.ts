1c88add6af21a119b6d65321e597b41db6b0d38f
Josh <joshblackmoor@users.noreply.github.com>
2023-10-13 10:44:42 +0100

    PUB-2195 Added dynatrace script (#1135)
    * Added dynatrace script
    
    * Unit tests updated
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/test/unit/views/delete-court-reference-data.test.ts
+++ src/test/unit/views/delete-court-reference-data.test.ts
@@ -38,12 +38,12 @@ describe('Delete Court List Search Page', () => {
     });
 
     it('should use accessible autocomplete in the script block', () => {
-        const script = htmlRes.getElementsByTagName('script')[5];
+        const script = htmlRes.getElementsByTagName('script')[6];
         expect(script.innerHTML).contains('accessibleAutocomplete');
     });
 
     it('should autocomplete source with court names', () => {
-        const script = htmlRes.getElementsByTagName('script')[5];
+        const script = htmlRes.getElementsByTagName('script')[6];
         expect(script.innerHTML).contains("Abergavenny Magistrates' Court", 'Could not find input field');
     });
 
