cc4deb54f4f7eb02963735f63722f753616e1f81
ashwini.venkatesha <ashwini.venkatesha@justice.gov.uk>
2023-10-19 14:52:17 +0100

    Changed list type in single-justice-procedure-test.ts

--- src/test/end-to-end/tests/single-justice-procedure-test.ts
+++ src/test/end-to-end/tests/single-justice-procedure-test.ts
@@ -3,10 +3,11 @@ import { uploadPublication } from '../shared/testingSupportApi';
 
 Feature('Single Justice Procedure cases');
 
-Scenario.skip('I should be able to view all the single procedure cases', async ({ I }) => {
+Scenario('I should be able to view all the single procedure cases', async ({ I }) => {
     const displayFrom = DateTime.now().toISO({ includeOffset: false });
     const displayTo = DateTime.now().plus({ days: 1 }).toISO({ includeOffset: false });
-    const artefactId = await uploadPublication('PUBLIC', '9', displayFrom, displayTo, 'ENGLISH');
+    const artefactId = await uploadPublication('PUBLIC', '9', displayFrom, displayTo, 'ENGLISH','primaryHealthList.json',
+        'PRIMARY_HEALTH_LIST');
 
     I.amOnPage('/');
     I.see('Court and tribunal hearings');
@@ -14,11 +15,11 @@ Scenario.skip('I should be able to view all the single procedure cases', async (
     I.waitForText('What do you want to do?');
     I.click(locate('//a').withText('Single Justice Procedure cases'));
     I.waitForText('What do you want to view from Single Justice Procedure?');
-    I.see('Civil and Family Daily Cause List');
-    I.click(locate('//a').withText('Civil and Family Daily Cause List'));
-    I.waitForText('Civil and Family Daily Cause List for Single Justice Procedure');
-    I.see('12345678');
-    I.see('A1 Vs B1');
+    I.see('Primary Health Tribunal Hearing List');
+    I.click(locate('//a').withText('Primary Health Tribunal Hearing List'));
+    I.waitForText('Tribunal Hearing List for Primary Health');
+    I.see('PRESTON');
+    I.see('A Vs B');
 
     I.deletePublicationByArtefactId(artefactId);
 });
