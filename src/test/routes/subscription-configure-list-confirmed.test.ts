bc4a20d43a2c8eae8299f4ffcbd9453c21824615
ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
2023-11-27 17:43:47 +0000

    PUB-2001 - Added auth to subscription-configure-list-confirmed screen

--- src/test/routes/subscription-configure-list-confirmed.test.ts
+++ src/test/routes/subscription-configure-list-confirmed.test.ts
@@ -2,15 +2,15 @@ import { expect } from 'chai';
 import request from 'supertest';
 import sinon from 'sinon';
 import { app } from '../../main/app';
-import { request as expressRequest } from 'express';
 import { SubscriptionService } from '../../main/service/subscriptionService';
 
 const PAGE_URL = '/subscription-configure-list-confirmed';
-expressRequest['user'] = { roles: 'VERIFIED' };
+
 
 describe('Subscription Configure list confirmation result', () => {
     describe('on GET', () => {
         test('should render subscription list type confirmation page', async () => {
+            app.request['user'] = { roles: 'VERIFIED' };
             await request(app)
                 .get(PAGE_URL)
                 .expect(res => {
@@ -21,6 +21,7 @@ describe('Subscription Configure list confirmation result', () => {
 
     describe('on POST', () => {
         test('should show subscription list type confirmation page', async () => {
+            app.request['user'] = { roles: 'VERIFIED' };
             sinon
                 .stub(SubscriptionService.prototype, 'configureListTypeForLocationSubscriptions')
                 .withArgs('1', 'CIVIL_DAILY_CAUSE_LIST')
@@ -31,6 +32,33 @@ describe('Subscription Configure list confirmation result', () => {
                 .send({ 'list-selections[]': 'CIVIL_DAILY_CAUSE_LIST' })
                 .expect(res => {
                     expect(res.status).to.equal(200);
+                });
+        });
+    });
+});
+
+describe('Subscription Configure list confirmation should redirect when no user', () => {
+    describe('on GET', () => {
+        test('should render the sign in page due to being unauthorised to get to confirmation page', async () => {
+            app.request['user'] = {};
+            await request(app)
+                .get(PAGE_URL)
+                .expect(res => {
+                    expect(res.status).to.equal(302);
+                    expect(res.headers['location']).to.equal('/sign-in');
+                });
+        });
+    });
+
+    describe('on POST', () => {
+        test('should render the sign in page due to being unauthorised to post to confirmation page', async () => {
+            app.request['user'] = {};
+            await request(app)
+                .post(PAGE_URL)
+                .send({ 'list-selections[]': 'CIVIL_DAILY_CAUSE_LIST' })
+                .expect(res => {
+                    expect(res.status).to.equal(302);
+                    expect(res.headers['location']).to.equal('/sign-in');
                 });
         });
     });
