0a9372d2400879bebc5193baf095220745622e98
Natasha Alker <NatashaAlker@yahoo.co.uk>
2024-01-04 15:10:20 +0000

    Refactor render logic to use guard clauses

--- src/main/controllers/SubscriptionManagementController.ts
+++ src/main/controllers/SubscriptionManagementController.ts
@@ -7,22 +7,24 @@ const subscriptionService = new SubscriptionService();
 
 export default class SubscriptionManagementController {
     public async get(req: PipRequest, res: Response): Promise<void> {
-        if (req.user) {
-            const subscriptionData = await subscriptionService.getSubscriptionDataForView(
+        if (!req.user) {
+            return res.render('error', req.i18n.getDataByLanguage(req.lng).error);
+        }
+
+        const subscriptionData =
+            await subscriptionService.getSubscriptionDataForView(
                 req.user['userId'],
                 req.lng,
                 Object.keys(req.query)[0]
             );
-            if (subscriptionService.checkForNullValues(subscriptionData)) {
-                res.render('error', req.i18n.getDataByLanguage(req.lng).error);
-            } else {
-                res.render('subscription-management', {
-                    ...cloneDeep(req.i18n.getDataByLanguage(req.lng)['subscription-management']),
-                    ...subscriptionData,
-                });
-            }
-        } else {
-            res.render('error', req.i18n.getDataByLanguage(req.lng).error);
+
+        if (subscriptionService.checkForNullValues(subscriptionData)) {
+            return res.render('error', req.i18n.getDataByLanguage(req.lng).error);
         }
+
+        return res.render('subscription-management', {
+            ...cloneDeep(req.i18n.getDataByLanguage(req.lng)['subscription-management']),
+            ...subscriptionData,
+        });
     }
 }
