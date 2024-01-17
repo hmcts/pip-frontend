7c86a30848e4969012ae37d6b2b1b496f33334f9
Natasha Alker <NatashaAlker@yahoo.co.uk>
2024-01-17 13:54:23 +0000

    Update findAndManipulateJudiciary method to return comma separated lists of judiciary, with presiding judiciary appearing first if present

--- src/main/service/listParseHelperService.ts
+++ src/main/service/listParseHelperService.ts
@@ -189,23 +189,28 @@ export class ListParseHelperService {
      */
     public findAndManipulateJudiciary(session: object): string {
         let judiciaries = '';
+        let presidingJudiciary = '';
         let foundPresiding = false;
+        let otherJudges = false;
         session['judiciary']?.forEach(judiciary => {
             if (judiciary?.isPresiding === true) {
-                judiciaries = ListParseHelperService.writeStringIfValid(judiciary?.johKnownAs);
+                presidingJudiciary = ListParseHelperService.writeStringIfValid(judiciary?.johKnownAs) + ', ';
                 foundPresiding = true;
-            } else if (!foundPresiding) {
+            } else {
                 if (ListParseHelperService.writeStringIfValid(judiciary?.johKnownAs) !== '') {
                     judiciaries += ListParseHelperService.writeStringIfValid(judiciary?.johKnownAs) + ', ';
+                    otherJudges = true;
                 }
             }
         });
 
-        if (!foundPresiding) {
-            judiciaries = judiciaries.slice(0, -2);
+        if (foundPresiding && otherJudges) {
+            return [judiciaries.slice(0, 0), presidingJudiciary, judiciaries.slice(0, -2)].join('');
+        } else if (foundPresiding) {
+            return presidingJudiciary.slice(0, -2);
+        } else {
+            return judiciaries.slice(0, -2);
         }
-
-        return judiciaries;
     }
 
     /**
@@ -324,39 +329,7 @@ export class ListParseHelperService {
             if (ListParseHelperService.writeStringIfValid(joh?.johKnownAs) !== '') {
                 formattedJoh += ListParseHelperService.writeStringIfValid(joh?.johKnownAs);
             }
-
-            if (ListParseHelperService.writeStringIfValid(joh?.johNameSurname) !== '') {
-                if (ListParseHelperService.writeStringIfValid(joh?.johKnownAs) !== '') {
-                    formattedJoh += ' ';
-                }
-                formattedJoh += ListParseHelperService.writeStringIfValid(joh?.johNameSurname);
-            }
         });
         return formattedJoh;
-    }
-
-    /**
-     * Take in the session and return the formatted judiciary with their title and nameSurname
-     * @param session The session to get the judiciary from
-     */
-    public getJudiciaryNameSurname(session: object): string {
-        let judiciaryFormatted = '';
-        session['judiciary']?.forEach(judiciary => {
-            if (judiciaryFormatted.length > 0) {
-                judiciaryFormatted += ', ';
-            }
-
-            if (ListParseHelperService.writeStringIfValid(judiciary?.johTitle) !== '') {
-                judiciaryFormatted += ListParseHelperService.writeStringIfValid(judiciary?.johTitle);
-            }
-
-            if (ListParseHelperService.writeStringIfValid(judiciary?.johNameSurname) !== '') {
-                if (ListParseHelperService.writeStringIfValid(judiciary?.johTitle) !== '') {
-                    judiciaryFormatted += ' ';
-                }
-                judiciaryFormatted += ListParseHelperService.writeStringIfValid(judiciary?.johNameSurname);
-            }
-        });
-        return judiciaryFormatted;
     }
 }
