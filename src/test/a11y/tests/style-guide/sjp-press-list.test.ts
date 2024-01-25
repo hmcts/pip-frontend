29fba3225ec6a2024230725e96ee3ec04fa9f8e6
Kian Kwa <kian.kwa@justice.gov.uk>
2024-01-25 22:32:50 +0000

    Added required stub

--- src/test/a11y/tests/style-guide/sjp-press-list.test.ts
+++ src/test/a11y/tests/style-guide/sjp-press-list.test.ts
@@ -1,8 +1,9 @@
 import sinon from 'sinon';
 import { PublicationService } from '../../../../main/service/publicationService';
 import { ListDownloadService } from '../../../../main/service/listDownloadService';
-import { testArtefactJsonData, testArtefactMetadata } from '../../common/testData';
+import {testArtefactJsonData, testArtefactMetadata, testLocationData} from '../../common/testData';
 import { testAccessibility } from '../../common/pa11yHelper';
+import {LocationRequests} from "../../../../main/resources/requests/locationRequests";
 
 const sjpPressFullListUrl = '/sjp-press-list';
 const sjpPressNewCasesUrl = '/sjp-press-list-new-cases';
@@ -17,12 +18,14 @@ const metaDataSjpFullList = testArtefactMetadata()[0];
 metaDataSjpFullList.listType = 'SJP_PRESS_LIST';
 const metaDataSjpNewCases = testArtefactMetadata()[0];
 metaDataSjpNewCases.listType = 'SJP_DELTA_PRESS_LIST';
+const locationData = testLocationData();
 
 const metadataStub = sinon.stub(PublicationService.prototype, 'getIndividualPublicationMetadata');
 metadataStub.withArgs(artefactIdMap.get(sjpPressFullListUrl)).resolves(metaDataSjpFullList);
 metadataStub.withArgs(artefactIdMap.get(sjpPressNewCasesUrl)).resolves(metaDataSjpNewCases);
 
 sinon.stub(PublicationService.prototype, 'getIndividualPublicationJson').resolves(jsonData);
+sinon.stub(LocationRequests.prototype, 'getLocation').resolves(locationData);
 sinon.stub(ListDownloadService.prototype, 'generateFiles').resolves(true);
 
 describe('Accessibility - SJP Press List (Full List) Page', () => {
--- src/test/a11y/tests/style-guide/sscs-daily-list.test.ts
+++ src/test/a11y/tests/style-guide/sscs-daily-list.test.ts
@@ -1,7 +1,8 @@
 import sinon from 'sinon';
 import { PublicationService } from '../../../../main/service/publicationService';
-import { testArtefactJsonData, testArtefactMetadata } from '../../common/testData';
+import {testArtefactJsonData, testArtefactMetadata, testLocationData} from '../../common/testData';
 import { testAccessibility } from '../../common/pa11yHelper';
+import {LocationRequests} from "../../../../main/resources/requests/locationRequests";
 
 const sscsDailyListUrl = '/sscs-daily-list';
 const sscsDailyListAdditionalHearingsUrl = '/sscs-daily-list-additional-hearings';
@@ -16,8 +17,11 @@ const metaDataSscs = testArtefactMetadata()[0];
 metaDataSscs.listType = 'SSCS_DAILY_LIST';
 const metaDataSscsAdditionalHearings = testArtefactMetadata()[0];
 metaDataSscsAdditionalHearings.listType = 'SSCS_DAILY_LIST_ADDITIONAL_HEARINGS';
+const locationData = testLocationData();
 
 sinon.stub(PublicationService.prototype, 'getIndividualPublicationJson').resolves(jsonData);
+sinon.stub(LocationRequests.prototype, 'getLocation').resolves(locationData);
+
 const metadataStub = sinon.stub(PublicationService.prototype, 'getIndividualPublicationMetadata');
 metadataStub.withArgs(artefactIdMap.get(sscsDailyListUrl)).resolves(metaDataSscs);
 metadataStub.withArgs(artefactIdMap.get(sscsDailyListAdditionalHearingsUrl)).resolves(metaDataSscsAdditionalHearings);
