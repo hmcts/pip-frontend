02a498a2213888366543fb015d59e70c1d485095
Kian Kwa <kian.kwa@justice.gov.uk>
2024-02-07 20:15:47 +0000

    Fix unit tests

--- src/test/unit/helpers/listHelper.test.ts
+++ src/test/unit/helpers/listHelper.test.ts
@@ -5,11 +5,11 @@ import path from 'path';
 describe('List helper', () => {
     it('Hearing should have party', () => {
         const rawData = fs.readFileSync(path.resolve(__dirname, '../mocks/hearingparty/crownDailyList.json'), 'utf-8');
-        expect(hearingHasParty(JSON.stringify(rawData))).toBeTruthy();
+        expect(hearingHasParty(JSON.parse(rawData))).toBeTruthy();
     });
 
     it('Hearing should have no party', () => {
         const rawData = fs.readFileSync(path.resolve(__dirname, '../mocks/crownDailyList.json'), 'utf-8');
-        expect(hearingHasParty(rawData)).toBeFalsy();
+        expect(hearingHasParty(JSON.parse(rawData))).toBeFalsy();
     });
 });
