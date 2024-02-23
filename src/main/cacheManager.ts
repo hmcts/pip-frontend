17c0ecf28d2e06b6485c57f6bc85aee378af2940
ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
2024-02-23 11:28:48 +0000

    PUB-2335 - Updated formatting

--- src/main/cacheManager.ts
+++ src/main/cacheManager.ts
@@ -4,12 +4,10 @@ import config from 'config';
 function getRedisPassword(): string {
     if (config.has('secrets.pip-ss-kv.REDIS_PASSWORD')) {
         return config.get('secrets.pip-ss-kv.REDIS_PASSWORD');
-    } else {
-        if (process.env.REDIS_LOCAL || process.env.REDIS_MOCK) {
+    } else if (process.env.REDIS_LOCAL || process.env.REDIS_MOCK) {
             return '';
-        } else {
-            throw new Error('A password must be set for non local / mock environments');
-        }
+    } else {
+        throw new Error('A password must be set for non local / mock environments');
     }
 }
 
