a1841474b8adb94a7abc619d907b57fbcd275a63
ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
2024-02-26 11:34:47 +0000

    PUB-2335 - Fortify empty password (#1214)
    * PUB-2335 - Fixed empty password in config
    
    * PUB-2335 - Fortify empty password
    
    * PUB-2335 - Fixed linting
    
    * PUB-2335 - Updated formatting
    
    * PUB-2335 - Updated formatting
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/cacheManager.ts
+++ src/main/cacheManager.ts
@@ -1,35 +1,43 @@
 import { Logger } from '@hmcts/nodejs-logging';
 import config from 'config';
 
+function getRedisPassword(): string {
+    if (config.has('secrets.pip-ss-kv.REDIS_PASSWORD')) {
+        return config.get('secrets.pip-ss-kv.REDIS_PASSWORD');
+    } else if (process.env.REDIS_LOCAL || process.env.REDIS_MOCK) {
+        return '';
+    } else {
+        throw new Error('A password must be set for non local / mock environments');
+    }
+}
+
 export function setRedisCredentials(): any {
     return {
         host: process.env.REDIS_HOST ? process.env.REDIS_HOST : config.get('secrets.pip-ss-kv.REDIS_HOST'),
         port: process.env.REDIS_PORT ? process.env.REDIS_PORT : config.get('secrets.pip-ss-kv.REDIS_PORT'),
-        password: process.env.REDIS_PASSWORD
-            ? process.env.REDIS_PASSWORD
-            : config.get('secrets.pip-ss-kv.REDIS_PASSWORD'),
+        password: process.env.REDIS_PASSWORD ? process.env.REDIS_PASSWORD : getRedisPassword(),
     };
 }
 
-const redisCredentials = setRedisCredentials();
-
 const logger = Logger.getLogger('app');
-const ioRedis = require('ioredis');
-
-let connectionString = '';
-if (process.env.REDIS_LOCAL) {
-    // for running local dev environment (i.e. 'start:dev' profile)
-    connectionString = `redis://:${redisCredentials.password}@${redisCredentials.host}:${redisCredentials.port}`;
-} else {
-    // double s is required when using TLS connection (i.e. 'start' profile)
-    connectionString = `rediss://:${redisCredentials.password}@${redisCredentials.host}:${redisCredentials.port}`;
-}
 
 let redisClient;
 if (process.env.REDIS_MOCK) {
     const redis = require('redis-mock');
     redisClient = redis.createClient();
 } else {
+    const redisCredentials = setRedisCredentials();
+    const ioRedis = require('ioredis');
+
+    let connectionString = '';
+    if (process.env.REDIS_LOCAL) {
+        // for running local dev environment (i.e. 'start:dev' profile)
+        connectionString = `redis://:${redisCredentials.password}@${redisCredentials.host}:${redisCredentials.port}`;
+    } else {
+        // double s is required when using TLS connection (i.e. 'start' profile)
+        connectionString = `rediss://:${redisCredentials.password}@${redisCredentials.host}:${redisCredentials.port}`;
+    }
+
     logger.info('Connecting to Redis');
     redisClient = new ioRedis(connectionString, { connectTimeout: 10000 });
 }
