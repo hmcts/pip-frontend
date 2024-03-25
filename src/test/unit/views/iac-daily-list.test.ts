a8f71006dab51232f51942600bc060e98e062cee
Paul Hudson <paul.hudson3@justice.gov.uk>
2024-03-25 14:10:00 +0000

    PUB-2186 - Modified IAC Daily List Nunjucks file and List test

--- src/test/unit/views/iac-daily-list.test.ts
+++ src/test/unit/views/iac-daily-list.test.ts
@@ -1,10 +1,10 @@
 import fs from 'fs';
 import path from 'path';
-import { PublicationService } from '../../../main/service/publicationService';
+import {PublicationService} from '../../../main/service/publicationService';
 import sinon from 'sinon';
 import request from 'supertest';
-import { app } from '../../../main/app';
-import { expect } from 'chai';
+import {app} from '../../../main/app';
+import {expect} from 'chai';
 
 const PAGE_URL = '/iac-daily-list?artefactId=abc';
 const headingClass = 'govuk-heading-l';
@@ -89,5 +89,10 @@ describe('IAC daily cause list page', () => {
     it('should display session channel if sitting channel is not present', () => {
         const cell = htmlRes.getElementsByClassName('govuk-table__cell');
         expect(cell[26].innerHTML).contains('VIDEO HEARING,');
+    });
+
+    it('should display hearing type if present', () => {
+        const cell = htmlRes.getElementsByClassName('govuk-table__cell');
+        expect(cell[6].innerHTML).contains('Directions');
     });
 });
