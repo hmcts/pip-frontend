71f63196361d031961997c10fc9db06399b4318a
ashwini.venkatesha <ashwini.venkatesha@justice.gov.uk>
2024-03-27 17:33:23 +0000

    Line alignment

--- src/test/end-to-end/tests/search-court-test.ts
+++ src/test/end-to-end/tests/search-court-test.ts
@@ -1,10 +1,10 @@
-import * as Assert from "assert";
+import * as Assert from 'assert';
 
 Feature('Search for court or tribunal');
 
 const invalidCourtName = 'InvalidCourt';
 
-Scenario('I should be able to view error summary if no results', async ({ I }) => {
+Scenario('I should be able to view error summary if no results', async ({I}) => {
     I.amOnPage('/search');
     I.waitForText('What court or tribunal are you interested in?');
     I.fillField('#search-input', invalidCourtName);
@@ -15,7 +15,7 @@ Scenario('I should be able to view error summary if no results', async ({ I }) =
     I.see('There is nothing matching your criteria');
 });
 
-Scenario('I should be able to view error summary in Welsh if no results', async ({ I }) => {
+Scenario('I should be able to view error summary in Welsh if no results', async ({I}) => {
     I.amOnPage('/search');
     I.waitForText('What court or tribunal are you interested in?');
     I.click(locate('//a').withText('Cymraeg'));
@@ -30,10 +30,13 @@ Scenario('I should be able to view error summary in Welsh if no results', async 
     I.see("Nid oes dim sy'n cyfateb i'ch meini prawf");
 });
 
-Scenario('As a user when I search I should be able to view dynamic court list to present as the first matching focussed results', async ({ I }) => {
-    I.amOnPage('/search');
-    I.waitForText('What court or tribunal are you interested in?');
-    I.fillField('#search-input', 'Mil');
-    const firstListItem = (await I.grabTextFrom(locate('#search-input__option--0'))).trim();
-    Assert.ok(firstListItem.startsWith('Mil'));
-});
+Scenario(
+    'As a user when I search I should be able to view dynamic court list to present as the first matching focussed results',
+    async ({I}) => {
+        I.amOnPage('/search');
+        I.waitForText('What court or tribunal are you interested in?');
+        I.fillField('#search-input', 'Mil');
+        const firstListItem = (await I.grabTextFrom(locate('#search-input__option--0'))).trim();
+        Assert.ok(firstListItem.startsWith('Mil'));
+    }
+);
