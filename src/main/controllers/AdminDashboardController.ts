12904981d1a3f7414d650882cf771cb12b12bdce
abottenberg <alexbottenberg@hotmail.com>
2024-03-27 08:46:25 +0000

    PUB-2029: Adding pending media requests banner

--- src/main/controllers/AdminDashboardController.ts
+++ src/main/controllers/AdminDashboardController.ts
@@ -1,11 +1,16 @@
 import { PipRequest } from '../models/request/PipRequest';
 import { Response } from 'express';
 import { cloneDeep } from 'lodash';
+import {MediaApplicationService} from "../service/mediaApplicationService";
+
+const mediaApplicationService = new MediaApplicationService();
 
 export default class AdminDashboardController {
-    public get(req: PipRequest, res: Response): void {
+    public async get(req: PipRequest, res: Response): Promise<void> {
+        const mediaApplications = await mediaApplicationService.getDateOrderedMediaApplications();
         res.render('admin-dashboard', {
             ...cloneDeep(req.i18n.getDataByLanguage(req.lng)['admin-dashboard']),
+            mediaApplications,
             user: req.user,
         });
     }
