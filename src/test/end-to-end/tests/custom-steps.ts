7291068c70b07f49894439ab4234d4879aa2a018
ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
2024-04-12 10:09:01 +0100

    PUB-2375 - Styling changes

--- src/test/end-to-end/tests/custom-steps.ts
+++ src/test/end-to-end/tests/custom-steps.ts
@@ -56,7 +56,7 @@ export = function () {
         },
 
         seeBetaFeedbackOnPage: function (page) {
-            this.see('Beta');
+            this.see('BETA');
             this.click('feedback');
             this.seeInCurrentUrl(`https://www.smartsurvey.co.uk/s/FBSPI22/?pageurl=${page}`);
         },
