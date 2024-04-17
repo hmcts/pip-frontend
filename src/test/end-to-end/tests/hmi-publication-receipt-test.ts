c11167f651e436bbe2545c854640c8865838d4eb
Kian Kwa <kian.kwa@justice.gov.uk>
2024-04-17 16:26:18 +0100

    Fix HMI publication receipt nightly test

--- src/test/end-to-end/tests/hmi-publication-receipt-test.ts
+++ src/test/end-to-end/tests/hmi-publication-receipt-test.ts
@@ -3,7 +3,7 @@ import { DateTime } from 'luxon';
 Feature('HMI publication receipt');
 
 Scenario('I should be able to confirm HMI publication is received successfully for the current date', async ({ I }) => {
-    const dateYesterday = DateTime.now().minus({ days: 1 }).toFormat('dd MMMM yyyy');
+    const expectedDate = DateTime.now().toFormat('dd MMMM yyyy');
     const locationName = 'HMI AUTOMATED TEST COURT';
 
     I.amOnPage('/search');
@@ -12,5 +12,5 @@ Scenario('I should be able to confirm HMI publication is received successfully f
     I.pressKey('Escape');
     I.click('Continue');
     I.waitForText('What do you want to view from ' + locationName);
-    I.see('Care Standards Tribunal Hearing List ' + dateYesterday);
+    I.see('Care Standards Tribunal Hearing List ' + expectedDate);
 }).tag('@Nightly');
