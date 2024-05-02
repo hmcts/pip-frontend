6105e046b458efbc7e43c09a4706c463debd0902
Natasha Alker <NatashaAlker@yahoo.co.uk>
2024-05-02 09:39:35 +0100

    ...

--- src/main/helpers/listHelper.ts
+++ src/main/helpers/listHelper.ts
@@ -11,6 +11,26 @@ export const isValidList = (searchResults: any, metaData: any): any => {
     }
 };
 
+export const formatMetaDataListType = (metaData: any): any => {
+    if (metaData) {
+        return metaData.listType.toLowerCase().replaceAll('_', '-');
+    } else {
+        return '';
+    }
+}
+
+export const isValidListType = (metaDataListType: any, listType: any): any => {
+    return metaDataListType === listType;
+}
+
+export const isOneOfValidListTypes = (metaDataListType: any, listType: any, altListType: any): any => {
+    return metaDataListType === listType || metaDataListType === altListType;
+}
+
+export const missingListType = (metaDataListType: any): any => {
+    return metaDataListType === '';
+}
+
 // TODO: To be removed once all lists have party field on the case level.
 export const hearingHasParty = (jsonData): boolean => {
     let hearingHasParty = false;
