83d85d3e726108c8ec7c2507c406b0f5505086e5
Natasha Alker <NatashaAlker@yahoo.co.uk>
2024-05-03 12:59:21 +0100

    re add opa results list

--- src/main/controllers/style-guide/OpaResultsController.ts
+++ src/main/controllers/style-guide/OpaResultsController.ts
@@ -3,7 +3,7 @@ import { PipRequest } from '../../models/request/PipRequest';
 import { PublicationService } from '../../service/PublicationService';
 import { ListParseHelperService } from '../../service/ListParseHelperService';
 import { LocationService } from '../../service/LocationService';
-import {formatMetaDataListType, isUnexpectedListType, isValidList, isValidListType} from '../../helpers/listHelper';
+import { formatMetaDataListType, isUnexpectedListType, isValidList, isValidListType } from '../../helpers/listHelper';
 import { HttpStatusCode } from 'axios';
 import { cloneDeep } from 'lodash';
 import { CrimeListsService } from '../../service/listManipulation/CrimeListsService';
@@ -43,7 +43,9 @@ export default class OpaResultsController {
                 courtName: locationName,
                 venueAddress: venueAddress,
             });
-        } else if (jsonData === HttpStatusCode.NotFound || metaData === HttpStatusCode.NotFound ||
+        } else if (
+            jsonData === HttpStatusCode.NotFound ||
+            metaData === HttpStatusCode.NotFound ||
             isUnexpectedListType(metadataListType, listType)
         ) {
             res.render('list-not-found', req.i18n.getDataByLanguage(req.lng)['list-not-found']);
