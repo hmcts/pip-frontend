ef087d4610918e3a1839284ad282bf374a91114c
Natasha Alker <NatashaAlker@yahoo.co.uk>
2024-05-03 09:15:39 +0100

    update tests

--- src/test/helpers/listHelper.test.ts
+++ src/test/helpers/listHelper.test.ts
@@ -1,9 +1,5 @@
 import {
-    formatMetaDataListType,
-    isOneOfValidListTypes,
-    isValidList,
-    isValidListType,
-    missingListType,
+    isValidList
 } from '../../main/helpers/listHelper';
 import { HttpStatusCode } from 'axios';
 
@@ -27,54 +23,5 @@ describe('List Helper', () => {
         const metaData = HttpStatusCode.NotFound;
 
         expect(isValidList(searchResults, metaData)).toBe(true);
-    });
-
-    it('should return formatted list type', () => {
-        const metaData = { listType: 'TEST_LIST_TYPE' };
-
-        expect(formatMetaDataListType(metaData)).toBe('test-list-type');
-    });
-
-    it('should not return any list type', () => {
-        const metaData = null;
-
-        expect(formatMetaDataListType(metaData)).toBe('');
-    });
-
-    it('should return true if the list type is valid', () => {
-        const metaDataListType = 'TEST_LIST_TYPE';
-        const altMetaDataListType = 'ALT_TEST_LIST_TYPE';
-        const listType = 'test-list-type';
-        const altListType = 'alt-test-list-type';
-
-        expect(isValidListType(metaDataListType, listType)).toBe(true);
-        expect(isOneOfValidListTypes(altMetaDataListType, listType, altListType)).toBe(true);
-    });
-
-    it('should return false if the list type is not valid', () => {
-        const metaDataListType = 'TEST_LIST_TYPE';
-        const listType = 'another-test-list-type';
-        const altListType = 'alt-test-list-type';
-
-        expect(isValidListType(metaDataListType, listType)).toBe(false);
-        expect(isOneOfValidListTypes(metaDataListType, listType, altListType)).toBe(false);
-    });
-
-    it('should return true if the list type is blank', () => {
-        const metaDataListType = '';
-
-        expect(missingListType(metaDataListType)).toBe(true);
-    });
-
-    it('should return false if the list type is not blank', () => {
-        const metaDataListType = '';
-
-        expect(missingListType(metaDataListType)).toBe(true);
-    });
-
-    it('should return false if the list type is not valid', () => {
-        const metaDataListType = 'TEST_LIST_TYPE';
-
-        expect(missingListType(metaDataListType)).toBe(false);
     });
 });
--- src/test/unit/helpers/listHelper.test.ts
+++ src/test/unit/helpers/listHelper.test.ts
@@ -1,4 +1,9 @@
-import { hearingHasParty } from '../../../main/helpers/listHelper';
+import {
+    formatMetaDataListType,
+    hearingHasParty,
+    isOneOfValidListTypes,
+    isValidListType, missingListType
+} from '../../../main/helpers/listHelper';
 import fs from 'fs';
 import path from 'path';
 
@@ -12,4 +17,48 @@ describe('List helper', () => {
         const rawData = fs.readFileSync(path.resolve(__dirname, '../mocks/crownDailyList.json'), 'utf-8');
         expect(hearingHasParty(JSON.parse(rawData))).toBeFalsy();
     });
+
+    it('should return formatted list type', () => {
+        const metaData = { listType: 'TEST_LIST_TYPE' };
+
+        expect(formatMetaDataListType(metaData)).toBe('test-list-type');
+    });
+
+    it('should not return any list type', () => {
+        const metaData = null;
+
+        expect(formatMetaDataListType(metaData)).toBe('');
+    });
+
+    it('should return true if the list type is valid', () => {
+        const metaDataListType = 'test-list-type';
+        const altMetaDataListType = 'alt-test-list-type';
+        const listType = 'test-list-type';
+        const altListType = 'alt-test-list-type';
+
+        expect(isValidListType(metaDataListType, listType)).toBe(true);
+        expect(isOneOfValidListTypes(altMetaDataListType, listType, altListType)).toBe(true);
+    });
+
+    it('should return false if the list type is not valid', () => {
+        const metaDataListType = 'test-list-type';
+        const listType = 'another-test-list-type';
+        const altListType = 'alt-test-list-type';
+
+        expect(isValidListType(metaDataListType, listType)).toBe(false);
+        expect(isOneOfValidListTypes(metaDataListType, listType, altListType)).toBe(false);
+    });
+
+    it('should return true if the list type is blank', () => {
+        const metaDataListType = '';
+
+        expect(missingListType(metaDataListType)).toBe(true);
+    });
+
+    it('should return false if the list type is not blank', () => {
+        const metaDataListType = 'TEST_LIST_TYPE';
+
+        expect(missingListType(metaDataListType)).toBe(false);
+    });
+
 });
