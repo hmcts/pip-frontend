98fdce996b812bf9d1cae310d60bb9bb2279aecd
abottenberg <alexbottenberg@hotmail.com>
2024-05-23 11:56:41 +0100

    PUB-2361: Cleaning up code

--- src/main/controllers/RemoveListConfirmationController.ts
+++ src/main/controllers/RemoveListConfirmationController.ts
@@ -34,47 +34,48 @@ export default class RemoveListConfirmationController {
     }
 
     public async post(req: PipRequest, res: Response): Promise<void> {
-        const listsToDelete = req.body.artefactIds;
-        const locationId = req.body.locationId;
+        const listsToDelete = req.body?.artefactIds;
+        const locationId = req.body?.locationId;
         const formData = req.body;
         const listData = [];
-        switch (formData['remove-choice']) {
-            case 'yes': {
-                const response = await removeListHelperService.removeLists(
-                    listsToDelete,
-                    req.user?.['userId']
-                );
-                if (response) {
-                    await userManagementService.auditAction(
-                        req.user,
-                        'DELETE_PUBLICATION',
-                        removeListHelperService.formatArtefactIdsForAudit(listsToDelete)
-                    );
-                    res.redirect('/remove-list-success');
-                } else {
-                    res.render('error', req.i18n.getDataByLanguage(req.lng).error);
-                }
-                break;
-            }
-            case 'no': {
-                res.clearCookie('formCookie');
-                res.redirect(`/remove-list-search-results?locationId=${locationId}`);
-                break;
-            }
-            default:
-                if (Array.isArray(listsToDelete)) {
-                    for (const artefactId of listsToDelete) {
-                        await addListDetailsToArray(artefactId, req.user?.['userId'], listData);
+        if (listsToDelete && locationId) {
+            switch (formData['remove-choice']) {
+                case 'yes': {
+                    const response = await removeListHelperService.removeLists(listsToDelete, req.user?.['userId']);
+                    if (response) {
+                        await userManagementService.auditAction(
+                            req.user,
+                            'DELETE_PUBLICATION',
+                            removeListHelperService.formatArtefactIdsForAudit(listsToDelete)
+                        );
+                        res.redirect('/remove-list-success');
+                    } else {
+                        res.render('error', req.i18n.getDataByLanguage(req.lng).error);
                     }
-                } else {
-                    await addListDetailsToArray(listsToDelete, req.user?.['userId'], listData);
+                    break;
                 }
-                res.render('remove-list-confirmation', {
-                    ...cloneDeep(req.i18n.getDataByLanguage(req.lng)['remove-list-confirmation']),
-                    court: await courtService.getLocationById(locationId),
-                    removalList: manualUploadService.formatListRemovalValues(listData),
-                    displayError: true,
-                });
+                case 'no': {
+                    res.clearCookie('formCookie');
+                    res.redirect(`/remove-list-search-results?locationId=${locationId}`);
+                    break;
+                }
+                default:
+                    if (Array.isArray(listsToDelete)) {
+                        for (const artefactId of listsToDelete) {
+                            await addListDetailsToArray(artefactId, req.user?.['userId'], listData);
+                        }
+                    } else {
+                        await addListDetailsToArray(listsToDelete, req.user?.['userId'], listData);
+                    }
+                    res.render('remove-list-confirmation', {
+                        ...cloneDeep(req.i18n.getDataByLanguage(req.lng)['remove-list-confirmation']),
+                        court: await courtService.getLocationById(locationId),
+                        removalList: manualUploadService.formatListRemovalValues(listData),
+                        displayError: true,
+                    });
+            }
+        } else {
+            res.render('error', req.i18n.getDataByLanguage(req.lng).error);
         }
     }
 }
