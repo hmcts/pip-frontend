ba65f0a1aac85aa806fecaf165ca09bf3cffab01
Paul Hudson <paul.hudson3@justice.gov.uk>
2024-06-12 13:39:11 +0100

    PUB-2454 - Added Style Guides for both English and Welsh (#1286)
    * PUB-2454 - Added Style Guides for both English and Welsh
    
    * PUB-2454 - Linting
    
    * PUB-2454 - More linting
    
    * PUB-2454 - Updated JSON + Tests
    
    * PUB-2454 - Updated translations + added accessibility test
    
    * Linting for IAC test
    
    * PUB-2454 - Updated controller + test
    
    * PUB-2454 - Fixed linting
    
    * PUB-2454 - Welsh Translation
    
    * PUB-2454 - Updated case
    
    * Updated list lookup
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>

--- src/test/a11y/tests/style-guide/iac-daily-list.test.ts
+++ src/test/a11y/tests/style-guide/iac-daily-list.test.ts
@@ -3,14 +3,26 @@ import { PublicationService } from '../../../../main/service/PublicationService'
 import { testArtefactJsonData, testArtefactMetadata } from '../../common/testData';
 import { testAccessibility } from '../../common/pa11yHelper';
 
-const url = '/iac-daily-list?artefactId=abc';
+const url = '/iac-daily-list?artefactId=1234';
+const additionalCasesUrl = '/iac-daily-list-additional-cases?artefactId=12345';
 
 const jsonData = testArtefactJsonData('iacDailyList.json');
 const metadata = testArtefactMetadata()[0];
+metadata.listType = 'IAC_DAILY_LIST';
+
+const additionalCasesMetadata = testArtefactMetadata()[0];
+additionalCasesMetadata.listType = 'IAC_DAILY_LIST_ADDITIONAL_CASES';
 
 sinon.stub(PublicationService.prototype, 'getIndividualPublicationJson').resolves(jsonData);
-sinon.stub(PublicationService.prototype, 'getIndividualPublicationMetadata').resolves(metadata);
+const metadataStub = sinon.stub(PublicationService.prototype, 'getIndividualPublicationMetadata');
+
+metadataStub.withArgs('1234').resolves(metadata);
+metadataStub.withArgs('12345').resolves(additionalCasesMetadata);
 
 describe('Accessibility - IAC Daily List Page', () => {
     testAccessibility(url);
+});
+
+describe('Accessibility - IAC Daily List Additional Hearings Page', () => {
+    testAccessibility(additionalCasesUrl);
 });
