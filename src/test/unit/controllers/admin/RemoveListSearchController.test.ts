dbfe0418a5c4ef393ed39f0fa2f06aef53e2f175
KianKwa <80688761+KianKwa@users.noreply.github.com>
2024-07-15 11:29:42 +0100

    PUB-2426 Refactor folder structure for admin and system-admin routes (#1302)
    * Refactor folder structure for admin and system-admin routes
    
    * Lint fixes
    
    * Fixed failing unit tests
    
    * Lint fixes
    
    * Update language file path
    
    * Fixed failing route test
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/test/unit/controllers/admin/RemoveListSearchController.test.ts
@@ -0,0 +1,129 @@
+import { Response } from 'express';
+import { LocationService } from '../../../../main/service/LocationService';
+import { mockRequest } from '../../mocks/mockRequest';
+import RemoveListSearchController from '../../../../main/controllers/admin/RemoveListSearchController';
+import sinon from 'sinon';
+import fs from 'fs';
+import path from 'path';
+
+const courtStub = sinon.stub(LocationService.prototype, 'getLocationByName');
+const removeListSearchController = new RemoveListSearchController();
+const rawData = fs.readFileSync(path.resolve(__dirname, '../../mocks/courtAndHearings.json'), 'utf-8');
+const courtList = JSON.parse(rawData);
+const court = { locationId: 1 };
+sinon.stub(LocationService.prototype, 'fetchAllLocations').returns(courtList);
+courtStub.withArgs('aa').resolves(null);
+courtStub.withArgs('test').resolves(null);
+courtStub.withArgs('Mut').resolves(null);
+courtStub.withArgs('Valid Location').resolves(court);
+
+const i18n = {
+    admin: {
+        'remove-list-search': {},
+    },
+};
+
+describe('Remove List Search Controller', () => {
+    it('should render the remove list search page', () => {
+        const response = {
+            render: () => {
+                return '';
+            },
+        } as unknown as Response;
+        const request = mockRequest(i18n);
+        const responseMock = sinon.mock(response);
+        const expectedData = {
+            ...i18n['admin']['remove-list-search'],
+            autocompleteList: courtList,
+            invalidInputError: false,
+            noResultsError: false,
+        };
+
+        responseMock.expects('render').once().withArgs('admin/remove-list-search', expectedData);
+        return removeListSearchController.get(request, response).then(() => {
+            responseMock.verify();
+        });
+    });
+
+    it('should render remove list search page if input is less than three characters long', () => {
+        const response = {
+            render: () => {
+                return '';
+            },
+        } as unknown as Response;
+        const request = mockRequest(i18n);
+        request.body = { 'input-autocomplete': 'aa' };
+        const responseMock = sinon.mock(response);
+        const expectedData = {
+            ...i18n['admin']['remove-list-search'],
+            autocompleteList: courtList,
+            invalidInputError: true,
+            noResultsError: false,
+        };
+
+        responseMock.expects('render').once().withArgs('admin/remove-list-search', expectedData);
+        return removeListSearchController.post(request, response).then(() => {
+            responseMock.verify();
+        });
+    });
+
+    it('should render remove list search page if there are no matching results', () => {
+        const response = {
+            render: () => {
+                return '';
+            },
+        } as unknown as Response;
+        const request = mockRequest(i18n);
+        request.body = { 'input-autocomplete': 'test' };
+        const responseMock = sinon.mock(response);
+        const expectedData = {
+            ...i18n['admin']['remove-list-search'],
+            autocompleteList: courtList,
+            invalidInputError: false,
+            noResultsError: true,
+        };
+
+        responseMock.expects('render').once().withArgs('admin/remove-list-search', expectedData);
+        return removeListSearchController.post(request, response).then(() => {
+            responseMock.verify();
+        });
+    });
+
+    it('should render remove list search page if input is three characters long and partially correct as noResultsError', () => {
+        const response = {
+            render: () => {
+                return '';
+            },
+        } as unknown as Response;
+        const request = mockRequest(i18n);
+        request.body = { 'input-autocomplete': 'Mut' };
+        const responseMock = sinon.mock(response);
+        const expectedData = {
+            ...i18n['admin']['remove-list-search'],
+            autocompleteList: courtList,
+            invalidInputError: false,
+            noResultsError: true,
+        };
+
+        responseMock.expects('render').once().withArgs('admin/remove-list-search', expectedData);
+        return removeListSearchController.post(request, response).then(() => {
+            responseMock.verify();
+        });
+    });
+
+    it('should redirect to removal confirmation page with input as query if court name input is valid', () => {
+        const response = {
+            redirect: () => {
+                return '';
+            },
+        } as unknown as Response;
+        const request = mockRequest(i18n);
+        request.body = { 'input-autocomplete': 'Valid Location' };
+        const responseMock = sinon.mock(response);
+
+        responseMock.expects('redirect').once().withArgs('remove-list-search-results?locationId=1');
+        return removeListSearchController.post(request, response).then(() => {
+            responseMock.verify();
+        });
+    });
+});
---
+++ src/test/unit/controllers/system-admin/DeleteCourtSubscriptionConfirmationController.test.ts
@@ -0,0 +1,129 @@
+import { Response } from 'express';
+import { LocationService } from '../../../../main/service/LocationService';
+import { SubscriptionService } from '../../../../main/service/SubscriptionService';
+import { mockRequest } from '../../mocks/mockRequest';
+import sinon from 'sinon';
+import DeleteCourtSubscriptionConfirmationController from '../../../../main/controllers/system-admin/DeleteCourtSubscriptionConfirmationController';
+import { PublicationService } from '../../../../main/service/PublicationService';
+
+const courtStub = sinon.stub(LocationService.prototype, 'getLocationById');
+const subDeleteStub = sinon.stub(SubscriptionService.prototype, 'deleteLocationSubscription');
+const pubDeleteStub = sinon.stub(PublicationService.prototype, 'deleteLocationPublication');
+
+const deleteCourtSubscriptionConfirmationController = new DeleteCourtSubscriptionConfirmationController();
+
+const court = { locationId: 1, jurisdiction: 'test', region: 'test' };
+sinon.stub(LocationService.prototype, 'formatCourtValue').returns(court);
+courtStub.withArgs('1').resolves(court);
+subDeleteStub.withArgs('1').resolves('success');
+subDeleteStub.withArgs('2').resolves(null);
+pubDeleteStub.withArgs('1').resolves('success');
+pubDeleteStub.withArgs('2').resolves(null);
+
+const pageName = 'delete-court-subscription-confirmation';
+const pagePublicationName = 'delete-court-publication-confirmation';
+const i18n = {
+    'system-admin': {
+        'delete-court-subscription-confirmation': {},
+    },
+};
+
+describe('Delete Court Subscription Controller', () => {
+    it('should render confirmation page if unexpected error occurred when deleting a subscription', () => {
+        const response = {
+            render: () => {
+                return '';
+            },
+        } as unknown as Response;
+        const request = mockRequest(i18n);
+        request.body = { locationId: '2', 'delete-choice': 'yes' };
+        const responseMock = sinon.mock(response);
+        const expectedData = {
+            ...i18n['system-admin']['delete-court-subscription-confirmation'],
+            court: court,
+            apiError: true,
+            errorMessage: 'Unknown error when attempting to delete all the subscriptions for the court',
+        };
+
+        responseMock
+            .expects('render')
+            .once()
+            .withArgs('system-admin/delete-court-subscription-confirmation', expectedData);
+        return deleteCourtSubscriptionConfirmationController.post(request, response, pageName).then(() => {
+            responseMock.verify();
+        });
+    });
+
+    it('should render confirmation page if unexpected error occurred when deleting the publication', () => {
+        const response = {
+            render: () => {
+                return '';
+            },
+        } as unknown as Response;
+        const request = mockRequest(i18n);
+        request.body = { locationId: '2', 'delete-choice': 'yes' };
+        const responseMock = sinon.mock(response);
+        const expectedData = {
+            ...i18n['system-admin']['delete-court-publication-confirmation'],
+            court: court,
+            apiError: true,
+            errorMessage: 'Unknown error when attempting to delete all the artefacts for the court',
+        };
+
+        responseMock
+            .expects('render')
+            .once()
+            .withArgs('system-admin/delete-court-publication-confirmation', expectedData);
+        return deleteCourtSubscriptionConfirmationController.post(request, response, pagePublicationName).then(() => {
+            responseMock.verify();
+        });
+    });
+
+    it('should redirect to success page when subscription is successfully deleted', () => {
+        const response = {
+            redirect: () => {
+                return '';
+            },
+        } as unknown as Response;
+        const request = mockRequest(i18n);
+        request.body = { locationId: '1', 'delete-choice': 'yes' };
+        const responseMock = sinon.mock(response);
+
+        responseMock.expects('redirect').once().withArgs('/delete-court-subscription-success?locationId=1');
+        return deleteCourtSubscriptionConfirmationController.post(request, response, pageName).then(() => {
+            responseMock.verify();
+        });
+    });
+
+    it('should redirect to success page when publication is successfully deleted', () => {
+        const response = {
+            redirect: () => {
+                return '';
+            },
+        } as unknown as Response;
+        const request = mockRequest(i18n);
+        request.body = { locationId: '1', 'delete-choice': 'yes' };
+        const responseMock = sinon.mock(response);
+
+        responseMock.expects('redirect').once().withArgs('/delete-court-publication-success?locationId=1');
+        return deleteCourtSubscriptionConfirmationController.post(request, response, pagePublicationName).then(() => {
+            responseMock.verify();
+        });
+    });
+
+    it('should render delete court reference data when No is selected', () => {
+        const response = {
+            redirect: () => {
+                return '';
+            },
+        } as unknown as Response;
+        const request = mockRequest(i18n);
+        request.body = { locationId: '1', 'delete-choice': 'no' };
+        const responseMock = sinon.mock(response);
+
+        responseMock.expects('redirect').once().withArgs('/delete-court-reference-data');
+        return deleteCourtSubscriptionConfirmationController.post(request, response, pageName).then(() => {
+            responseMock.verify();
+        });
+    });
+});
