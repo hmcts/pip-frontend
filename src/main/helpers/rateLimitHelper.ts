37dc23224baf88a77aea191ebddb4f95df979c6d
Kian Kwa <kian.kwa@justice.gov.uk>
2024-08-22 11:34:58 +0100

    Try fixing redis client

--- src/main/helpers/rateLimitHelper.ts
+++ src/main/helpers/rateLimitHelper.ts
@@ -5,34 +5,31 @@ import { PipRequest } from '../models/request/PipRequest';
 
 const { redisClient } = require('../cacheManager');
 
+const redisStore = process.env.REDIS_MOCK ? null
+    : new RedisStore({
+        prefix: 'RateLimit',
+        sendCommand: async (...args: string[]) => redisClient.call(args),
+    });
+
 export const standardRateLimiter = rateLimit({
     windowMs: 60 * 1000,
     limit: 50,
     message: 'Too many requests from this IP address, please try again later.',
-    store: new RedisStore({
-        prefix: 'RateLimit',
-        sendCommand: async (...args: string[]) => redisClient.call(...args),
-    }),
+    store: redisStore,
 });
 
 export const strictRateLimiter = rateLimit({
     windowMs: 60 * 1000,
     limit: 5,
     message: 'Too many requests from this IP address, please try again later.',
-    store: new RedisStore({
-        prefix: 'RateLimit',
-        sendCommand: async (...args: string[]) => redisClient.call(...args),
-    }),
+    store: redisStore,
 });
 
 export const rateLimiterWithUserId = rateLimit({
     windowMs: 60 * 1000,
     limit: 10,
     message: 'Too many requests from this IP address, please try again later.',
-    store: new RedisStore({
-        prefix: 'RateLimit',
-        sendCommand: async (...args: string[]) => redisClient.call(...args),
-    }),
+    store: redisStore,
     keyGenerator: function (req: PipRequest) {
         return req.user['userId'];
     },
@@ -42,8 +39,5 @@ export const slowDownLimiter = slowDown({
     windowMs: 60 * 1000,
     delayAfter: 5,
     delayMs: hits => hits * 200, // Add 200 ms of delay to every request after the 5th one.
-    store: new RedisStore({
-        prefix: 'RateLimit',
-        sendCommand: async (...args: string[]) => redisClient.call(...args),
-    }),
+    store: redisStore,
 });
