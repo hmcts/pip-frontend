56027249e007a03fae8a4bca3eded5e75ee8b2ae
ashwini.venkatesha <ashwini.venkatesha@justice.gov.uk>
2024-09-12 12:54:59 +0100

    Added playwright axe example setup

--- src/test/accessibility/fixture/axe-fixture.ts
+++ src/test/accessibility/fixture/axe-fixture.ts
@@ -1,5 +1,5 @@
-import { test as base } from "@playwright/test";
-import AxeBuilder from "@axe-core/playwright";
+import { test as base } from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright';
 
 type AxeFixture = {
     makeAxeBuilder: () => AxeBuilder;
@@ -9,10 +9,10 @@ export const test = base.extend<AxeFixture>({
     makeAxeBuilder: async ({ page }, use) => {
         const makeAxeBuilder = () =>
             new AxeBuilder({ page })
-                .withTags(["wcag2a", "wcag2aa", "wcag21a", "wcag21aa", "wcag22a", "wcag22aa"])
-                .exclude("#commonly-reused-element-with-known-issue");
+                .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa', 'wcag22a', 'wcag22aa'])
+                .exclude('#commonly-reused-element-with-known-issue');
 
         await use(makeAxeBuilder);
     },
 });
-export { expect } from "@playwright/test";
+export { expect } from '@playwright/test';
