499626488a63011ede5984d564aa3acc134fde0b
ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
2024-11-04 11:26:59 +0000

    PUB-2647 - Updated controller

--- src/main/controllers/SsoRejectedLoginController.ts
+++ src/main/controllers/SsoRejectedLoginController.ts
@@ -8,7 +8,7 @@ import { ssoNotAuthorised } from '../helpers/consts';
 export default class SsoRejectedLoginController {
     public async get(req: PipRequest, res: Response): Promise<void> {
         const sessionMessages = req['session']?.messages;
-        if (Array.isArray(sessionMessages) && sessionMessages.length > 0 && sessionMessages[0] === ssoNotAuthorised) {
+        if (sessionMessages && sessionMessages.includes(ssoNotAuthorised)) {
             res.render('sso-rejected-login', req.i18n.getDataByLanguage(req.lng)['sso-rejected-login']);
         } else {
             res.render('error', req.i18n.getDataByLanguage(req.lng).error);
