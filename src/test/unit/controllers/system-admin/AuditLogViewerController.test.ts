2e98c8d49674839302c635c47e97e938806efcdb
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2024-11-22 15:54:59 +0000

    add tests

--- src/test/unit/controllers/system-admin/AuditLogViewerController.test.ts
+++ src/test/unit/controllers/system-admin/AuditLogViewerController.test.ts
@@ -1,8 +1,9 @@
 import { mockRequest } from '../../mocks/mockRequest';
 import { Response } from 'express';
 import sinon from 'sinon';
-import AuditLogViewerController from '../../../../main/controllers/system-admin/AuditLogViewerController';
 import { AuditLogService } from '../../../../main/service/AuditLogService';
+import AuditLogViewerController from '../../../../main/controllers/system-admin/AuditLogViewerController';
+import { AuditLogSearchCriteria } from '../../../../main/models/AuditLogSearchCriteria';
 
 const auditLogViewerController = new AuditLogViewerController();
 
@@ -10,35 +11,145 @@ const i18n = {
     'audit-log-viewer': {},
 };
 
-sinon.stub(AuditLogService.prototype, 'getFormattedAuditData').returns({
+const userId = '1234';
+const email = 'TestAdminEmail';
+const testPage = '2';
+const testEmail = 'TestEmail';
+const testUserId = 'TestUserId';
+const testActions = 'TestActions';
+const testFilterDate = 'TestFilterDate';
+const testPath = '';
+
+const formattedDataStub = sinon.stub(AuditLogService.prototype, 'getFormattedAuditData');
+
+formattedDataStub
+    .withArgs(
+        new AuditLogSearchCriteria(testPage, testEmail, testUserId, testActions, testFilterDate),
+        testPath,
+        { userId: userId, email: email }
+    )
+    .returns({
+        auditLogData: 'otherData',
+        paginationData: 'test2',
+        emailFieldData: 'test3',
+        userIdFieldData: 'test4',
+        actionsFieldData: 'test5',
+        filterDateFieldData: 'test6',
+        categories: 'test7',
+    });
+
+formattedDataStub.returns({
     auditLogData: 'test',
     paginationData: 'test2',
+    emailFieldData: 'test3',
+    userIdFieldData: 'test4',
+    actionsFieldData: 'test5',
+    filterDateFieldData: 'test6',
+    categories: 'test7',
 });
 
 sinon.stub(AuditLogService.prototype, 'getTableHeaders').returns('testHeader');
+sinon.stub(AuditLogService.prototype, 'generateFilterKeyValues').returns('ThisIsAFilter=Filter');
+sinon.stub(AuditLogService.prototype, 'validateDate').returns('testDate');
 
-describe('Audit log viewer controller', () => {
+describe('Audit log view controller', () => {
     const response = {
         render: () => {
+            return '';
+        },
+        redirect: () => {
             return '';
         },
     } as unknown as Response;
     const request = mockRequest(i18n);
     request.path = '/audit-log-viewer';
 
-    it('should render the audit log viewer page', async () => {
-        request.query = { page: '1' };
+    it('should render the audit log view page', async () => {
+        request.url = '/audit-log-viewer';
+
         const responseMock = sinon.mock(response);
         const expectedData = {
             ...i18n['audit-log-viewer'],
             header: 'testHeader',
             auditLogData: 'test',
             paginationData: 'test2',
+            emailFieldData: 'test3',
+            userIdFieldData: 'test4',
+            actionsFieldData: 'test5',
+            filterDateFieldData: 'test6',
+            categories: 'test7',
+            displayError: false,
         };
 
         responseMock.expects('render').once().withArgs('system-admin/audit-log-viewer', expectedData);
 
         await auditLogViewerController.get(request, response);
+        return responseMock.verify();
+    });
+
+    it('should render the audit log view page when query is provided', async () => {
+        request.query = {
+            clear: undefined,
+            page: testPage,
+            email: testEmail,
+            userId: testUserId,
+            actions: testActions,
+            filterDate: testFilterDate,
+        };
+        request.url = '/audit-log-viewer';
+        request.user = { userId: userId, email: email };
+
+        const responseMock = sinon.mock(response);
+        const expectedData = {
+            ...i18n['audit-log-viewer'],
+            header: 'testHeader',
+            auditLogData: 'test',
+            paginationData: 'test2',
+            emailFieldData: 'test3',
+            userIdFieldData: 'test4',
+            actionsFieldData: 'test5',
+            filterDateFieldData: 'test6',
+            categories: 'test7',
+            displayError: false,
+        };
+
+        responseMock.expects('render').once().withArgs('system-admin/audit-log-viewer', expectedData);
+
+        await auditLogViewerController.get(request, response);
+        return responseMock.verify();
+    });
+
+    it('should redirect to the audit log view page on clear', async () => {
+        const mockFilter = 'ThisIsAFilter=Filter';
+
+        sinon.stub(AuditLogService.prototype, 'handleFilterClearing').returns('success');
+
+        request.query = { clear: 'all' };
+        request.url = '/audit-log-viewer';
+
+        const responseMock = sinon.mock(response);
+        responseMock
+            .expects('redirect')
+            .once()
+            .withArgs('audit-log-viewer?' + mockFilter);
+
+        await auditLogViewerController.get(request, response);
+        return responseMock.verify();
+    });
+
+    it('should redirect to the audit log view page on post', async () => {
+        const mockFilter = 'ThisIsAFilter=Filter';
+
+        request.url = '/audit-log-viewer';
+        request.body = 'FilterValues';
+
+        const responseMock = sinon.mock(response);
+        responseMock
+            .expects('redirect')
+            .once()
+            .withArgs('audit-log-viewer?' + mockFilter);
+
+        await auditLogViewerController.post(request, response);
         return responseMock.verify();
     });
 });
--- src/test/unit/service/AuditLogService.test.ts
+++ src/test/unit/service/AuditLogService.test.ts
@@ -1,27 +1,51 @@
 import { expect } from 'chai';
+import sinon from 'sinon';
 import { AuditLogService } from '../../../main/service/AuditLogService';
 import { AccountManagementRequests } from '../../../main/resources/requests/AccountManagementRequests';
-import sinon from 'sinon';
+import {AuditLogSearchCriteria} from "../../../main/models/AuditLogSearchCriteria";
 
 const auditLogService = new AuditLogService();
 
-const testAuditLogId = '5c2c7849-f30f-40e1-b61b-8aea393fd9fe';
-const testUserId = '158f4249-a763-4a4a-866c-8e0dd5b3bdaf';
-const testUserEmail = 'test@justice.gov.uk';
-
-const testAuditLogContent = {
-    id: testAuditLogId,
-    timestamp: '2023-01-26T09:33:34.560132',
-    userId: testUserId,
-    userEmail: testUserEmail,
-    roles: 'INTERNAL_SUPER_ADMIN_CTSC',
-    userProvenance: 'PI_AAD',
-    action: 'USER_MANAGEMENT',
-    details: 'details test text here',
+const testReqBody = {
+    email: 'test',
+    userId: '',
+    actions: ['USER_MANAGEMENT_VIEW'],
+    filterDate: '2024-10-21',
 };
 
 const testApiResponseData = {
-    content: [testAuditLogContent],
+    content: [
+        {
+            id: '158f4249-a763-4a4a-866c-8e0dd5b3bdaf',
+            userEmail: 'test1@email.com',
+            action: 'USER_MANAGEMENT_VIEW',
+            timestamp: '2022-01-31T14:23:35.056402',
+        },
+        {
+            id: '309b7fbe-65be-45d3-8aee-5e3b59f2da6g',
+            userEmail: 'test2@email.com',
+            action: 'REFERENCE_DATA_UPLOAD',
+            timestamp: '2022-10-13T12:47:05.866678',
+        },
+        {
+            id: '240cd401-5829-4383-95e9-912feb0a3cf0',
+            userEmail: 'test3@email.com',
+            action: 'VIEW_THIRD_PARTY_USERS',
+            timestamp: '2022-10-13T12:47:05.866678',
+        },
+        {
+            id: '66a37f65-9813-49d9-be88-301f0b7b6840',
+            userEmail: 'test4@email.com',
+            action: 'USER_MANAGEMENT_VIEW',
+            timestamp: '2022-02-15T09:47:42.784288',
+        },
+        {
+            id: '4dd14e00-0531-43e2-a443-7f25a85fcdd6',
+            userEmail: 'test5@email.com',
+            action: 'USER_MANAGEMENT_VIEW',
+            timestamp: '2022-09-03T09:27:25.289474',
+        },
+    ],
     pageable: {
         sort: { empty: true, unsorted: true, sorted: false },
         offset: 5,
@@ -42,9 +66,8 @@ const testApiResponseData = {
 };
 
 sinon.stub(AccountManagementRequests.prototype, 'getAllAuditLogs').resolves(testApiResponseData);
-sinon.stub(AccountManagementRequests.prototype, 'getAuditLogById').resolves(testAuditLogContent);
 
-describe('Audit log service', () => {
+describe('Audit Log Service management service', () => {
     it('should return the correct table headers', () => {
         const response = auditLogService.getTableHeaders();
 
@@ -54,11 +77,76 @@ describe('Audit log service', () => {
         expect(response[1].classes).to.equal('govuk-!-padding-top-0');
         expect(response[2].text).to.equal('Action');
         expect(response[2].classes).to.equal('govuk-!-padding-top-0');
-        expect(response[3].text).to.be.empty;
+        expect(response[3].text).to.equal('');
+        expect(response[3].classes).to.equal('govuk-!-padding-top-0');
+    });
+
+    it('should generate the filter key values', () => {
+        // Have to parse JSON in this way to fully replicate req.body
+        const response = auditLogService.generateFilterKeyValues(JSON.parse(JSON.stringify(testReqBody)));
+
+        expect(response).to.contain('email=test');
+        expect(response).to.contain('&actions=USER_MANAGEMENT_VIEW');
+        expect(response).to.contain('&filterDate=2024-10-21');
+    });
+
+    it('should handle filter clearing of audit action', () => {
+        const testClear = {
+            actions: 'USER_MANAGEMENT_VIEW,REFERENCE_DATA_UPLOAD',
+            clear: 'actions=View User Management',
+        };
+
+        const testClearResponse = {
+            actions: ['REFERENCE_DATA_UPLOAD'],
+        };
+
+        const response = auditLogService.handleFilterClearing(testClear);
+        expect(response).to.eql(testClearResponse);
+    });
+
+    it('should handle filter clearing of email', () => {
+        const testClear = {
+            email: 'test@email.com',
+            clear: 'email=test@email.com',
+        };
+        const testClearResponse = {};
+
+        const response = auditLogService.handleFilterClearing(testClear);
+        expect(response).to.eql(testClearResponse);
+    });
+
+    it('should handle filter clearing of user id', () => {
+        const testClear = {
+            userId: '1234',
+            clear: 'userId=1234',
+        };
+        const testClearResponse = {};
+
+        const response = auditLogService.handleFilterClearing(testClear);
+        expect(response).to.eql(testClearResponse);
+    });
+
+    it('should handle filter clearing of filter date', () => {
+        const testClear = {
+            clear: 'filterDate=2024-10-21',
+        };
+        const testClearResponse = {};
+
+        const response = auditLogService.handleFilterClearing(testClear);
+        expect(response).to.eql(testClearResponse);
+    });
+
+    it('should handle filter clearing all', () => {
+        const response = auditLogService.handleFilterClearing({
+            clear: 'all',
+        });
+        expect(response).to.be.empty;
     });
 
     it('should return formatted data from the getFormattedAuditData function', async () => {
-        const response = await auditLogService.getFormattedAuditData(1, '1234', '');
+        const auditSearchCriteria = new AuditLogSearchCriteria(1, 'test', '', '', '');
+
+        const response = await auditLogService.getFormattedAuditData(auditSearchCriteria, '?page=2', '1234');
 
         // Pagination data
         expect(response['paginationData']['previous'].labelText).to.equal('1 of 10');
@@ -66,29 +154,46 @@ describe('Audit log service', () => {
         expect(response['paginationData']['next'].labelText).to.equal('3 of 10');
         expect(response['paginationData']['next'].href).to.equal('?page=3');
 
-        // Audit log formatted table data
-        const auditLogData = response['auditLogData'][0];
-        expect(auditLogData.id).to.equal('5c2c7849-f30f-40e1-b61b-8aea393fd9fe');
-        expect(auditLogData.email).to.equal(testUserEmail);
-        expect(auditLogData.action).to.equal('USER_MANAGEMENT');
-        expect(auditLogData.timestamp).to.equal('26/01/2023 09:33:34');
+        // Audit data
+        expect(response['auditLogData'][0]['id']).to.equal('158f4249-a763-4a4a-866c-8e0dd5b3bdaf');
+        expect(response['auditLogData'][0]['email']).to.equal('test1@email.com');
+        expect(response['auditLogData'][0]['action'].name).to.equal('View User Management');
+        expect(response['auditLogData'][0]['timestamp']).to.equal('31/01/2022 14:23:35');
+
+        // Check field data exists
+        expect(response['emailFieldData']).to.exist;
+        expect(response['userIdFieldData']).to.exist;
+        expect(response['actionsFieldData']).to.exist;
+        expect(response['filterDateFieldData']).to.exist;
+        expect(response['categories']).to.exist;
     });
 
-    it('should build audit log details summary list', async () => {
-        const response = await auditLogService.buildAuditLogDetailsSummaryList(testAuditLogId);
-        const rows = response['rows'];
+    it('should validate filter date', () => {
+        const testDate = {
+            'filterDate-day': '01',
+            'filterDate-month': '01',
+            'filterDate-year': '2021',
+        };
 
-        expect(rows[0].key.text).to.equal('User ID');
-        expect(rows[0].value.text).to.equal(testUserId);
-        expect(rows[1].key.text).to.equal('Email');
-        expect(rows[1].value.text).to.equal(testUserEmail);
-        expect(rows[2].key.text).to.equal('Role');
-        expect(rows[2].value.text).to.equal('CTSC Super Admin');
-        expect(rows[3].key.text).to.equal('Provenance');
-        expect(rows[3].value.text).to.equal('B2C');
-        expect(rows[4].key.text).to.equal('Action');
-        expect(rows[4].value.text).to.equal('USER_MANAGEMENT');
-        expect(rows[5].key.text).to.equal('Details');
-        expect(rows[5].value.text).to.equal('details test text here');
+        const response = auditLogService.validateDate(testDate, 'filterDate');
+        expect(response).to.eql('2021-01-01');
+    });
+
+    it('should return null if date is invalid', () => {
+        const testDate = {
+            'filterDate-day': '01',
+            'filterDate-month': '',
+            'filterDate-year': '2021',
+        };
+
+        const response = auditLogService.validateDate(testDate, 'filterDate');
+        expect(response).is.null;
+    });
+
+    it('should return empty if nothing is provided', () => {
+        const testDate = { };
+
+        const response = auditLogService.validateDate(testDate, 'filterDate');
+        expect(response).is.empty;
     });
 });
