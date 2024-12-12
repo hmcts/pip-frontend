59573dc52a0b4b568d337828faf8d98785926914
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2024-12-12 12:23:40 +0000

    fix upload publication link on confirmation page

--- src/test/unit/controllers/admin/ManualUploadConfirmationController.test.ts
+++ src/test/unit/controllers/admin/ManualUploadConfirmationController.test.ts
@@ -2,6 +2,7 @@ import { Response } from 'express';
 import { mockRequest } from '../../mocks/mockRequest';
 import sinon from 'sinon';
 import ManualUploadConfirmationController from '../../../../main/controllers/admin/ManualUploadConfirmationController';
+import {cloneDeep} from "lodash";
 
 const manualUploadConfirmationController = new ManualUploadConfirmationController();
 
@@ -17,11 +18,13 @@ describe('Manual Upload Confirmation Controller', () => {
         } as unknown as Response;
         const request = mockRequest(i18n);
         const responseMock = sinon.mock(response);
+        const nonStrategicUpload = false;
+        const options = {
+            ...cloneDeep(request.i18n.getDataByLanguage(request.lng)['manual-upload-confirmation']),
+            nonStrategicUpload,
+        };
 
-        responseMock
-            .expects('render')
-            .once()
-            .withArgs('admin/manual-upload-confirmation', { ...i18n['manual-upload-confirmation'] });
+        responseMock.expects('render').once().withArgs('admin/manual-upload-confirmation', options);
 
         await manualUploadConfirmationController.get(request, response);
         responseMock.verify();
