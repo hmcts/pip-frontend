d51bbfacd0b96ebe3521250832c5678dd2ce943a
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2024-12-13 15:53:44 +0000

    fix code review comments

--- src/main/controllers/admin/ManualUploadConfirmationController.ts
+++ src/main/controllers/admin/ManualUploadConfirmationController.ts
@@ -4,10 +4,7 @@ import { cloneDeep } from 'lodash';
 
 export default class ManualUploadConfirmationController {
     public get(req: PipRequest, res: Response): void {
-        let nonStrategicUpload = false;
-        if (req.query?.['non-strategic'] === 'true') {
-            nonStrategicUpload = true;
-        }
+        const nonStrategicUpload = req.query?.['non-strategic'] === 'true'
 
         res.render('admin/manual-upload-confirmation', {
             ...cloneDeep(req.i18n.getDataByLanguage(req.lng)['manual-upload-confirmation']),
