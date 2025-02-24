60c6f367559e4ce803e0774ae601fa9d3b47e0e6
Kian Kwa <kian.kwa@justice.gov.uk>
2025-02-24 15:45:38 +0000

    A no list message override for Mental Health Tribunal

--- src/main/controllers/SummaryOfPublicationsController.ts
+++ src/main/controllers/SummaryOfPublicationsController.ts
@@ -11,8 +11,17 @@ export default class SummaryOfPublicationsController {
     public async get(req: PipRequest, res: Response): Promise<void> {
         const locationId = req.query['locationId'] as string;
         if (locationId) {
-            const court = await courtService.getLocationById(parseInt(locationId.toString()));
+            const court = await courtService.getLocationById(parseInt(locationId));
             const locationName = courtService.findCourtName(court, req.lng, 'summary-of-publications');
+            const additionalLocationInfo = courtService.getAdditionalLocationInfo(locationId);
+
+            let noListMessageOverride = '';
+
+            if (additionalLocationInfo) {
+                noListMessageOverride = req.lng === 'cy' ? additionalLocationInfo.welshNoListMessage
+                    : additionalLocationInfo.noListMessage;
+            }
+
             const publications = await summaryOfPublicationsService.getPublications(
                 parseInt(locationId.toString()),
                 req.user?.['userId']
@@ -23,6 +32,7 @@ export default class SummaryOfPublicationsController {
                 publications,
                 locationName,
                 court,
+                noListMessageOverride,
             });
         } else {
             res.render('error', req.i18n.getDataByLanguage(req.lng).error);
