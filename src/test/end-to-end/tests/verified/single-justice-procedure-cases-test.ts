29d03084dfe98ccdcef4a81bde5c4cecca5512a3
Kian Kwa <kian.kwa@justice.gov.uk>
2025-02-25 11:29:41 +0000

    Simplify SJP test by removing publication upload

--- src/test/end-to-end/tests/verified/single-justice-procedure-cases-test.ts
+++ src/test/end-to-end/tests/verified/single-justice-procedure-cases-test.ts
@@ -1,13 +1,6 @@
-import { DateTime } from 'luxon';
-import { uploadPublication } from '../../shared/testingSupportApi';
-
 Feature('Verified user single Justice Procedure cases');
 
 Scenario('I as a verified user should be able to view all the single procedure cases', async ({ I }) => {
-    const displayFrom = DateTime.now().toISO({ includeOffset: false });
-    const displayTo = DateTime.now().plus({ days: 1 }).toISO({ includeOffset: false });
-    const artefactId = await uploadPublication('PUBLIC', '9', displayFrom, displayFrom, displayTo, 'ENGLISH');
-
     I.loginAsMediaUser();
     I.see('Single Justice Procedure cases');
     I.see(
@@ -15,11 +8,6 @@ Scenario('I as a verified user should be able to view all the single procedure c
     );
     I.click('#card-summary-of-publications\\?locationId\\=9');
     I.waitForText('What do you want to view from Single Justice Procedure?');
-    I.see('Civil and Family Daily Cause List');
-    I.click(locate('//a').withText('Civil and Family Daily Cause List'));
-    I.waitForText('Civil and Family Daily Cause List for Single Justice Procedure');
-    I.see('12345678');
-    I.see('A1 Vs B1');
 
     I.click('Dashboard');
     I.click('#card-search');
@@ -27,12 +15,6 @@ Scenario('I as a verified user should be able to view all the single procedure c
     I.fillField('#search-input', 'Single Justice Procedure');
     I.click('Continue');
     I.waitForText('What do you want to view from Single Justice Procedure?');
-    I.see('Civil and Family Daily Cause List');
-    I.click(locate('//a').withText('Civil and Family Daily Cause List'));
-    I.waitForText('Civil and Family Daily Cause List for Single Justice Procedure');
-    I.see('12345678');
-    I.see('A1 Vs B1');
-    I.logout();
 
-    I.deletePublicationByArtefactId(artefactId);
+    I.logout();
 });
