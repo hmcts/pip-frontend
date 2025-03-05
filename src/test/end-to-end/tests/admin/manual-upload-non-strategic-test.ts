c43b0076292762f94a93916759a9e47b51864f46
Kian Kwa <kian.kwa@justice.gov.uk>
2025-03-05 14:38:04 +0000

    Remove list type check in e2e test

--- src/test/end-to-end/tests/admin/manual-upload-non-strategic-test.ts
+++ src/test/end-to-end/tests/admin/manual-upload-non-strategic-test.ts
@@ -35,7 +35,6 @@ Scenario('I as a admin user should be able to upload non strategic file successf
     I.click('Continue');
     I.waitForText('Check upload details');
     I.see(locationName);
-    I.see(listType);
     I.see(nonStrategicFileName);
     I.click('Confirm');
     I.waitForText('Success');
