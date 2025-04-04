0e616330e8829e8d6e4a003fe6123eb777c43ce1
ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
2025-04-04 12:37:58 +0100

    PUB-2781 - Verified login subscription test

--- src/test/end-to-end/tests/performance/verified-login-subscription-test.ts
+++ src/test/end-to-end/tests/performance/verified-login-subscription-test.ts
@@ -3,10 +3,14 @@ Feature('Login - Performance');
 //This value can be changed to determine how many tests should run.
 //The number of concurrent logins can be changed via the 'run-workers' setting, in the package.json
 for (let i = 0; i < 10; i++) {
-    Scenario('I as a verified user should be able to sign-in with the valid credentials and see the subscription screen ' + i, async ({ I }) => {
-        I.loginAsMediaUser();
-        I.click('#card-subscription-management');
-        I.waitForText('Your email subscriptions');
-        I.logout();
-    }).tag('@Performance');
+    Scenario(
+        'I as a verified user should be able to sign-in with the valid credentials and see the subscription screen ' +
+            i,
+        async ({ I }) => {
+            I.loginAsMediaUser();
+            I.click('#card-subscription-management');
+            I.waitForText('Your email subscriptions');
+            I.logout();
+        }
+    ).tag('@Performance');
 }
