a0b195eec4d26cf1142099ca66fabe5c5c4e2d18
kian.kwa <kian.kwa@justice.gov.uk>
2025-04-25 18:58:59 +0100

    Add new view tests

---
+++ src/test/unit/views/style-guide/kings-bench-masters-daily-cause-list.test.ts
@@ -0,0 +1,152 @@
+import sinon from 'sinon';
+import request from 'supertest';
+import { app } from '../../../../main/app';
+import { expect } from 'chai';
+import fs from 'fs';
+import path from 'path';
+import { PublicationService } from '../../../../main/service/PublicationService';
+
+const headingClass = 'govuk-heading-l';
+const bodyText = 'govuk-body';
+const govukLinkClass = 'govuk-link';
+const cell = 'govuk-table__cell';
+const tableHeader = 'govuk-table__header';
+
+const rawData = fs.readFileSync(path.resolve(__dirname, '../../mocks/kingsBenchMastersDailyCauseList.json'), 'utf-8');
+const jsonData = JSON.parse(rawData);
+const rawMetaData = fs.readFileSync(path.resolve(__dirname, '../../mocks/returnedArtefacts.json'), 'utf-8');
+const metaData = JSON.parse(rawMetaData)[0];
+metaData.listType = 'KINGS_BENCH_MASTERS_DAILY_CAUSE_LIST';
+
+sinon.stub(PublicationService.prototype, 'getIndividualPublicationJson').returns(jsonData);
+sinon.stub(PublicationService.prototype, 'getIndividualPublicationMetadata').returns(metaData);
+
+let htmlRes: Document;
+const PAGE_URL = '/kings-bench-masters-daily-cause-list?artefactId=abc';
+
+describe('King’s Bench Masters Daily Cause List page', () => {
+    beforeAll(async () => {
+        await request(app)
+            .get(PAGE_URL)
+            .then(res => {
+                htmlRes = new DOMParser().parseFromString(res.text, 'text/html');
+            });
+    });
+
+    it('should display header', () => {
+        const header = htmlRes.getElementsByClassName(headingClass);
+        expect(header[0].innerHTML).contains('King’s Bench Masters Daily Cause List');
+    });
+
+    it('should display venue', () => {
+        const text = htmlRes.getElementsByClassName(bodyText);
+        expect(text[4].innerHTML).equals('Royal Courts of Justice');
+        expect(text[5].innerHTML).equals('Strand, London');
+        expect(text[6].innerHTML).equals('WC2A 2LL');
+    });
+
+    it('should display list date', () => {
+        const text = htmlRes.getElementsByClassName(bodyText);
+        expect(text[7].innerHTML).equals('List for 14 February 2022');
+    });
+
+    it('should display list updated date text', () => {
+        const text = htmlRes.getElementsByClassName(bodyText);
+        expect(text[8].innerHTML).equals('Last updated 20 January 2025 at 9:30am');
+    });
+
+    it('should display important information heading', () => {
+        const text = htmlRes.getElementsByClassName('govuk-details__summary-text');
+        expect(text[0].innerHTML).equals('Important information');
+    });
+
+    it('should display important information message', () => {
+        const text = htmlRes.getElementsByClassName(bodyText);
+        expect(text[10].innerHTML).contains('Any media representative (or any other member of the public) wishing to attend a remote hearing should provide an email address and contact number to be sent an appropriate link for access.');
+    });
+
+    it('should display King\'s Bench Guide link', () => {
+        const link = htmlRes.getElementsByClassName(govukLinkClass);
+        expect(link[5].getAttribute('href')).eq('https://www.judiciary.uk/guidance-and-resources/kings-bench-guide-2024/');
+    });
+
+    it('should display current trail windows link', () => {
+        const link = htmlRes.getElementsByClassName(govukLinkClass);
+        expect(link[6].getAttribute('href')).eq('https://www.gov.uk/guidance/queens-bench-hearing-and-trial-dates');
+    });
+
+    it('should display Venue header', () => {
+        const headerCell = htmlRes.getElementsByClassName(tableHeader);
+        expect(headerCell[0].innerHTML).equals('Venue');
+    });
+
+    it('should display Judge header', () => {
+        const headerCell = htmlRes.getElementsByClassName(tableHeader);
+        expect(headerCell[1].innerHTML).equals('Judge');
+    });
+
+    it('should display Time header', () => {
+        const headerCell = htmlRes.getElementsByClassName(tableHeader);
+        expect(headerCell[2].innerHTML).equals('Time');
+    });
+
+    it('should display Case Number header', () => {
+        const headerCell = htmlRes.getElementsByClassName(tableHeader);
+        expect(headerCell[3].innerHTML).equals('Case number');
+    });
+
+    it('should display Case Details header', () => {
+        const headerCell = htmlRes.getElementsByClassName(tableHeader);
+        expect(headerCell[4].innerHTML).equals('Case details');
+    });
+
+    it('should display Hearing Type header', () => {
+        const headerCell = htmlRes.getElementsByClassName(tableHeader);
+        expect(headerCell[5].innerHTML).equals('Hearing type');
+    });
+
+    it('should display Additional Information header', () => {
+        const headerCell = htmlRes.getElementsByClassName(tableHeader);
+        expect(headerCell[6].innerHTML).equals('Additional information');
+    });
+
+    it('should display Venue cell data', () => {
+        const cellText = htmlRes.getElementsByClassName(cell);
+        expect(cellText[0].innerHTML).equals('Venue A');
+    });
+
+    it('should display Judge cell data', () => {
+        const cellText = htmlRes.getElementsByClassName(cell);
+        expect(cellText[1].innerHTML).equals('Judge A');
+    });
+
+    it('should display Time cell data', () => {
+        const cellText = htmlRes.getElementsByClassName(cell);
+        expect(cellText[2].innerHTML).equals('9am');
+    });
+
+    it('should display Case Number cell data', () => {
+        const cellText = htmlRes.getElementsByClassName(cell);
+        expect(cellText[3].innerHTML).equals('12345');
+    });
+
+    it('should display Case Details cell data', () => {
+        const cellText = htmlRes.getElementsByClassName(cell);
+        expect(cellText[4].innerHTML).equals('Case details A');
+    });
+
+    it('should display Hearing Type cell data', () => {
+        const cellText = htmlRes.getElementsByClassName(cell);
+        expect(cellText[5].innerHTML).equals('Hearing type A');
+    });
+
+    it('should display Additional Information cell data', () => {
+        const cellText = htmlRes.getElementsByClassName(cell);
+        expect(cellText[6].innerHTML).equals('This is additional information');
+    });
+
+    it('should display data source text', () => {
+        const text = htmlRes.getElementsByClassName(bodyText);
+        expect(text[21].innerHTML).contains('Data Source: Prov1');
+    });
+});
