4ee727134d09609eb1f650324fd1d5701ebfd140
kian.kwa <kian.kwa@justice.gov.uk>
2025-04-25 19:04:37 +0100

    Lint fixes on route tests

--- src/test/routes/style-guide/family-division-high-court-daily-cause-list.test.ts
+++ src/test/routes/style-guide/family-division-high-court-daily-cause-list.test.ts
@@ -7,7 +7,10 @@ import { PublicationService } from '../../../main/service/PublicationService';
 import fs from 'fs';
 import path from 'path';
 
-const rawData = fs.readFileSync(path.resolve(__dirname, '../../unit/mocks/familyDivisionHighCourtDailyCauseList.json'), 'utf-8');
+const rawData = fs.readFileSync(
+    path.resolve(__dirname, '../../unit/mocks/familyDivisionHighCourtDailyCauseList.json'),
+    'utf-8'
+);
 const rawJson = JSON.parse(rawData);
 const rawMetaData = fs.readFileSync(path.resolve(__dirname, '../../unit/mocks/returnedArtefacts.json'), 'utf-8');
 const metaData = JSON.parse(rawMetaData)[0];
