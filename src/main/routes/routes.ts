465bdea7ae21f5eb46f9b533f6415bd0677a03d3
Natasha Alker <NatashaAlker@yahoo.co.uk>
2025-04-26 13:13:58 +0100

    Add interim applications list to routes and list lookup

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -345,6 +345,9 @@ export default function (app: Application): void {
     app.get('/sscs-london-daily-hearing-list', (req, res) =>
         app.locals.container.cradle.nonStrategicTribunalListsController.get(req, res, 'sscs-london-daily-hearing-list')
     );
+    app.get('/interim-applications-chd-daily-cause-list', (req, res) =>
+        app.locals.container.cradle.nonStrategicTribunalListsController.get(req, res, 'interim-applications-chd-daily-cause-list')
+    );
 
     // Restricted paths
     app.get('/account-home', isPermittedMedia, app.locals.container.cradle.accountHomeController.get);
