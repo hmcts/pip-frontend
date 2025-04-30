bc9b2f260bd06331b868ba54cb5a0dfa08dc816c
kian.kwa <kian.kwa@justice.gov.uk>
2025-04-30 19:14:38 +0100

    Add a11y tests

---
+++ src/test/a11y/tests/style-guide/intellectual-property-and-enterprise-court-daily-cause-list.test.ts
@@ -0,0 +1,20 @@
+import sinon from 'sinon';
+import { PublicationService } from '../../../../main/service/PublicationService';
+import { LocationRequests } from '../../../../main/resources/requests/LocationRequests';
+import { testArtefactJsonData, testArtefactMetadata, testLocationData } from '../../common/testData';
+import { testAccessibility } from '../../common/pa11yHelper';
+
+const url = '/intellectual-property-and-enterprise-court-daily-cause-list?artefactId=abc';
+
+const jsonData = testArtefactJsonData('intellectualPropertyAndEnterpriseCourtDailyCauseList.json');
+const metadata = testArtefactMetadata()[0];
+const locationData = testLocationData();
+metadata.listType = 'INTELLECTUAL_PROPERTY_AND_ENTERPRISE_COURT_CHD_DAILY_CAUSE_LIST';
+
+sinon.stub(PublicationService.prototype, 'getIndividualPublicationJson').resolves(jsonData);
+sinon.stub(PublicationService.prototype, 'getIndividualPublicationMetadata').resolves(metadata);
+sinon.stub(LocationRequests.prototype, 'getLocation').resolves(locationData);
+
+describe('Accessibility - Intellectual Property and Enterprise Court (ChD) Daily Cause List Page', () => {
+    testAccessibility(url);
+});
---
+++ src/test/a11y/tests/style-guide/revenue-list-chd-daily-cause-list.test.ts
@@ -0,0 +1,20 @@
+import sinon from 'sinon';
+import { PublicationService } from '../../../../main/service/PublicationService';
+import { LocationRequests } from '../../../../main/resources/requests/LocationRequests';
+import { testArtefactJsonData, testArtefactMetadata, testLocationData } from '../../common/testData';
+import { testAccessibility } from '../../common/pa11yHelper';
+
+const url = '/revenue-list-chd-civil-daily-cause-list?artefactId=abc';
+
+const jsonData = testArtefactJsonData('revenueListChdDailyCauseList.json');
+const metadata = testArtefactMetadata()[0];
+const locationData = testLocationData();
+metadata.listType = 'REVENUE_LIST_CHD_DAILY_CAUSE_LIST';
+
+sinon.stub(PublicationService.prototype, 'getIndividualPublicationJson').resolves(jsonData);
+sinon.stub(PublicationService.prototype, 'getIndividualPublicationMetadata').resolves(metadata);
+sinon.stub(LocationRequests.prototype, 'getLocation').resolves(locationData);
+
+describe('Accessibility - Revenue List (ChD) Daily Cause List Page', () => {
+    testAccessibility(url);
+});
