ac6f774eecdb33a9b8ee60be79977be466103643
kian.kwa <kian.kwa@justice.gov.uk>
2025-05-01 09:18:56 +0100

    Add routes test

---
+++ src/test/routes/style-guide/intellectual-property-and-enterprise-court-daily-cause-list.test.ts
@@ -0,0 +1,30 @@
+import { expect } from 'chai';
+import request from 'supertest';
+import sinon from 'sinon';
+
+import { app } from '../../../main/app';
+import { PublicationService } from '../../../main/service/PublicationService';
+import fs from 'fs';
+import path from 'path';
+
+const rawData = fs.readFileSync(
+    path.resolve(__dirname, '../../unit/mocks/intellectualPropertyAndEnterpriseCourtDailyCauseList.json'),
+    'utf-8'
+);
+const rawJson = JSON.parse(rawData);
+const rawMetaData = fs.readFileSync(path.resolve(__dirname, '../../unit/mocks/returnedArtefacts.json'), 'utf-8');
+const metaData = JSON.parse(rawMetaData)[0];
+metaData.listType = 'INTELLECTUAL_PROPERTY_AND_ENTERPRISE_COURT_CHD_DAILY_CAUSE_LIST';
+
+sinon.stub(PublicationService.prototype, 'getIndividualPublicationJson').resolves(rawJson);
+sinon.stub(PublicationService.prototype, 'getIndividualPublicationMetadata').resolves(metaData);
+
+describe('Intellectual Property and Enterprise Court (ChD) Daily Cause List Page', () => {
+    describe('on GET', () => {
+        test('should return Intellectual Property and Enterprise Court (ChD) Daily Cause List page', async () => {
+            await request(app)
+                .get('/intellectual-property-and-enterprise-court-daily-cause-list?artefactId=abc')
+                .expect(res => expect(res.status).to.equal(200));
+        });
+    });
+});
