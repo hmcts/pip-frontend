be3677d12e997c8dcfa95d7a596896f76f28c2e9
junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>
2025-06-03 11:01:59 +0100

    PUB-2880 Add Administrative Courts: Create Schema and List types (Part 1) (#1477)
    * Administrative Courts: Create Schema and List types (Part 1)
    
    * Fix style
    
    * fix styles
    
    * fix style
    
    * fix merge conflict
    
    * fix code review comments
    
    * fix style
    
    * fix style
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/test/unit/service/ManualUploadService.test.ts
+++ src/test/unit/service/ManualUploadService.test.ts
@@ -1,9 +1,9 @@
 import sinon from 'sinon';
-import {expect} from 'chai';
-import {ManualUploadService} from '../../../main/service/ManualUploadService';
-import {LocationService} from '../../../main/service/LocationService';
-import {DataManagementRequests} from '../../../main/resources/requests/DataManagementRequests';
-import {PublicationService} from '../../../main/service/PublicationService';
+import { expect } from 'chai';
+import { ManualUploadService } from '../../../main/service/ManualUploadService';
+import { LocationService } from '../../../main/service/LocationService';
+import { DataManagementRequests } from '../../../main/resources/requests/DataManagementRequests';
+import { PublicationService } from '../../../main/service/PublicationService';
 import fs from 'fs';
 import path from 'path';
 
@@ -169,7 +169,7 @@ describe('Manual upload service', () => {
 
         it('should build form data list subtypes when non-strategic', async () => {
             const data = await manualUploadService.buildFormData(englishLanguage, true, undefined);
-            expect(data['listSubtypes'].length).to.equal(58);
+            expect(data['listSubtypes'].length).to.equal(62);
             expect(data['listSubtypes'][0]).to.deep.equal({
                 text: '<Please choose a list type>',
                 value: 'EMPTY',
