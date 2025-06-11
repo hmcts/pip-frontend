33efc764f2f0523467c1aa761a6db6f925e7de88
Christopher Stacey <christopher.stacey@justice.gov.uk>
2025-06-11 15:24:47 +0100

    PUB-2922 - Styling

--- src/test/unit/views/style-guide/mayor-and-city-civil-daily-cause-list.test.ts
+++ src/test/unit/views/style-guide/mayor-and-city-civil-daily-cause-list.test.ts
@@ -65,13 +65,13 @@ describe('Mayor & City Civil Daily Cause List page in English', () => {
         const text = htmlRes.getElementsByClassName(bodyText);
         expect(text[10].innerHTML).contains(
             'Central London County Court and Mayors &amp; City of London Court – Hearings for the County Court at ' +
-            'Central London will be heard at the Thomas More Building, located in the Royal Courts of Justice. ' +
-            'Cases are also listed at Mayors &amp; City of London Court, Guildhall Buildings, Basinghall Street, ' +
-            'London, EC2V 5AR. Due to judicial availability, cases may be moved between the County Court at ' +
-            'Central London and Mayors &amp; City of London Court. ' +
-            'Please ensure you check the cause lists for Central ' +
-            'London County Court and Mayors and City of London Court the day prior to your hearing. ' +
-            'Lists aim to be published by 1700.'
+                'Central London will be heard at the Thomas More Building, located in the Royal Courts of Justice. ' +
+                'Cases are also listed at Mayors &amp; City of London Court, Guildhall Buildings, Basinghall Street, ' +
+                'London, EC2V 5AR. Due to judicial availability, cases may be moved between the County Court at ' +
+                'Central London and Mayors &amp; City of London Court. ' +
+                'Please ensure you check the cause lists for Central ' +
+                'London County Court and Mayors and City of London Court the day prior to your hearing. ' +
+                'Lists aim to be published by 1700.'
         );
     });
 
@@ -79,11 +79,11 @@ describe('Mayor & City Civil Daily Cause List page in English', () => {
         const text = htmlRes.getElementsByClassName(bodyText);
         expect(text[11].innerHTML).contains(
             'Requests for the media and others, including legal bloggers, should be made to County Court at ' +
-            'Central London via enquiries.centrallondon.countycourt@justice.gov.uk ' +
-            'or Mayors and City of London Court via enquiries.centrallondon.countycourt@justice.gov.uk. ' +
-            'Arrangements will then be made for you to attend. When considering the use of telephone and video ' +
-            'technology the judiciary will have regard to the principles of open justice. The Court may exclude ' +
-            'observers where necessary to secure the proper administration of Justice.'
+                'Central London via enquiries.centrallondon.countycourt@justice.gov.uk ' +
+                'or Mayors and City of London Court via enquiries.centrallondon.countycourt@justice.gov.uk. ' +
+                'Arrangements will then be made for you to attend. When considering the use of telephone and video ' +
+                'technology the judiciary will have regard to the principles of open justice. The Court may exclude ' +
+                'observers where necessary to secure the proper administration of Justice.'
         );
     });
 
@@ -204,13 +204,13 @@ describe('Mayor & City Civil Daily Cause List page in Welsh', () => {
         const text = htmlRes.getElementsByClassName(bodyText);
         expect(text[10].innerHTML).contains(
             'Llys Sirol Canol Llundain a Llys Maer a Dinas Llundain – Bydd gwrandawiadau ar gyfer ' +
-            'Llys Sirol Canol Llundain yn cael eu gwrando yn Adeilad Thomas More, ' +
-            'sydd wedi’i leoli yn y Llysoedd Barn Brenhinol. Rhestrir achosion hefyd yn ' +
-            'Llys Maer a Dinas Llundain, Adeiladau’r Guildhall, Stryd Basinghall, ' +
-            'Llundain, EC2V 5AR. Oherwydd argaeledd barnwrol, gellir symud achosion rhwng ' +
-            'y Llys Sirol yng Nghanol Llundain a Llys Maer a Dinas Llundain. Gwnewch yn ' +
-            'siŵr eich bod yn gwirio rhestrau achosion Llys Sirol Canol Llundain a ' +
-            'Llys Maer a Dinas Llundain y diwrnod cyn eich gwrandawiad. Anelir at gyhoeddi rhestrau erbyn 5pm.'
+                'Llys Sirol Canol Llundain yn cael eu gwrando yn Adeilad Thomas More, ' +
+                'sydd wedi’i leoli yn y Llysoedd Barn Brenhinol. Rhestrir achosion hefyd yn ' +
+                'Llys Maer a Dinas Llundain, Adeiladau’r Guildhall, Stryd Basinghall, ' +
+                'Llundain, EC2V 5AR. Oherwydd argaeledd barnwrol, gellir symud achosion rhwng ' +
+                'y Llys Sirol yng Nghanol Llundain a Llys Maer a Dinas Llundain. Gwnewch yn ' +
+                'siŵr eich bod yn gwirio rhestrau achosion Llys Sirol Canol Llundain a ' +
+                'Llys Maer a Dinas Llundain y diwrnod cyn eich gwrandawiad. Anelir at gyhoeddi rhestrau erbyn 5pm.'
         );
     });
 
@@ -218,11 +218,11 @@ describe('Mayor & City Civil Daily Cause List page in Welsh', () => {
         const text = htmlRes.getElementsByClassName(bodyText);
         expect(text[11].innerHTML).contains(
             'Dylid gwneud ceisiadau ar gyfer y cyfryngau ac eraill, gan gynnwys blogwyr cyfreithiol, ' +
-            'i Lys Sirol Canol Llundain drwy enquiries.centrallondon.countycourt@justice.gov.uk ' +
-            'neu Lys Maer a Dinas Llundain drwy enquiries.centrallondon.countycourt@justice.gov.uk. ' +
-            'Yna gwneir trefniadau i chi fynychu. Wrth ystyried y defnydd o dechnoleg ffôn a fideo, ' +
-            'bydd y farnwriaeth yn ystyried egwyddorion cyfiawnder agored. Gall y llys eithrio arsylwyr ' +
-            'lle bo angen i sicrhau gweinyddiaeth briodol cyfiawnder.'
+                'i Lys Sirol Canol Llundain drwy enquiries.centrallondon.countycourt@justice.gov.uk ' +
+                'neu Lys Maer a Dinas Llundain drwy enquiries.centrallondon.countycourt@justice.gov.uk. ' +
+                'Yna gwneir trefniadau i chi fynychu. Wrth ystyried y defnydd o dechnoleg ffôn a fideo, ' +
+                'bydd y farnwriaeth yn ystyried egwyddorion cyfiawnder agored. Gall y llys eithrio arsylwyr ' +
+                'lle bo angen i sicrhau gweinyddiaeth briodol cyfiawnder.'
         );
     });
 
