99fd5461f21114021d5b1450838fcaf370f215f1
kian.kwa <kian.kwa@justice.gov.uk>
2025-08-19 17:07:26 +0100

    Added new view test for blob view explorer flat file

--- src/test/unit/views/system-admin/blob-view-publication.test.ts
+++ src/test/unit/views/system-admin/blob-view-publication.test.ts
@@ -15,12 +15,16 @@ sinon.stub(LocationService.prototype, 'getLocationById').returns({ name: 'Test c
 
 const rawMetadata = fs.readFileSync(path.resolve(__dirname, '../../mocks/returnedArtefacts.json'), 'utf-8');
 const metadata = JSON.parse(rawMetadata)[1];
+const flatFileMetadata = JSON.parse(rawMetadata)[1];
+flatFileMetadata.isFlatFile = true;
+
 const metadataStub = sinon.stub(PublicationService.prototype, 'getIndividualPublicationMetadata');
 metadataStub.withArgs('abc').resolves(metadata);
 
 const noMatchMetadata = JSON.parse(rawMetadata)[1];
 noMatchMetadata.locationId = 'NoMatch123';
 metadataStub.withArgs('def').resolves(noMatchMetadata);
+metadataStub.withArgs('ghi').resolves(flatFileMetadata)
 
 const headingClass = 'govuk-heading-l';
 const subHeadingClass = 'govuk-heading-m';
@@ -36,7 +40,7 @@ const resubmitSubscriptionId = 'resubmit-subscription';
 let htmlRes: Document;
 
 describe('Blob explorer publication page', () => {
-    describe('for existed location', () => {
+    describe('for existed location - with JSON publication', () => {
         beforeAll(async () => {
             app.request['user'] = { userId: '1', roles: 'SYSTEM_ADMIN' };
 
@@ -47,9 +51,14 @@ describe('Blob explorer publication page', () => {
                 });
         });
 
-        it('should display the heading', () => {
+        it('should display the heading - prefix', () => {
             const header = htmlRes.getElementsByClassName(headingClass);
             expect(header[0].innerHTML).contains('Blob Explorer', 'Could not find the header');
+        });
+
+        it('should display the heading - file type', () => {
+            const header = htmlRes.getElementsByClassName(headingClass);
+            expect(header[0].innerHTML).contains('JSON file', 'Could not find the header');
         });
 
         it('should display the sub heading', () => {
@@ -204,6 +213,172 @@ describe('Blob explorer publication page', () => {
                 'Could not find correct json output on the line'
             );
             expect(jsonContainer.innerHTML).contains('documentName', 'Could not find correct json output on the line');
+        });
+
+        it('should display the re-submit subscription button', () => {
+            const button = htmlRes.getElementById(resubmitSubscriptionId);
+            expect(button).to.exist;
+            expect(button.innerHTML).contains('Re-submit subscription', 'Button text does not match');
+        });
+    });
+
+    describe('for existed location - with flat file publication', () => {
+        beforeAll(async () => {
+            app.request['user'] = { userId: '1', roles: 'SYSTEM_ADMIN' };
+
+            await request(app)
+                .get('/blob-view-publication?artefactId=ghi')
+                .then(res => {
+                    htmlRes = new DOMParser().parseFromString(res.text, 'text/html');
+                });
+        });
+
+        it('should display the heading - prefix', () => {
+            const header = htmlRes.getElementsByClassName(headingClass);
+            expect(header[0].innerHTML).contains('Blob Explorer', 'Could not find the header');
+        });
+
+        it('should display the heading - file type', () => {
+            const header = htmlRes.getElementsByClassName(headingClass);
+            expect(header[0].innerHTML).contains('Flat file', 'Could not find the header');
+        });
+
+        it('should display the sub heading', () => {
+            const subHeading = htmlRes.getElementsByClassName(subHeadingClass);
+            expect(subHeading[1].innerHTML).contains('Metadata', 'Could not find the sub heading');
+        });
+
+        it('should have a summary list of the meta data on the page', () => {
+            const summaryList = htmlRes.getElementsByClassName(summaryListClass)[0];
+            expect(summaryList).to.exist;
+        });
+
+        it('should have the artefact id row in the summary list', () => {
+            const summaryListRow = htmlRes.getElementsByClassName(summaryListRowClass)[0];
+            const rowKey = summaryListRow.getElementsByClassName(summaryListRowKeyClass)[0];
+            const rowValue = summaryListRow.getElementsByClassName(summaryListRowValueClass)[0];
+
+            expect(summaryListRow).to.exist;
+            expect(rowKey.innerHTML).contains('Artefact ID', 'Could not find row key');
+            expect(rowValue.innerHTML).contains('745', 'Could not find row value');
+        });
+
+        it('should have the location id row in the summary list', () => {
+            const summaryListRow = htmlRes.getElementsByClassName(summaryListRowClass)[1];
+            const rowKey = summaryListRow.getElementsByClassName(summaryListRowKeyClass)[0];
+            const rowValue = summaryListRow.getElementsByClassName(summaryListRowValueClass)[0];
+
+            expect(summaryListRow).to.exist;
+            expect(rowKey.innerHTML).contains('Location ID', 'Could not find row key');
+            expect(rowValue.innerHTML).contains('123', 'Could not find row value');
+        });
+
+        it('should have the location name row in the summary list', () => {
+            const summaryListRow = htmlRes.getElementsByClassName(summaryListRowClass)[2];
+            const rowKey = summaryListRow.getElementsByClassName(summaryListRowKeyClass)[0];
+            const rowValue = summaryListRow.getElementsByClassName(summaryListRowValueClass)[0];
+
+            expect(summaryListRow).to.exist;
+            expect(rowKey.innerHTML).contains('Location Name', 'Could not find row key');
+            expect(rowValue.innerHTML).contains('Test court 100', 'Could not find row value');
+        });
+
+        it('should have the publication type row in the summary list', () => {
+            const summaryListRow = htmlRes.getElementsByClassName(summaryListRowClass)[3];
+            const rowKey = summaryListRow.getElementsByClassName(summaryListRowKeyClass)[0];
+            const rowValue = summaryListRow.getElementsByClassName(summaryListRowValueClass)[0];
+
+            expect(summaryListRow).to.exist;
+            expect(rowKey.innerHTML).contains('Publication Type', 'Could not find row key');
+            expect(rowValue.innerHTML).contains('General_publication', 'Could not find row value');
+        });
+
+        it('should have the list type row in the summary list', () => {
+            const summaryListRow = htmlRes.getElementsByClassName(summaryListRowClass)[4];
+            const rowKey = summaryListRow.getElementsByClassName(summaryListRowKeyClass)[0];
+            const rowValue = summaryListRow.getElementsByClassName(summaryListRowValueClass)[0];
+
+            expect(summaryListRow).to.exist;
+            expect(rowKey.innerHTML).contains('List Type', 'Could not find row key');
+            expect(rowValue.innerHTML).contains('Single Justice Procedure Public List', 'Could not find row value');
+        });
+
+        it('should have the provenance row in the summary list', () => {
+            const summaryListRow = htmlRes.getElementsByClassName(summaryListRowClass)[5];
+            const rowKey = summaryListRow.getElementsByClassName(summaryListRowKeyClass)[0];
+            const rowValue = summaryListRow.getElementsByClassName(summaryListRowValueClass)[0];
+
+            expect(summaryListRow).to.exist;
+            expect(rowKey.innerHTML).contains('Provenance', 'Could not find row key');
+            expect(rowValue.innerHTML).contains('prov2', 'Could not find row value');
+        });
+
+        it('should have the language row in the summary list', () => {
+            const summaryListRow = htmlRes.getElementsByClassName(summaryListRowClass)[6];
+            const rowKey = summaryListRow.getElementsByClassName(summaryListRowKeyClass)[0];
+            const rowValue = summaryListRow.getElementsByClassName(summaryListRowValueClass)[0];
+
+            expect(summaryListRow).to.exist;
+            expect(rowKey.innerHTML).contains('Language', 'Could not find row key');
+            expect(rowValue.innerHTML).contains('English', 'Could not find row value');
+        });
+
+        it('should have the sensitivity row in the summary list', () => {
+            const summaryListRow = htmlRes.getElementsByClassName(summaryListRowClass)[7];
+            const rowKey = summaryListRow.getElementsByClassName(summaryListRowKeyClass)[0];
+            const rowValue = summaryListRow.getElementsByClassName(summaryListRowValueClass)[0];
+
+            expect(summaryListRow).to.exist;
+            expect(rowKey.innerHTML).contains('Sensitivity', 'Could not find row key');
+            expect(rowValue.innerHTML).contains('Public', 'Could not find row value');
+        });
+
+        it('should have the content date row in the summary list', () => {
+            const summaryListRow = htmlRes.getElementsByClassName(summaryListRowClass)[8];
+            const rowKey = summaryListRow.getElementsByClassName(summaryListRowKeyClass)[0];
+            const rowValue = summaryListRow.getElementsByClassName(summaryListRowValueClass)[0];
+
+            expect(summaryListRow).to.exist;
+            expect(rowKey.innerHTML).contains('Content Date', 'Could not find row key');
+            expect(rowValue.innerHTML).contains('14th February 2022 at 02:14 pm', 'Could not find row value');
+        });
+
+        it('should have the display from row in the summary list', () => {
+            const summaryListRow = htmlRes.getElementsByClassName(summaryListRowClass)[9];
+            const rowKey = summaryListRow.getElementsByClassName(summaryListRowKeyClass)[0];
+            const rowValue = summaryListRow.getElementsByClassName(summaryListRowValueClass)[0];
+
+            expect(summaryListRow).to.exist;
+            expect(rowKey.innerHTML).contains('Display From', 'Could not find row key');
+            expect(rowValue.innerHTML).contains(' 14th February 2022 at 02:14 pm', 'Could not find row value');
+        });
+
+        it('should have the display to row in the summary list', () => {
+            const summaryListRow = htmlRes.getElementsByClassName(summaryListRowClass)[10];
+            const rowKey = summaryListRow.getElementsByClassName(summaryListRowKeyClass)[0];
+            const rowValue = summaryListRow.getElementsByClassName(summaryListRowValueClass)[0];
+
+            expect(summaryListRow).to.exist;
+            expect(rowKey.innerHTML).contains('Display To', 'Could not find row key');
+            expect(rowValue.innerHTML).contains('14th February 2052 at 02:14 pm', 'Could not find row value');
+        });
+
+        it('should display the link to flat file', () => {
+            const link = htmlRes.getElementsByClassName(linkClass)[5];
+            expect(link.innerHTML).contains('Link to file', 'Could not find the link to flat file');
+        });
+
+        it('should have the correct href to the template', () => {
+            const link = htmlRes.getElementsByClassName(linkClass)[5];
+            expect(link.outerHTML).contains(
+                '/file-publication?artefactId=ghi',
+                'URL to the flat file does not match'
+            );
+        });
+
+        it('should not have the details dropdown for viewing the json content', () => {
+            const detailsSummaryText = htmlRes.getElementsByClassName(detailsSummaryTextClass)[0];
+            expect(detailsSummaryText).is.undefined
         });
 
         it('should display the re-submit subscription button', () => {
