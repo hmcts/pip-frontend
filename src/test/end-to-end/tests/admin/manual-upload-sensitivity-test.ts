573701051d367e5028a9eefafe6555fa3ca0ffa8
KianKwa <kian.kwa@justice.gov.uk>
2026-03-25 15:25:36 +0000

    Update expected e2e test result

--- src/test/end-to-end/tests/admin/manual-upload-sensitivity-test.ts
+++ src/test/end-to-end/tests/admin/manual-upload-sensitivity-test.ts
@@ -35,7 +35,7 @@ Scenario('Manual upload sensitivity test', async ({ I }) => {
     I.selectOption(listTypeId, 'Magistrates Standard List');
 
     classification = await I.grabValueFrom(classificationId);
-    Assert.equal(classification, sensitivityPrivate);
+    Assert.equal(classification, sensitivityClassified);
 
     I.selectOption(listTypeId, 'Crown Firm List');
 
