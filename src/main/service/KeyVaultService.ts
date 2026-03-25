ecd9efb576bca327962ea4e6bd1c48d056e96841
junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>
2026-03-25 12:36:36 +0000

    testing

--- src/main/service/KeyVaultService.ts
+++ src/main/service/KeyVaultService.ts
@@ -8,20 +8,12 @@ export class KeyVaultService {
     constructor() {
         const thirdPartyKeyVault = process.env.THIRD_PARTY_KEY_VAULT || 'pip-ss-tp-kv-stg';
         const keyVaultUrl = 'https://' + thirdPartyKeyVault + '.vault.azure.net/';
-        const nodeENV = process.env.NODE_ENV || 'development';
 
-        const credential =
-            nodeENV === 'development'
-                ? new DefaultAzureCredential()
-                : new DefaultAzureCredential({
-                      managedIdentityClientId: '0e0c8682-a038-4aa8-9619-bb88a7ba9357',
-                      tenantId: '531ff96d-0ae9-462a-8d2d-bec7c0b42082',
+        const MANAGED_IDENTITY_CLIENT_ID = '0e0c8682-a038-4aa8-9619-bb88a7ba9357';
+
+        const credential = new DefaultAzureCredential({
+                      managedIdentityClientId: MANAGED_IDENTITY_CLIENT_ID as string,
                   });
-
-        credential
-            .getToken('https://vault.azure.net/.default')
-            .then(token => console.log('SUCCESS:', token))
-            .catch(err => console.error('ERROR:', JSON.stringify(err, null, 2)));
 
         this.client = new SecretClient(keyVaultUrl, credential);
     }
