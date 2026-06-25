f5cc5c1485dd70a6e4791cae6fc206f34085e0fd
ashwini.makanakattevenkatesha <ashwini.venkatesha@justice.gov.uk>
2026-06-25 17:05:01 +0100

    Added new functional test for System admin manage list types

--- src/test/end-to-end/tests/system-admin/manage-list-types-test.ts
+++ src/test/end-to-end/tests/system-admin/manage-list-types-test.ts
@@ -1,4 +1,3 @@
-
 Feature('System admin manage list types');
 
 Scenario('I as a system admin should be able to delete a user', async ({ I }) => {
@@ -9,9 +8,8 @@ Scenario('I as a system admin should be able to delete a user', async ({ I }) =>
     I.see('Single Justice Procedure Public List (Full List)');
     I.click(locate('//tr').withText('Single Justice Procedure Public List (Full List)').find('a').withText('Manage'));
     I.waitForText('Configure list type search fields for Single Justice Procedure Public List (Full List)');
-   I.see('Enter the JSON field names used to extract case details for this list type.');
+    I.see('Enter the JSON field names used to extract case details for this list type.');
     I.see('Case number JSON field name');
     I.see('Case name JSON field name');
     I.see('Continue');
 });
-
