b4e74023e7275b57052ea1754b7bddc54bb549a8
KianKwa <kian.kwa@justice.gov.uk>
2026-07-28 12:43:41 +0100

    Update e2e test

--- src/test/end-to-end/tests/verified/email-subscriptions-test.ts
+++ src/test/end-to-end/tests/verified/email-subscriptions-test.ts
@@ -15,9 +15,7 @@ const TEST_LAST_NAME = testConfig.TEST_SUITE_PREFIX + 'Surname';
 
 const caseId = '12341234';
 const caseName = 'Test Case Name';
-const caseURN = 'Case URN';
 const caseNameNumber = '12341232';
-const caseNameUrn = '18472381412';
 
 const displayFrom = DateTime.now().toISO({ includeOffset: false });
 const displayTo = DateTime.now().plus({ days: 1 }).toISO({ includeOffset: false });
@@ -96,24 +94,6 @@ Scenario(
 
         I.click('Email subscriptions');
         I.click('Add email subscription');
-        I.click('#subscription-choice-3');
-        I.click('Continue');
-        I.waitForText('What is the reference number?');
-        I.see(
-            'Please enter either a case reference number, case ID or unique reference number (URN). You must enter an exact match.'
-        );
-        I.fillField('#search-input', caseURN);
-        I.click('Continue');
-        I.waitForText('Subscription case search results');
-        I.see('1 found');
-        I.see(caseURN);
-        I.click('Continue');
-        I.waitForText('Confirm your email subscriptions');
-        I.click('Confirm Subscriptions');
-        I.waitForText('Subscription confirmation');
-
-        I.click('Email subscriptions');
-        I.click('Add email subscription');
         I.click('#subscription-choice-2');
         I.click('Continue');
         I.waitForText('What is the name of the case?');
@@ -123,7 +103,6 @@ Scenario(
         I.waitForText('Subscription case search results');
         I.see(caseName);
         I.see(caseNameNumber);
-        I.see(caseNameUrn);
         I.checkOption('//*[@id="12341232"]');
         I.checkOption('//*[@id="18472381412"]');
         I.click('Continue');
@@ -136,27 +115,22 @@ Scenario(
         I.see(locationName);
         I.see(caseId);
         I.see(caseName);
-        I.see(caseURN);
         I.see(caseNameNumber);
-        I.see(caseNameUrn);
 
         I.click('Subscriptions by case');
         I.dontSee(locationName);
         I.see(caseId);
         I.see(caseName);
-        I.see(caseURN);
         I.see(caseNameNumber);
-        I.see(caseNameUrn);
 
         I.click('Subscriptions by court or tribunal');
         I.see(locationName);
         I.dontSee(caseId);
         I.dontSee(caseName);
-        I.dontSee(caseURN);
 
         I.click('All subscriptions');
-        I.see(caseURN);
-        I.click(locate('//tr').withText(caseURN).find('a').withText('Unsubscribe'));
+        I.see(caseId);
+        I.click(locate('//tr').withText(caseId).find('a').withText('Unsubscribe'));
 
         I.waitForText('Are you sure you want to remove this subscription?');
         I.click('#unsubscribe-confirm');
@@ -169,7 +143,6 @@ Scenario(
         I.click(locate('//tr').withText(caseName).find('input').withAttr({ name: 'caseSubscription' }));
         I.click(locate('//tr').withText(locationName).find('input').withAttr({ name: 'courtSubscription' }));
         I.click(locate('//tr').withText(caseId).find('input').withAttr({ name: 'caseSubscription' }));
-        I.click(locate('//tr').withText(caseNameUrn).find('input').withAttr({ name: 'caseSubscription' }));
 
         I.click('#bulk-unsubscribe-button');
         I.waitForText('Are you sure you want to remove these subscriptions?');
