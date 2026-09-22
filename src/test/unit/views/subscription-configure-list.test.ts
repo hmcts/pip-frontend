75c9897be40edcc14a46e4bf33645ccb80077734
alexbottenberg <alexandra.bottenberg@justice.gov.uk>
2026-09-22 15:24:23 +0100

    Removing TECHNOLOGY_AND_CONSTRUCTION_COURT_KB_DAILY_CAUSE_LIST and adding TECHNOLOGY_AND_CONSTRUCTION_COURT_DAILY_CAUSE_LIST

--- src/test/unit/views/subscription-configure-list.test.ts
+++ src/test/unit/views/subscription-configure-list.test.ts
@@ -128,7 +128,7 @@ describe('Subscription Configure List', () => {
                 'Single Justice Procedure Press Register',
                 'Single Justice Procedure Public List (Full List)',
                 'Single Justice Procedure Public List (New Cases)',
-                'TECHNOLOGY_AND_CONSTRUCTION_COURT_KB_DAILY_CAUSE_LIST',
+                'Technology and Construction Court Daily Cause List',
             ];
 
             const tableRows = htmlRes
@@ -181,7 +181,7 @@ describe('Subscription Configure List', () => {
                 'SJP_PRESS_REGISTER',
                 'SJP_PUBLIC_LIST',
                 'SJP_DELTA_PUBLIC_LIST',
-                'TECHNOLOGY_AND_CONSTRUCTION_COURT_KB_DAILY_CAUSE_LIST',
+                'TECHNOLOGY_AND_CONSTRUCTION_COURT_DAILY_CAUSE_LIST',
             ];
 
             const checkboxes = htmlRes.getElementsByName('list-selections[]');
@@ -250,7 +250,7 @@ describe('Subscription Configure List', () => {
                 'Select Single Justice Procedure Press Register\nCofrestr y Wasg Y Weithdrefn Un Ynad',
                 'Select Single Justice Procedure Public List (Full List)\nRhestr Gyhoeddus Y Weithdrefn Un Ynad (Rhestr Lawn)',
                 'Select Single Justice Procedure Public List (New Cases)\nRhestr Gyhoeddus Y Weithdrefn Un Ynad (Achosion Newydd)',
-                'Select Technology and Construction Court (King’s Bench Division) Daily Cause List\nRhestr Achosion Dyddiol Llys Technoleg ac Adeiladu (Adran Mainc y Brenin)',
+                'Select Technology and Construction Court Daily Cause List\nRhestr Achosion Dyddiol Llys Technoleg ac Adeiladwaith',
             ];
 
             const tableRows = htmlRes
@@ -304,7 +304,7 @@ describe('Subscription Configure List', () => {
                 'SJP_PRESS_REGISTER',
                 'SJP_PUBLIC_LIST',
                 'SJP_DELTA_PUBLIC_LIST',
-                'TECHNOLOGY_AND_CONSTRUCTION_COURT_KB_DAILY_CAUSE_LIST',
+                'TECHNOLOGY_AND_CONSTRUCTION_COURT_DAILY_CAUSE_LIST',
             ];
 
             const checkboxes = htmlRes.getElementsByName('list-selections[]');
